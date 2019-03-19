c2d296199a3bd326a57e71e946fe765e3c879f22
Marcelo Maia <mmaia.cc@gmail.com>
2019-03-19 18:43:28 -0300

    carga DRE ok.

--- utility/dre_load_data.py
+++ utility/dre_load_data.py
@@ -16,39 +16,40 @@ df['EMAIL2'] = df['EMAIL2'].str.strip()
 df['TELEFONE2'] = df['TELEFONE2'].str.strip()
 
 
-def get_or_create_manager(line, instance):
-    if isinstance(instance, SubManagerProfile):  # sub manager
-        sub = instance.objects.filter(functional_register=line.RF).first()
-        if not line.RF:
-            print(line)
-            return None
-        mob, ph = line.TELEFONE.split('_')
-        if not sub:
-            sub = instance(name=line.COGESTOR,
-                           email=line.EMAIL,
-                           functional_register=line.RF,
-                           phone=ph,
-                           mobile_phone=mob)
-            sub.save()
-    else:  # alternate
-        sub = instance.objects.filter(functional_register=line.RF2).first()
-        if not line.RF2:
-            return None
-        if not sub:
-            sub = instance(name=line.SUPLENTE,
-                           email=line.EMAIL2,
-                           functional_register=line.RF2,
-                           phone=line.TELEFONE2)
-            sub.save()
+def get_or_create_submanager(line):
+    print(line)
+    sub = SubManagerProfile.objects.filter(functional_register=line.RF).first()
+    if not line.RF:
+        return None
+    mob, ph = line.TELEFONE.split('_')
+    if not sub:
+        sub = SubManagerProfile(name=line.COGESTOR,
+                                email=line.EMAIL,
+                                functional_register=line.RF,
+                                phone=ph,
+                                mobile_phone=mob)
+        sub.save()
+    return sub
+
+
+def get_or_create_alternate(line):
+    sub = AlternateProfile.objects.filter(functional_register=line.RF2).first()
+    if not line.RF2:
+        print(line)
+        return None
+    if not sub:
+        sub = AlternateProfile(name=line.SUPLENTE,
+                               email=line.EMAIL2,
+                               functional_register=line.RF2,
+                               phone=line.TELEFONE2)
+        sub.save()
     return sub
 
 
 for _, line in df.iterrows():
-    # print(line)
     phone = line.TELEFONE
-
-    sub_manager = get_or_create_manager(line, SubManagerProfile)
-    alternate_manager = get_or_create_manager(line, AlternateProfile)
+    sub_manager = get_or_create_submanager(line)
+    alternate_manager = get_or_create_alternate(line)
 
     dre = RegionalDirectorProfile(abbreviation=line.DRE,
                                   description='My Wonderful Description',
