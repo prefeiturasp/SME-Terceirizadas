162eeb2d865f0aab9a041bd5316b0de2ba5c02bf
calvin rossinhole <calvin.masters@gmail.com>
2019-03-21 15:56:03 -0300

    melhorias no admin

--- sme_pratoaberto_terceirizadas/user_profiles/admin.py
+++ sme_pratoaberto_terceirizadas/user_profiles/admin.py
@@ -1,1 +1,15 @@
 from django.contrib import admin
+from .models import NutritionistProfile, RegionalDirectorProfile
+
+
+class NutritionistProfileAdmin(admin.ModelAdmin):
+    list_display = ('regional_director',)
+
+
+class RegionalDirectorProfileAdmin(admin.ModelAdmin):
+    list_display = ('abbreviation', 'description', 'sub_manager', 'alternate')
+    search_fields = ('abbreviation',)
+
+
+admin.site.register(NutritionistProfile, NutritionistProfileAdmin)
+admin.site.register(RegionalDirectorProfile, RegionalDirectorProfileAdmin)
