970bdaf6f39f838f95c768fdea0c06f7c933cd3f
Marcelo Maia <mmaia.cc@gmail.com>
2019-03-22 11:36:46 -0300

    test user model creation

--- sme_pratoaberto_terceirizadas/users/tests/test_models.py
+++ sme_pratoaberto_terceirizadas/users/tests/test_models.py
@@ -6,3 +6,8 @@ pytestmark = pytest.mark.django_db
 
 def test_user_get_absolute_url(user: settings.AUTH_USER_MODEL):
     assert user.get_absolute_url() == f"/users/{user.username}/"
+
+
+def test_new_user(django_user_model):
+    django_user_model.objects.create(username="someone", is_nutritionist=True, functional_register="etc",
+                                     password="something", phone='phone', mobile_phone='mob')
