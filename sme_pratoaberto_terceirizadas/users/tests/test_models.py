5a07415715f27135716bc2f32e488baef6a7fba0
marcelo <mmaia.cc@gmail.com>
2019-03-30 22:16:25 -0300

    não precisa testar persistencia dos models. já são testados pelo django.

--- sme_pratoaberto_terceirizadas/users/tests/test_models.py
+++ sme_pratoaberto_terceirizadas/users/tests/test_models.py
@@ -8,8 +8,3 @@ class TestUsers:
 
     def test_user_get_absolute_url(self, user: settings.AUTH_USER_MODEL):
         assert user.get_absolute_url() == f"/users/{user.username}/"
-
-    def test_new_user(self, django_user_model):
-        self.user = django_user_model.objects.create(username="someone", is_nutritionist=True,
-                                                     functional_register="etc", phone='phone',
-                                                     password="something", mobile_phone='mob')
