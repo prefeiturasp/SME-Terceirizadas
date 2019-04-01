31a7fd00dc40a7008c984dde7b825c0fdce9655c
wesley1263 <weslei.souza@amcom.com.br>
2019-04-01 16:07:23 -0300

    Ajustes de modelagem 01/04 : 16:07

--- sme_pratoaberto_terceirizadas/users/models.py
+++ sme_pratoaberto_terceirizadas/users/models.py
@@ -1,20 +1,19 @@
 import uuid
 
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
-class User(AbstractUser):
-    is_nutritionist = models.BooleanField(default=False, editable=False)
-    is_regional_director = models.BooleanField(default=False, editable=False)
-    is_alternate = models.BooleanField(default=False, editable=False)
-    is_sub_manager = models.BooleanField(default=False, editable=False)
-    is_outsourced = models.BooleanField(default=False, editable=False)
+class User(AbstractUser,PermissionsMixin):
     phone = models.CharField(_('Phone'), max_length=11, null=True)
     mobile_phone = models.CharField(_('Mobile phone'), max_length=11, null=True)
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    is_trusty = models.BooleanField(_('trusty'), default=False,
+                                    help_text=_('Designates whether this user has confirmed his account.'))
+
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
+
