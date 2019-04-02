689cbe0b7801a9a76401851c14609ddedeed8d63
wesley1263 <weslei.souza@amcom.com.br>
2019-04-02 18:15:30 -0300

    Removido conteudos de user_profile

--- sme_pratoaberto_terceirizadas/user_profiles/admin.py
+++ sme_pratoaberto_terceirizadas/user_profiles/admin.py
@@ -1,20 +1,2 @@
 from django.contrib import admin
 
-from .models import AlternateProfile, NutritionistProfile
-
-
-class AlternateProfileAdmin(admin.ModelAdmin):
-    pass
-
-
-class NutritionistProfileAdmin(admin.ModelAdmin):
-    list_display = ('regional_director',)
-
-
-class RegionalDirectorProfileAdmin(admin.ModelAdmin):
-    list_display = ('abbreviation', 'description', 'sub_manager', 'alternate')
-    search_fields = ('abbreviation',)
-
-
-admin.site.register(AlternateProfile, AlternateProfileAdmin)
-admin.site.register(NutritionistProfile, NutritionistProfileAdmin)
--- sme_pratoaberto_terceirizadas/user_profiles/urls.py
+++ sme_pratoaberto_terceirizadas/user_profiles/urls.py
@@ -4,15 +4,15 @@ from .api import views
 app_name = "user_profiles"
 urlpatterns = [
     # /user_profiles/api/
-    url(
-        regex=r'^api/$',
-        view=views.SchoolProfileListCreateAPIView.as_view(),
-        name='user_profile_rest_api'
-    ),
-    # /user_profiles/api/:slug/
-    url(
-        regex=r'^api/(?P<eol_code>[-\w]+)/$',
-        view=views.SchoolProfileRetrieveUpdateDestroyAPIView.as_view(),
-        name='user_profile_rest_api'
-    )
+    # url(
+    #     regex=r'^api/$',
+    #     view=views.SchoolProfileListCreateAPIView.as_view(),
+    #     name='user_profile_rest_api'
+    # ),
+    # # /user_profiles/api/:slug/
+    # url(
+    #     regex=r'^api/(?P<eol_code>[-\w]+)/$',
+    #     view=views.SchoolProfileRetrieveUpdateDestroyAPIView.as_view(),
+    #     name='user_profile_rest_api'
+    # )
 ]
