ec39688467903b986a3e98386a1e1f3d47484f73
wesley1263 <weslei.souza@amcom.com.br>
2019-04-02 11:48:17 -0300

    Ajustando auth model do django| removido app CORE

--- sme_pratoaberto_terceirizadas/users/models.py
+++ sme_pratoaberto_terceirizadas/users/models.py
@@ -1,6 +1,6 @@
 import uuid
 
-from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.core.mail import send_mail
 from django.db import models
@@ -9,50 +9,50 @@ from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 
+class Profile(models.Model):
+    title = models.CharField(_('title'), max_length=90)
+    is_active = models.BooleanField(_('is_active'), default=True)
+
+
 class UserManager(BaseUserManager):
-
-    def _create_user(self, username, email, password, is_staff, is_superuser, **extra_fields):
-        now = timezone.now()
-
-        if not username:
-            raise ValueError(_('The given username must be set'))
-
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, is_staff=is_staff, is_active=True,
-                          is_superuser=is_superuser, last_login=now,
-                          date_joined=now, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-
-        return user
-
-    def create_user(self, username, email=None, password=None, **extra_fields):
-        return self._create_user(username, email, password, False, False, **extra_fields)
-
-    def create_superuser(self, username, email, password, **extra_fields):
-        user = self._create_user(username, email, password, True, True, **extra_fields)
-        user.is_active = True
-        user.save(using=self._db)
-        return user
+    pass
 
 
-class User(AbstractUser, PermissionsMixin):
+class User(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(_('name'), max_length=150)
+    email = models.CharField(_('email'), unique=True)
+    functional_register = models.CharField(_('functional register'),)
     phone = models.CharField(_('Phone'), max_length=11, null=True)
     mobile_phone = models.CharField(_('Mobile phone'), max_length=11, null=True)
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    last_login = models.DateTimeField(auto_now_add=True)
     is_trusty = models.BooleanField(_('trusty'), default=False,
                                     help_text=_('Designates whether this user has confirmed his account.'))
+    profile = models.ForeignKey(Profile, verbose_name=_('profile'), on_delete=models.DO_NOTHING)
+
+    is_staff = models.BooleanField(_('is_staff'), default=False)
+    is_active = models.BooleanField(_('is_active'), default=True)
+    admin = models.BooleanField(_('admin'), default=False)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def is_admin(self):
+        return self.admin
+
+    @property
+    def is_staff(self):
+        return self.is_staff
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    REQUIRED_FIELDS = ['name', '']
 
     objects = UserManager()
 
     class Meta:
         verbose_name = _('user')
         verbose_name_plural = _('users')
-        managed = False
-        db_table = 'user'
 
     def get_full_name(self):
         return '{} {}'.format(self.first_name, self.last_name)
