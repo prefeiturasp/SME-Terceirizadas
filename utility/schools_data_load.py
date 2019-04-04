0add0fb50d3166c3cf745af483cb96244a9e6009
Jaime Rozo <jaime.rozo@amcom.com.br>
2019-04-04 11:53:31 -0300

    alterações 01

--- utility/schools_data_load.py
+++ utility/schools_data_load.py
@@ -1,43 +1,114 @@
 #
-# schools_data_load.py
-#   Loads school's info from a csv file
+# Loads school data from csv
 #
 
-import environ
 import pandas as pd
 
-from sme_pratoaberto_terceirizadas.user_profiles.models import CityLocation, Address, SubTownHall, SchoolUnitType,
-ManagementType, SchoolProfile
+from sme_pratoaberto_terceirizadas.school.models import School, ManagementType, SchoolUnitType, Borough
+from sme_pratoaberto_terceirizadas.common_data.models import Address, CityLocation, Contact
 
-env = environ.Env()
-df = pd.read_csv('nutris.csv', sep=',', converters={'RF': str})
-df['DRE'] = df['DRE'].str.strip()
-df['NOME'] = df['NOME'].str.strip()
-df['RF'] = df['RF'].str.strip()
+df = pd.read_csv('escolas.csv', sep=',')
+df['EOL'] = df['EOL'].str.strip()
+df['UE'] = df['UE'].str.trip().str.upper()
+df['NOME'] = df['NOME'].str.upper()
+df['DRE'] = df['DRE'].str.strip().str.upper()
+df['DRE_SIGLA'] = df['DRE_SIGLA'].str.strip().str.upper()
+df['LOTE'] = df['LOTE'].str.strip().str.upper()
+df['EMAIL'] = df['EMAIL'].str.strip().str.lower()
+df['ENDERECO'] = df['ENDERECO'].str.strip().str.upper()
+df['NUMERO'] = df['NUMERO'].str.strip().str.upper()
+df['BAIRRO'] = df['BAIRRO'].str.strip().str.upper()
+df['CEP'] = df['CEP'].str.strip()
+df['TELEFONE1'] = df['TELEFONE1'].str.strip()
+df['TELEFONE2'] = df['TELEFONE2'].str.strip()
+df['EMPRESA'] = df['EMPRESA'].str.strip().str.upper()
+df['CODAE'] = df['CODAE'].str.trip()
 
 
-def get_or_create_dre(abbreviation):
-    reg = RegionalDirectorProfile.objects.filter(abbreviation=abbreviation).first()
-    if not reg:
-        reg = RegionalDirectorProfile(abbreviation=abbreviation)
-        reg.save()
-    return reg
+def check_create_borough(name, description):
+     borough = Borough.objects.filter(name=name).first()
+     if not borough:
+         borough =  Borough(name=name,
+                            description=description)
+         borough.save()
+     return borough
+
+def check_create_management_type(name, description):
+    mt = ManagementType.objects.filter(name=name).first()
+    if not mt:
+        mt = ManagementType(name=name,
+                            description=description)
+        mt.save()
+    return mt
+
+def check_create_school_unit(name, description):
+    ue = SchoolUnitType.objects.filter(name=name).first()
+    if not ue:
+        ue = SchoolUnitType(name=name,
+                            description=description)
+        ue.save()
+    return ue
+
+def check_create_city_location(city, state):
+    city = CityLocation.objects.filter(city=city, state=state).first()
+    if not city:
+        city = CityLocation(city=city,
+                            state=state)
+        city.save()
+    return city
+
+
+def create_address(line, city):
+    # TODO common_data_address.complement pode ser NULL
+    # TODO incluir complemento no csv
+    # TODO common_data_address.lat e common_data_address.lon podem ser NULL
+    address = Address(street_name = line.ENDERECO,
+                      # complement = line.COMPLEMENTO,
+                      district = line.BAIRRO,
+                      number = line.NUMERO,
+                      postal_code = line.CEP,
+                      city_location = city)
+    address.save()
+    return address
+
+# ----------------------------------------------------------------------------------------
 
 
 for _, line in df.iterrows():
-    print(line.RF, line.NOME, line.DRE.replace(' ', '').replace('.', ''))
-    dre = get_or_create_dre(abbreviation=line.DRE.replace(' ', '').replace('.', ''))
-    print(dre)
-    print('________')
-    names = line.NOME.split(' ')
-    first = names[0]
-    last = ' '.join(p for p in names[1:])
-    n = NutritionistProfile(first_name=first.strip(),
-                            last_name=last.strip(),
-                            password=env('DEFAULT_PASSWORD'),
-                            username=first + last[:3],  # primeiro nome + 3 letras
-                            functional_register=line.RF,
-                            regional_director=dre)
-    n.save()
+    print(line.NOME)
 
+    # SUBPREFEITURA/BOROUGH
+    # TODO Não tem subprefeitura no csv. Usando o nome da DRE.
+    subpref = check_create_borough(line.DRE, 'Nome da DRE')
 
+    # GESTÃO/MANAGEMENT_TYPE
+    # TODO Sempre 'TERCEIRIZADA'
+    gestao = check_create_management_type('TERCEIRIZADA', 'Gestão terceirizada')
+
+    # UNIDADE ESCOLAR/SCHOOL UNIT
+    ue = check_create_school_unit(line.UE, 'Unidade Escolar')
+
+    # CIDADE-ESTADO/CITY-STATE
+    # TODO por emquanto city='SÃO PAULO' state='SP'
+    cidade = check_create_city_location('SÃO PAULO', 'SP')
+
+    # ENDEREÇO/ADDRESS
+    endereco = create_address(line, cidade)
+
+    # ESCOLA/SCHOOL
+    escola = School(name = line.NOME,
+                    # grouping = line.AGRUPAMENTO,
+                    address = endereco,
+                    unit_id = ue,
+                    management_type = gestao,
+                    borough_id = subpref,
+                    eol_code = lin.EOL,
+                    codae_code = lin.CODAE)
+    escola.save()
+
+    # CONTATO/CONTACT
+    contato = Contact(name=line.NOME,
+                      phone=line.TELEFONE1,
+                      mobile_phone=line.TELEFONE2,
+                      email=line.EMAIL)
+    contato.save()
