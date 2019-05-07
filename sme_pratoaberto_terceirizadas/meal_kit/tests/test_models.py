e0b8b36dce0fadea72939b4b133c1c9b6c5b8a72
Marcelo Maia <mmaia.cc@gmail.com>
2019-05-07 16:36:26 -0300

    100% coverage kit lanche models.

--- sme_pratoaberto_terceirizadas/meal_kit/tests/test_models.py
+++ sme_pratoaberto_terceirizadas/meal_kit/tests/test_models.py
@@ -3,9 +3,12 @@ import pytest
 pytestmark = pytest.mark.django_db
 
 
-def test_meal_kit_name(meal_kit):
+def test_meal_kit_attrs(meal_kit):
     assert meal_kit.name == 'Jessica Bradley'
+    assert meal_kit.description == 'Door ten experience amount. Gas mind indeed notice live while analysis crime.'
+    assert meal_kit.__str__() == 'Jessica Bradley'
 
 
-def test_meal_kit_description(meal_kit):
-    assert meal_kit.description == 'Hit reveal rise now. Her medical enjoy city point nice.'
+def test_meal_kit_meta(meal_kit):
+    assert meal_kit._meta.verbose_name_plural == "Meal Kits"
+    assert meal_kit._meta.verbose_name == "Meal Kit"
