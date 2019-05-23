6b5afb3f149e0843536da0d81043a479b7894369
Marcelo Maia <mmaia.cc@gmail.com>
2019-05-23 13:18:19 -0300

    estrutura para fluxo de notificação + email ok.

--- sme_pratoaberto_terceirizadas/cardapio/api/viewsets.py
+++ sme_pratoaberto_terceirizadas/cardapio/api/viewsets.py
@@ -5,8 +5,11 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from sme_pratoaberto_terceirizadas.users.models import User
+from sme_pratoaberto_terceirizadas.utils import send_notification, send_email
 from .serializers import AlteracaoCardapioSerializer
 from ..models import AlteracaoCardapio
+
+cintia_qs = User.objects.filter(email='cintia.ramos@amcom.com.br')
 
 
 class AlteracaoCardapioViewSet(ModelViewSet):
@@ -15,9 +18,24 @@ class AlteracaoCardapioViewSet(ModelViewSet):
     lookup_field = 'uuid'
 
     def create(self, request: Request, *args: Any, **kwargs: Any) -> Response:
-        cintia_user = User.objects.get(2)
-        print(request.user, 'my user...', type(request.user))
-        retval = super().create(request, *args, **kwargs)
-        if retval:
-            print('disparando notificação para a DRE...')
-        return retval
+        response = super().create(request, *args, **kwargs)
+        if response:
+            # TODO: implementar logica das partes interessadas na notificação...
+            short_desc = 'Alteração de cardápio criada'.format(response.data.get('id', None))
+            send_notification(sender=request.user, recipients=cintia_qs,
+                              short_desc=short_desc, long_desc=response.data)
+            send_email(short_desc, str(response.data), 'mmaia.cc@gmail.com')
+        return response
+
+    def partial_update(self, request: Request, *args: Any, **kwargs: Any) -> Response:
+        response = super().partial_update(request, *args, **kwargs)
+        if response:
+            alt_id = response.data.get('id', None)
+            status = response.data.get('status', None)
+            short_desc = 'Alteração de cardápio #{} atualizada para: {}'.format(
+                alt_id, status)
+            send_notification(sender=request.user, recipients=cintia_qs,
+                              short_desc=short_desc, long_desc=response.data)
+
+            send_email(short_desc, str(response.data), 'mmaia.cc@gmail.com')
+        return response
