fe6b0789f338cec1730251db02dc2028ea33f5a3
Wesley1263 <weslei.andrade.souza@gmail.com>
2019-05-23 19:28:05 -0300

    Terminado backend para solicitação de kit lanche definitiva

--- sme_pratoaberto_terceirizadas/meal_kit/api/viewsets.py
+++ sme_pratoaberto_terceirizadas/meal_kit/api/viewsets.py
@@ -8,7 +8,9 @@ from rest_framework.viewsets import ModelViewSet
 
 from sme_pratoaberto_terceirizadas.meal_kit.api.serializers import MealKitSerializer, OrderMealKitSerializer
 from sme_pratoaberto_terceirizadas.school.models import School
+from sme_pratoaberto_terceirizadas.users.models import User
 from ..models import MealKit, OrderMealKit
+from sme_pratoaberto_terceirizadas.utils import send_notification
 
 
 class MealKitViewSet(ModelViewSet):
@@ -40,28 +42,55 @@ class OrderMealKitViewSet(ModelViewSet):
         else:
             return Response({'detail': 'Solicitação não encontrada'}, status=status.HTTP_400_BAD_REQUEST)
 
+    def update(self, request, pk=None, *args, **kwargs, ):
+
+        data = request.data
+        data_passeio = datetime.strptime(data['evento_data'], '%d/%m/%Y').date()
+        meals = MealKit.objects.filter(meals__uuid__in=data['kit_lanche'])
+
+        solicitacao = OrderMealKit.objects.get(pk=pk)
+        if solicitacao:
+            solicitacao.location = data['local_passeio']
+            solicitacao.students_quantity = data['nro_alunos']
+            solicitacao.order_date = data_passeio
+            solicitacao.observation = data['obs'],
+            solicitacao.status = 'SAVED'
+            solicitacao.scheduled_time = data['tempo_passeio']
+            for m in meals:
+                solicitacao.meal_kits.add(m)
+            solicitacao.save()
+
+            return Response({'details': 'Solicitação atualizada com sucesso'})
+        return Response({'details': 'Solicitação não encontrada'}, status=status.HTTP_400_BAD_REQUEST)
+
     def create(self, request):
 
-        school = School.objects.filter(users=request.user).first()
+        escola = self._valida_escola(request.user)
 
-        if not school:
-            return Response({'detail': 'Sem escola relacinada a este usuário'}, status=status.HTTP_401_UNAUTHORIZED)
+        # lote_solicitacoes = self._checa_solicitacoes_is_lote(request.data, escola)
 
-        if self._validar_duplicidade(request.data, school):
-            return self._enviar_solicitacao_kit_lanche(request.data, school)
+        if self._validar_duplicidade(request.data, escola):
+            return self._enviar_solicitacao_kit_lanche(request.data, escola)
 
         return Response({'detail': 'A Solicitação já foi cadastrada no sistema'}, status=status.HTTP_400_BAD_REQUEST)
 
-    def _validar_duplicidade(self, data, school):
+    def _valida_escola(self, user: User):
+        escola = School.objects.filter(users=user).first()
+
+        if not escola:
+            return Response({'detail': 'Sem escola relacinada a este usuário'}, status=status.HTTP_401_UNAUTHORIZED)
+        return escola
+
+    def _validar_duplicidade(self, data, escolas, status='SAVED'):
 
         data_passeio = datetime.strptime(data['evento_data'], '%d/%m/%Y')
 
-        if OrderMealKit.objects.filter(schools=school, order_date=data_passeio):
+        if OrderMealKit.objects.filter(schools=escolas, order_date=data_passeio, status=status):
             return False
 
         return True
 
-    def _enviar_solicitacao_kit_lanche(self, params, school):
+    def _enviar_solicitacao_kit_lanche(self, params, escola):
 
         try:
             data_passeio = datetime.strptime(params['evento_data'], '%d/%m/%Y').date()
@@ -85,7 +114,7 @@ class OrderMealKitViewSet(ModelViewSet):
             )
             solicitacao.save()
 
-            solicitacao.schools.add(school)
+            solicitacao.schools.add(escola)
             for m in meals:
                 solicitacao.meal_kits.add(m)
 
@@ -95,26 +124,18 @@ class OrderMealKitViewSet(ModelViewSet):
         except ObjectDoesNotExist:
             return Response({'details': 'Erro ao tentar salvar solicitação'}, status=status.HTTP_502_BAD_GATEWAY)
 
-    def update(self, request, pk=None, *args, **kwargs, ):
+    def _checa_solicitacoes_is_lote(self, request, usuario):
+        if 'ids' in request:
+            solicitacoes = OrderMealKit.objects.filter(pk__in=request['ids'])
 
-        data = request.data
-        data_passeio = datetime.strptime(data['evento_data'], '%d/%m/%Y').date()
-        meals = MealKit.objects.filter(meals__uuid__in=data['kit_lanche'])
+        if solicitacoes:
+            for solicitacao in solicitacoes:
+                solicitacao.status = 'SENDED'
+                solicitacao.save()
+                send_notification(usuario, User.objects.filter(email='mmaia.cc@gmail.com'), 'Teste de nofiticação', 'Teste de nofiticação')
+            return Response({'details': 'Solicitações enviadas com sucesso.'})
+        return Response({'details': 'Nenhuma soliciação encontrada'})
 
-        solicitacao = OrderMealKit.objects.get(pk=pk)
-        if solicitacao:
-            solicitacao.location = data['local_passeio']
-            solicitacao.students_quantity = data['nro_alunos']
-            solicitacao.order_date = data_passeio
-            solicitacao.observation = data['obs'],
-            solicitacao.status = 'SAVED'
-            solicitacao.scheduled_time = data['tempo_passeio']
-            for m in meals:
-                solicitacao.meal_kits.add(m)
-            solicitacao.save()
-
-            return Response({'details': 'Solicitação atualizada com sucesso'})
-        return Response({'details': 'Solicitação não encontrada'}, status=status.HTTP_400_BAD_REQUEST)
-
-    def _disparar_notificacao(self, data):
-        pass
+    @action(detail=False, methods=['post'])
+    def solicitacoes(self, request):
+        return self._checa_solicitacoes_is_lote(request.data, request.user)
