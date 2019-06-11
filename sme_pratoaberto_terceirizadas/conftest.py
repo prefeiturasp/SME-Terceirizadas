e7c3bda1944b59c394f1650a3f9c85aec48b3069
Marcelo Maia <mmaia.cc@gmail.com>
2019-06-11 16:44:31 -0300

    removido completamente factory boy

--- sme_pratoaberto_terceirizadas/conftest.py
+++ sme_pratoaberto_terceirizadas/conftest.py
@@ -2,30 +2,36 @@ import pytest
 from faker import Faker
 from model_mommy import mommy
 
+from sme_pratoaberto_terceirizadas.meal_kit.models import MealKit
 from sme_pratoaberto_terceirizadas.permission.models import Permission, ProfilePermission
-from .meal_kit.tests.factories import MealKitFactory
-from .users.tests.factories import ProfileFactory
+from sme_pratoaberto_terceirizadas.users.models import Profile, Institution
 
 fake = Faker('pt_BR')
 fake.seed(420)
 
 
 @pytest.fixture
-def meal_kit() -> MealKitFactory:
-    return MealKitFactory()
+def meal_kit():
+    #     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    #     name = models.CharField(_('Name'), max_length=160)
+    #     description = models.TextField(_('Description'), blank=True, null=True)
+    #     is_active = models.BooleanField(_('Is Active'), default=True)
+    #     meals = models.ManyToManyField(Meal)
+    return mommy.make(MealKit, name='kit lance nro tal', description='este kit lanche foi feito por fulano em...')
 
 
 @pytest.fixture
-def profile() -> ProfileFactory:
-    return ProfileFactory()
+def profile():
+    institution = mommy.make(Institution, name='Faker SA')
+    return mommy.make(Profile, title='título do perfil', institution=institution)
 
 
 @pytest.fixture
 def permission():
-    return mommy.make(Permission, title=fake.sentence(), endpoint=fake.url())
+    return mommy.make(Permission, title='A permissão do fulano de tal', endpoint='http://meu.endpoint/')
 
 
 @pytest.fixture
 def profile_permission():
-    permission = mommy.make(Permission, title=fake.sentence(), endpoint=fake.url())
+    permission = mommy.make(Permission, title='A permissão do fulano de tal', endpoint='http://meu.endpoint/')
     return mommy.make(ProfilePermission, permission=permission)
--- sme_pratoaberto_terceirizadas/permission/tests/test_models.py
+++ sme_pratoaberto_terceirizadas/permission/tests/test_models.py
@@ -4,10 +4,10 @@ pytestmark = pytest.mark.django_db
 
 
 def test_permission(permission):
-    assert permission.title == 'Qui in ad placeat deserunt.'
-    assert permission.endpoint == 'https://www.moura.org/'
+    assert permission.title == 'A permissão do fulano de tal'
+    assert permission.endpoint == 'http://meu.endpoint/'
 
 
 def test_profile_permission(profile_permission):
-    assert profile_permission.permission.title == 'Quidem vero occaecati iste delectus voluptate iure.'
-    assert profile_permission.permission.endpoint == 'http://da.br/'
+    assert profile_permission.permission.title == 'A permissão do fulano de tal'
+    assert profile_permission.permission.endpoint == 'http://meu.endpoint/'
