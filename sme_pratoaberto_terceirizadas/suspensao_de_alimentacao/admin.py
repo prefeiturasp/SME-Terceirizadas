577d15decd2b06c5e2466881735feecc649baa03
Calvin Rossinhole <calvin.masters@gmail.com>
2019-06-11 13:47:27 -0300

    testes para suspensao de alimentacao

--- sme_pratoaberto_terceirizadas/suspensao_de_alimentacao/admin.py
+++ sme_pratoaberto_terceirizadas/suspensao_de_alimentacao/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import DiaRazaoSuspensaoDeAlimentacao, RazaoSuspensaoDeAlimentacao, SuspensaoDeAlimentacao, \
-    DescricaoSuspensaoDeAlimentacao
+    DescricaoSuspensaoDeAlimentacao, StatusSuspensaoDeAlimentacao
 
 admin.site.register(SuspensaoDeAlimentacao)
 admin.site.register(DiaRazaoSuspensaoDeAlimentacao)
 admin.site.register(RazaoSuspensaoDeAlimentacao)
 admin.site.register(DescricaoSuspensaoDeAlimentacao)
+admin.site.register(StatusSuspensaoDeAlimentacao)
