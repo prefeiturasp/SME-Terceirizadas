06c77e89510359380c972dbaeb1ee3778a61d7cd
Marcelo Maia <mmaia.cc@gmail.com>
2019-06-11 14:10:41 -0300

    teste dos validadores ok

--- sme_pratoaberto_terceirizadas/common_data/utils.py
+++ sme_pratoaberto_terceirizadas/common_data/utils.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 
 from workalendar.america import BrazilSaoPauloCity
 
@@ -10,12 +10,12 @@ def str_to_date(date_str, format_date_str='%d/%m/%Y'):
     Auxilliary method to define format to send and receive dates
     """
     try:
-        return datetime.strptime(date_str, format_date_str).date()
+        return datetime.datetime.strptime(date_str, format_date_str).date()
     except ValueError:
         raise Exception('invalid_date')
 
 
-def get_working_days_after(days=5, date=datetime.utcnow().date()):
+def get_working_days_after(days=2, date=datetime.datetime.now()):
     """Retorna o próximo dia útil após a variável days"""
     return calendar.add_working_days(date, days)
 
