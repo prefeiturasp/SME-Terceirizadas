8484089d0739143af0a01e6f82524cb5416f1a97
Marcelo Maia <mmaia.cc@gmail.com>
2019-06-11 17:00:37 -0300

    testes ok!

--- sme_pratoaberto_terceirizadas/conftest.py
+++ sme_pratoaberto_terceirizadas/conftest.py
@@ -2,9 +2,10 @@ import pytest
 from faker import Faker
 from model_mommy import mommy
 
-from sme_pratoaberto_terceirizadas.meal_kit.models import MealKit
-from sme_pratoaberto_terceirizadas.permission.models import Permission, ProfilePermission
-from sme_pratoaberto_terceirizadas.users.models import Profile, Institution
+from sme_pratoaberto_terceirizadas.food.models import Meal
+from .meal_kit.models import MealKit
+from .permission.models import Permission, ProfilePermission
+from .users.models import Profile, Institution
 
 fake = Faker('pt_BR')
 fake.seed(420)
@@ -12,12 +13,12 @@ fake.seed(420)
 
 @pytest.fixture
 def meal_kit():
-    #     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    #     name = models.CharField(_('Name'), max_length=160)
-    #     description = models.TextField(_('Description'), blank=True, null=True)
-    #     is_active = models.BooleanField(_('Is Active'), default=True)
-    #     meals = models.ManyToManyField(Meal)
-    return mommy.make(MealKit, name='kit lance nro tal', description='este kit lanche foi feito por fulano em...')
+    meal1 = mommy.make(Meal)
+    meal2 = mommy.make(Meal)
+    meal3 = mommy.make(Meal)
+    return mommy.make(MealKit, name='kit lance nro tal',
+                      description='este kit lanche foi feito por fulano em...',
+                      meals=[meal1, meal2, meal3])
 
 
 @pytest.fixture
--- sme_pratoaberto_terceirizadas/meal_kit/tests/test_models.py
+++ sme_pratoaberto_terceirizadas/meal_kit/tests/test_models.py
@@ -7,6 +7,7 @@ def test_meal_kit_attrs(meal_kit):
     assert meal_kit.name == 'kit lance nro tal'
     assert meal_kit.description == 'este kit lanche foi feito por fulano em...'
     assert meal_kit.__str__() == 'kit lance nro tal'
+    assert meal_kit.meals.count() == 3
 
 
 def test_meal_kit_meta(meal_kit):
