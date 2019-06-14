da8576cafc91a7e3ce99d4c073691770b2ac57ed
BrunoTSBuzzo <brunobuzzo.dev@gmail.com>
2019-06-14 15:16:54 -0300

    Tradução de app escola pt1 (alterando nome das classes)

--- sme_pratoaberto_terceirizadas/escola/admin.py
+++ sme_pratoaberto_terceirizadas/escola/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
-from .models import DiretorRegional, PeriodoEscolar, Escola, SchoolGroup
+from .models import DiretorRegional, PeriodoEscolar, Escola, GrupoEscolar
 
 
 @admin.register(DiretorRegional)
 class RegionalDirectorAdmin(admin.ModelAdmin):
-    list_display = ['abbreviation']
-    list_filter = ['abbreviation']
+    list_display = ['codigo']
+    list_filter = ['codigo']
 
 
 admin.site.register(Escola)
 admin.site.register(PeriodoEscolar)
-admin.site.register(SchoolGroup)
+admin.site.register(GrupoEscolar)
--- sme_pratoaberto_terceirizadas/escola/models.py
+++ sme_pratoaberto_terceirizadas/escola/models.py
@@ -2,98 +2,98 @@ import uuid as uuid
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from sme_pratoaberto_terceirizadas.abstract_shareable import Describable, Activable, TemChaveExterna
+from sme_pratoaberto_terceirizadas.abstract_shareable import Descritivel, Ativavel, TemChaveExterna
 from sme_pratoaberto_terceirizadas.users.models import User
 from sme_pratoaberto_terceirizadas.terceirizada.models import Lote
 
 
-class DiretorRegional(Describable):
+class DiretorRegional(Descritivel):
     """DRE - Diretoria Regional"""
     # TODO chave estrangeira para Institution
-    abbreviation = models.CharField(_('Abbreviation'), max_length=10)
+    codigo = models.CharField(_('Codigo'), max_length=10)
     users = models.ManyToManyField(User, blank=True, related_name='DREs')
 
     def __str__(self):
-        return _('Abbreviation') + self.abbreviation
+        return _('Codigo') + self.codigo
 
     class Meta:
-        verbose_name = _("Regional Director")
-        verbose_name_plural = _("Regional Directors")
+        verbose_name = _("Diretor Regional")
+        verbose_name_plural = _("Diretores Regionais")
 
 
-class PeriodoEscolar(Describable):
+class PeriodoEscolar(Descritivel):
     """Períodos Escolares"""
-    FIRST_PERIOD = 'first_period'
-    SECOND_PERIOD = 'second_period'
-    THIRD_PERIOD = 'third_period'
-    FOURTH_PERIOD = 'fourth_period'
-    INTEGRATE = 'integrate'
-    value = models.CharField(max_length=50, blank=True, null=True)
-    meal_types = models.ManyToManyField('food.MealType', blank=True)
+    PRIMEIRO_PERIODO = 'primeiro_periodo'
+    SEGUNDO_PERIODO = 'segundo_periodo'
+    TERCEIRO_PERIODO = 'terceiro_periodo'
+    QUARTO_PERIODO = 'quarto_periodo'
+    INTEGRAL = 'integral'
+    valor = models.CharField(max_length=50, blank=True, null=True)
+    tipo_refeicao = models.ManyToManyField('food.MealType', blank=True)
 
     def __str__(self):
-        return self.name
+        return self.nome
 
     class Meta:
-        verbose_name = _("School Period")
-        verbose_name_plural = _("School Periods")
+        verbose_name = _("Periodo Escolar")
+        verbose_name_plural = _("Periodos Escolares")
 
 
-class SchoolAge(Describable, Activable, TemChaveExterna):
+class IdadeEscolar(Descritivel, Ativavel, TemChaveExterna):
     """Tabela utilizada para registrar faixa etária de idade"""
 
 
-class Borough(Describable, Activable):
+class SubPrefeitura(Descritivel, Ativavel):
     """Sub Prefeitura"""
 
 
-class SchoolUnitType(Describable, Activable):
+class TipoUnidadeEscolar(Descritivel, Ativavel):
     """Tipo de unidade escolar: EMEI, EMEF, CIEJA..."""
 
 
-class ManagementType(Describable, Activable):
+class TipoGestao(Descritivel, Ativavel):
     """Tipo de gestão escolar: DIRETA, MISTA, TERCEIRIZADA..."""
 
 
-class SchoolGroup(TemChaveExterna):
+class GrupoEscolar(TemChaveExterna):
     """Agrupamento"""
     codigo = models.SmallIntegerField(_('Grouping'))
 
 
-class Escola(Activable):
+class Escola(Ativavel):
     """Escola"""
     # TODO chave estrangeira para Institution
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    name = models.CharField(_("Name"), max_length=160)
-    eol_code = models.CharField(_("EOL code"), max_length=10)
-    codae_code = models.CharField(_('CODAE code'), max_length=10)
+    nome = models.CharField(_("Nome"), max_length=160)
+    codigo_eol = models.CharField(_("Codigo EOL"), max_length=10)
+    codigo_codae = models.CharField(_('Codigo CODAE'), max_length=10)
     lote = models.ForeignKey(Lote,
                              on_delete=models.DO_NOTHING,
                              blank=True,
                              null=True)
-    grouping = models.ForeignKey(SchoolGroup,
+    agrupamento = models.ForeignKey(GrupoEscolar,
                                  on_delete=models.DO_NOTHING,
                                  blank=True,
                                  null=True)
-    regional_director = models.ForeignKey(DiretorRegional,
+    diretor_regional = models.ForeignKey(DiretorRegional,
                                           on_delete=models.DO_NOTHING,
                                           blank=True,
                                           null=True)
-    unit_type = models.ForeignKey(SchoolUnitType,
+    tipo_unidade = models.ForeignKey(TipoUnidadeEscolar,
                                   on_delete=models.DO_NOTHING,
                                   blank=True,
                                   null=True)
-    management_type = models.ForeignKey(ManagementType,
+    tipo_gestao = models.ForeignKey(TipoGestao,
                                         on_delete=models.DO_NOTHING,
                                         blank=True,
                                         null=True)
-    borough = models.ForeignKey(Borough,
+    sub_prefeitura = models.ForeignKey(SubPrefeitura,
                                 blank=True,
                                 null=True,
                                 on_delete=models.DO_NOTHING)
-    ages = models.ManyToManyField(SchoolAge, blank=True)
-    periods = models.ManyToManyField(PeriodoEscolar, blank=True)
+    idades = models.ManyToManyField(IdadeEscolar, blank=True)
+    periodos = models.ManyToManyField(PeriodoEscolar, blank=True)
     users = models.ManyToManyField(User, blank=True)
 
     def __str__(self):
-        return self.name
+        return self.nome
