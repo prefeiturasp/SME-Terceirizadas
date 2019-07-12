5b6637beb1f04db52858d91c2c393d08df8b8463
Marcelo Maia <mmaia.cc@gmail.com>
2019-07-12 11:44:07 -0300

    criando lote e DRE.

--- utility/carga_dados/escola/diretoria_regional.py
+++ utility/carga_dados/escola/diretoria_regional.py
@@ -7,22 +7,47 @@ Diretoria regional:
 import pandas as pd
 
 from sme_pratoaberto_terceirizadas.escola.models import DiretoriaRegional
+from sme_pratoaberto_terceirizadas.terceirizada.models import Lote
 
-df = pd.read_excel('/home/amcom/Documents/planilhas/lista_unidades.xlsx', sheet_name='Escolas2801 Consulta')
+df = pd.read_excel('/home/marcelo/Desktop/docs PO alimentacao/lista de lotes.xlsx',
+                   sheet_name='Sheet1')
 
-lista_nomes = [df['DRE'][index] for index in df.index]
+# padroniza os dados
+df['DRE'] = df['DRE'].str.strip().str.upper()
+df['LOTES'] = df['LOTES'].str.strip().str.upper()
+df['LOTE_SIGLA'] = df['LOTE_SIGLA'].str.strip().str.upper()
 
-lista_elementos = []
 
-for elemento in lista_nomes:
-    nome_dre = ""
+def cria_dres():
+    cont = 0
+    for index, row in df.iterrows():
+        dre_str = row.DRE
+        obj, created = DiretoriaRegional.objects.get_or_create(nome=dre_str)
+        if created:
+            cont += 1
+        print('DRE {} criada? {}'.format(dre_str, created))
+    print('quantidade de DRES criadas: {}'.format(cont))
 
-    for nome in elemento.split()[4:]:
-        nome_dre += " " + nome
 
-    if nome_dre not in lista_elementos:
-        lista_elementos.append(nome_dre)
-        dre = DiretoriaRegional(
-            nome=nome_dre
-        )
-        dre.save()
+def cria_lotes():
+    cont = 0
+    for index, row in df.iterrows():
+        dre_str = row.DRE
+        dre_obj, created_dre = DiretoriaRegional.objects.get_or_create(
+            nome=dre_str)
+
+        lote_str = row.LOTES
+        sigla_lote_str = row.LOTE_SIGLA
+        lote_obj, created_lote = Lote.objects.get_or_create(
+            nome=lote_str,
+            iniciais=sigla_lote_str,
+            diretoria_regional=dre_obj)
+
+        if created_lote:
+            cont += 1
+        print('Lote {} criado? {}'.format(lote_str, created_lote))
+    print('quantidade de LOTES criadas: {}'.format(cont))
+
+
+cria_dres()
+cria_lotes()
