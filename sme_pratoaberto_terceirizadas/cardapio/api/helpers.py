84de7d44d358b8e74ee535a23c0f63ccfe7101e9
Weslei Souza <weslei.souza@amcom.com.br>
2019-07-16 16:12:28 -0300

    Finalizando solicitação de inversão de dia de cardápio parte 2

--- sme_pratoaberto_terceirizadas/cardapio/api/helpers.py
+++ sme_pratoaberto_terceirizadas/cardapio/api/helpers.py
@@ -1,20 +1,17 @@
-# TODO criar as notificações e disparar e-mails
 from sme_pratoaberto_terceirizadas.perfil.models import Usuario
-
-
-# TODO Resolver conflitos de disparos de notificações
 from sme_pratoaberto_terceirizadas.utils import enviar_notificacao
 
 
+# TODO Resolver conflitos de disparos de notificações
 def notificar_partes_envolvidas(usuario: Usuario, **kwargs):
-    escola = kwargs['escola']
-    usuarios_dre = escola.diretoria_regional__usuarios
-    de = kwargs['cardapio_de']
-    para = kwargs['cardapio_para']
-    descricao = 'Solicitação para inversção de dia de cardápio da escola: {} do dia: {} para o dia {}'.format(
-        escola.nome,
-        de,
-        para
-    )
-    enviar_notificacao(usuario, usuarios_dre, descricao, descricao)
+    # escola = kwargs['escola']
+    # usuarios_dre = escola.diretoria_regional__usuarios
+    # de = kwargs['cardapio_de']
+    # para = kwargs['cardapio_para']
+    # descricao = 'Solicitação para inversção de dia de cardápio da escola: {} do dia: {} para o dia {}'.format(
+    #     escola.nome,
+    #     de,
+    #     para
+    # )
+    # enviar_notificacao(usuario, usuarios_dre, descricao, descricao)
     pass
