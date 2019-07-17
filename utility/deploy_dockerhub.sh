6c769a33107fc6314c0c3e4f451290d5b9dd3224
Marcelo Maia <mmaia.cc@gmail.com>
2019-07-17 19:25:34 -0300

    Script de deploy tentativa 3

--- utility/deploy_dockerhub.sh
+++ utility/deploy_dockerhub.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
 if [ "$TRAVIS_BRANCH" = "development" ]; then
     echo "Deploy da imagem de desenvolvimento..."
-    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
     docker push marcelomaia/terceirizadas_backend
 fi
 
 if [ "$TRAVIS_BRANCH" = "master" ]; then
-    echo "Deploy da imagem de desenvolvimento..."
-    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-    docker push marcelomaia/terceirizadas_backend
+    tag="$TRAVIS_TAG"
+    if [ -n "$tag" ]; then
+        echo "Deploy da imagem de produção..."
+        docker push marcelomaia/terceirizadas_backend:$tag
+    fi
 fi
 
 #https://docs.travis-ci.com/user/environment-variables/#convenience-variables
-
-#$TRAVIS_TAG
+#https://docs.travis-ci.com/user/deployment/script/
