184f6ff1c78355619534bc722bd0b565dbd7baba
alcfernandes <alessandro.c.fernandes@gmail.com>
2019-07-25 10:15:39 -0300

    Adiciona motivo de alteração de cardápio no Admin do Django.

--- sme_pratoaberto_terceirizadas/cardapio/admin.py
+++ sme_pratoaberto_terceirizadas/cardapio/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 
 from .models import TipoAlimentacao, Cardapio, InversaoCardapio, AlteracaoCardapio, SuspensaoAlimentacao
+from .models import MotivoAlteracaoCardapio
 
 admin.site.register(TipoAlimentacao)
 admin.site.register(InversaoCardapio)
 admin.site.register(AlteracaoCardapio)
 admin.site.register(SuspensaoAlimentacao)
+admin.site.register(MotivoAlteracaoCardapio)
 
 
 @admin.register(Cardapio)
