6c173826dc49b796975324b6a6c2c9c74181a8ed
Calvin Rossinhole <calvin.masters@gmail.com>
2019-08-20 18:34:44 -0300

    removendo fixture inutilizada

--- sme_pratoaberto_terceirizadas/inclusao_alimentacao/__tests__/conftest.py
+++ sme_pratoaberto_terceirizadas/inclusao_alimentacao/__tests__/conftest.py
@@ -44,19 +44,3 @@ def inclusao_alimentacao_continua():
                       motivo=motivo,
                       outro_motivo=fake.name(),
                       escola=escola)
-
-
-@pytest.fixture
-@freeze_time("2019-08-01")
-def inclusao_alimentacao_continua_hoje():
-    mommy.make(TemplateMensagem, assunto='TESTE',
-               tipo=TemplateMensagem.INCLUSAO_ALIMENTACAO_CONTINUA,
-               template_html='@id @criado_em @status @link')
-    motivo = mommy.make(models.MotivoInclusaoContinua, nome=fake.name())
-    escola = mommy.make('escola.Escola')
-    return mommy.make(models.InclusaoAlimentacaoContinua,
-                      uuid='98dc7cb7-7a38-408d-907c-c0f073ca2d13',
-                      motivo=motivo,
-                      data_inicial=datetime.date.today(),
-                      outro_motivo=fake.name(),
-                      escola=escola)
