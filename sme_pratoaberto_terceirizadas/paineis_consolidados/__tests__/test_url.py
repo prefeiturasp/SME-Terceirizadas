0cb6e5d6718fc40417b0f8892e02d0c4ce2786ea
Marcelo Maia <mmaia.cc@gmail.com>
2019-08-26 17:00:33 -0300

    - ajuste diretorios

--- sme_pratoaberto_terceirizadas/paineis_consolidados/__tests__/test_url.py
+++ sme_pratoaberto_terceirizadas/paineis_consolidados/__tests__/test_url.py
@@ -5,4 +5,3 @@ from rest_framework import status
 def test_url_endpoint_painel_dre(client):
     response = client.get('/dre-pendentes-aprovacao/')
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
-
