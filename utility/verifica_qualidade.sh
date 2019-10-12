62b3808c767b42dd4dbd45d66ae99947b7ebb28e
Marcelo Maia <mmaia.cc@gmail.com>
2019-10-12 14:37:30 -0300

    Ajuste final de qualidade automatizada!!! \o/

--- utility/verifica_qualidade.sh
+++ utility/verifica_qualidade.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
-echo "Pytest..."
+echo "Verificando testes unitários e de integração..."
 pytest
-echo "Flake8..."
+
+echo "Verificando estilo e qualidade de código..."
 flake8
+
+echo "Verificando falhas de segurança média ou superior..."
+bandit -r sme_terceirizadas -ll
+
+echo "Verificando tipagem estática..."
+mypy sme_terceirizadas
+
+echo "Rodando code climate..."
+docker run \
+  --interactive --tty --rm \
+  --env CODECLIMATE_CODE="$PWD" \
+  --volume "$PWD":/code \
+  --volume /var/run/docker.sock:/var/run/docker.sock \
+  --volume /tmp/cc:/tmp/cc \
+  codeclimate/codeclimate analyze
