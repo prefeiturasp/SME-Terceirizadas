f3eec7440e8120f0dbbde79d05e39769652b1dfb
Marcelo Maia <mmaia.cc@gmail.com>
2019-10-28 12:01:44 -0300

    teste data inclusao continua

--- sme_terceirizadas/inclusao_alimentacao/__tests__/conftest.py
+++ sme_terceirizadas/inclusao_alimentacao/__tests__/conftest.py
@@ -39,17 +39,25 @@ def quantidade_por_periodo():
                       )
 
 
-@pytest.fixture
-def inclusao_alimentacao_continua(escola):
+@pytest.fixture(params=[
+    # data ini, data fim, esperado
+    (datetime.date(2019, 10, 1), datetime.date(2019, 10, 30), datetime.date(2019, 10, 1)),
+    (datetime.date(2019, 10, 1), datetime.date(2019, 9, 20), datetime.date(2019, 9, 20))
+]
+)
+def inclusao_alimentacao_continua_params(escola, motivo_inclusao_continua, request):
     mommy.make(TemplateMensagem, assunto='TESTE',
                tipo=TemplateMensagem.INCLUSAO_ALIMENTACAO_CONTINUA,
                template_html='@id @criado_em @status @link')
-    motivo = mommy.make(models.MotivoInclusaoContinua, nome=fake.name())
-    return mommy.make(models.InclusaoAlimentacaoContinua,
-                      uuid='98dc7cb7-7a38-408d-907c-c0f073ca2d13',
-                      motivo=motivo,
-                      outro_motivo=fake.name(),
-                      escola=escola)
+    data_inicial, data_final, esperado = request.param
+    model = mommy.make(models.InclusaoAlimentacaoContinua,
+                       uuid='98dc7cb7-7a38-408d-907c-c0f073ca2d13',
+                       motivo=motivo_inclusao_continua,
+                       data_inicial=data_inicial,
+                       data_final=data_final,
+                       outro_motivo=fake.name(),
+                       escola=escola)
+    return model, esperado
 
 
 @pytest.fixture(params=[
