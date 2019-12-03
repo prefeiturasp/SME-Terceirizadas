6ad1c0106bf20b348604e6a1d3c0a856ad28b457
Calvin Rossinhole <calvin.masters@gmail.com>
2019-12-03 14:35:06 -0300

    ajuste dieta especial - suporte .docx nos anexos

--- sme_terceirizadas/dieta_especial/api/serializers.py
+++ sme_terceirizadas/dieta_especial/api/serializers.py
@@ -1,11 +1,29 @@
-from rest_framework import serializers
-from drf_base64.serializers import ModelSerializer
+import base64
+import uuid
+from mimetypes import guess_extension
 
-from ...escola.api.serializers import EscolaSimplesSerializer
-from ...dados_comuns.api.serializers import LogSolicitacoesUsuarioSerializer
+from django.core.files.base import ContentFile
+from drf_base64.serializers import ModelSerializer
+from rest_framework import serializers
+
 from .validators import deve_ter_extensao_valida
-from ...dados_comuns.validators import deve_ser_no_passado
 from ..models import Anexo, SolicitacaoDietaEspecial
+from ...dados_comuns.api.serializers import LogSolicitacoesUsuarioSerializer
+from ...dados_comuns.validators import deve_ser_no_passado
+from ...escola.api.serializers import EscolaSimplesSerializer
+
+
+class AnexoCreateSerializer(serializers.ModelSerializer):
+    arquivo = serializers.CharField()
+    nome = serializers.CharField()
+
+    def validate_nome(self, nome):
+        deve_ter_extensao_valida(nome)
+        return nome
+
+    class Meta:
+        model = Anexo
+        fields = ('arquivo', 'nome')
 
 
 class AnexoSerializer(ModelSerializer):
@@ -22,7 +40,7 @@ class AnexoSerializer(ModelSerializer):
 
 class SolicitacaoDietaEspecialCreateSerializer(serializers.ModelSerializer):
     anexos = serializers.ListField(
-        child=AnexoSerializer(), required=True
+        child=AnexoCreateSerializer(), required=True
     )
 
     def validate_anexos(self, anexos):
@@ -40,9 +58,12 @@ class SolicitacaoDietaEspecialCreateSerializer(serializers.ModelSerializer):
         solicitacao = SolicitacaoDietaEspecial.objects.create(**validated_data)
 
         for anexo in anexos:
+            format, imgstr = anexo.get('arquivo').split(';base64,')
+            ext = guess_extension(format[5:])
+            data = ContentFile(base64.b64decode(imgstr), name=str(uuid.uuid4()) + ext)
             anexo.pop('nome')
             Anexo.objects.create(
-                solicitacao_dieta_especial=solicitacao, arquivo=anexo.get('arquivo')
+                solicitacao_dieta_especial=solicitacao, arquivo=data
             )
 
         solicitacao.inicia_fluxo(user=self.context['request'].user)
