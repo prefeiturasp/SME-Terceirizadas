09cebec187b137ab8c480aa90ee182a88aff8e00
Kelwy Oliveira <kelwyduarte@gmail.com>
2020-01-03 15:31:07 -0300

    Adiciona modelo de relatório utilizando o conceito de block do djando template.
    Ajusta estilo e adiciona rodpé pelo css.

--- sme_terceirizadas/relatorios/relatorios.py
+++ sme_terceirizadas/relatorios/relatorios.py
@@ -14,7 +14,7 @@ def generate_pdf(request):
     filtro = {'data_de': datetime.date.today(), 'data_para': datetime.date.today() + datetime.timedelta(days=30),
               'tipo_solicitacao': "TODOS", 'status': 'TODOS'}
     # Rendered
-    html_string = render_to_string('test.html', {'escola': escola, 'filtro': filtro})
+    html_string = render_to_string('relatorio1.html', {'escola': escola, 'filtro': filtro, })
     pdf_file = HTML(string=html_string, base_url=request.build_absolute_uri()).write_pdf()
     response = HttpResponse(pdf_file, content_type='application/pdf')
     response['Content-Disposition'] = 'filename="XXX.pdf"'
