d2bc865b02421d0e3b0c4b1502f69258132902c9
Pedro <pedrohardware@gmail.com>
2020-01-13 10:44:39 -0300

    ajustes

--- sme_terceirizadas/dieta_especial/__tests__/test_urls.py
+++ sme_terceirizadas/dieta_especial/__tests__/test_urls.py
@@ -9,7 +9,7 @@ from ..constants import (
 from ..models import AlergiaIntolerancia, ClassificacaoDieta, MotivoNegacao, TipoDieta
 
 
-def testa_endpoint_lista(client_autenticado, endpoint, quantidade):
+def endpoint_lista(client_autenticado, endpoint, quantidade):
     response = client_autenticado.get(f'/{endpoint}/')
     assert response.status_code == status.HTTP_200_OK
     json = response.json()
@@ -18,7 +18,7 @@ def testa_endpoint_lista(client_autenticado, endpoint, quantidade):
 
 def test_url_endpoint_lista_alergias_intolerancias(client_autenticado,
                                                    alergias_intolerancias):
-    testa_endpoint_lista(
+    endpoint_lista(
         client_autenticado,
         ENDPOINT_ALERGIAS_INTOLERANCIAS,
         quantidade=2
@@ -27,7 +27,7 @@ def test_url_endpoint_lista_alergias_intolerancias(client_autenticado,
 
 def test_url_endpoint_lista_classificacoes_dieta(client_autenticado,
                                                  classificacoes_dieta):
-    testa_endpoint_lista(
+    endpoint_lista(
         client_autenticado,
         ENDPOINT_CLASSIFICACOES_DIETA,
         quantidade=3
@@ -36,7 +36,7 @@ def test_url_endpoint_lista_classificacoes_dieta(client_autenticado,
 
 def test_url_endpoint_lista_motivos_negacao(client_autenticado,
                                             motivos_negacao):
-    testa_endpoint_lista(
+    endpoint_lista(
         client_autenticado,
         ENDPOINT_MOTIVOS_NEGACAO,
         quantidade=4
@@ -45,14 +45,14 @@ def test_url_endpoint_lista_motivos_negacao(client_autenticado,
 
 def test_url_endpoint_lista_tipos_dieta(client_autenticado,
                                         tipos_dieta):
-    testa_endpoint_lista(
+    endpoint_lista(
         client_autenticado,
         ENDPOINT_TIPOS_DIETA_ESPECIAL,
         quantidade=5
     )
 
 
-def testa_endpoint_detalhe(client_autenticado, endpoint, modelo, tem_nome=False):
+def endpoint_detalhe(client_autenticado, endpoint, modelo, tem_nome=False):
     obj = modelo.objects.first()
     response = client_autenticado.get(f'/{endpoint}/{obj.id}/')
     assert response.status_code == status.HTTP_200_OK
@@ -64,7 +64,7 @@ def testa_endpoint_detalhe(client_autenticado, endpoint, modelo, tem_nome=False)
 
 def test_url_endpoint_detalhe_alergias_intolerancias(client_autenticado,
                                                      alergias_intolerancias):
-    testa_endpoint_detalhe(
+    endpoint_detalhe(
         client_autenticado,
         ENDPOINT_ALERGIAS_INTOLERANCIAS,
         AlergiaIntolerancia
@@ -73,7 +73,7 @@ def test_url_endpoint_detalhe_alergias_intolerancias(client_autenticado,
 
 def test_url_endpoint_detalhe_classificacoes_dieta(client_autenticado,
                                                    classificacoes_dieta):
-    testa_endpoint_detalhe(
+    endpoint_detalhe(
         client_autenticado,
         ENDPOINT_CLASSIFICACOES_DIETA,
         ClassificacaoDieta,
@@ -83,7 +83,7 @@ def test_url_endpoint_detalhe_classificacoes_dieta(client_autenticado,
 
 def test_url_endpoint_detalhe_motivos_negacao(client_autenticado,
                                               motivos_negacao):
-    testa_endpoint_detalhe(
+    endpoint_detalhe(
         client_autenticado,
         ENDPOINT_MOTIVOS_NEGACAO,
         MotivoNegacao
@@ -92,7 +92,7 @@ def test_url_endpoint_detalhe_motivos_negacao(client_autenticado,
 
 def test_url_endpoint_detalhe_tipos_dieta(client_autenticado,
                                           tipos_dieta):
-    testa_endpoint_detalhe(
+    endpoint_detalhe(
         client_autenticado,
         ENDPOINT_TIPOS_DIETA_ESPECIAL,
         TipoDieta
