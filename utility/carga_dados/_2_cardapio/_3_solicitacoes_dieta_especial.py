7cc4e187ebb880d05b04edcc2f23758e2a001c12
Pedro <pedrohardware@gmail.com>
2020-01-23 10:49:41 -0300

    Cria massa de dados com escola aleatória

--- utility/carga_dados/_2_cardapio/_3_solicitacoes_dieta_especial.py
+++ utility/carga_dados/_2_cardapio/_3_solicitacoes_dieta_especial.py
@@ -5,10 +5,10 @@ import datetime
 import random
 import string
 
-from django.db import IntegrityError
+from django.db import IntegrityError, transaction
 from faker import Faker
 
-from sme_terceirizadas.escola.models import Aluno
+from sme_terceirizadas.escola.models import Aluno, Escola
 from .helper import base64_encode
 
 from sme_terceirizadas.dieta_especial.models import (
@@ -41,20 +41,46 @@ def fluxo_escola_felix_dieta_especial(obj, user, index):
             return
 
 
+def _get_random_from_queryset(qs):
+    return qs[random.randint(0, len(qs) - 1)]
+
+
+tipos_de_dieta = TipoDieta.objects.all()
 def _get_random_tipo_de_dieta():
-    return TipoDieta.objects.order_by("?").first()
+    return _get_random_from_queryset(tipos_de_dieta)
 
 
+classificacoes_dieta = ClassificacaoDieta.objects.all()
 def _get_random_classificacao_de_dieta():
-    return ClassificacaoDieta.objects.order_by("?").first()
+    return _get_random_from_queryset(classificacoes_dieta)
 
 
+alergias = AlergiaIntolerancia.objects.all()
 def _get_random_alergia():
-    return AlergiaIntolerancia.objects.order_by("?").first()
+    return _get_random_from_queryset(alergias)
 
 
+escola = Escola.objects.all()
+def _get_random_escola():
+    return _get_random_from_queryset(escola)
+
+
+@transaction.atomic
 def cria_solicitacoes_dieta_especial(qtd=50):
     user = Usuario.objects.get(email="escola@admin.com")
+    codigos_eol = random.sample(range(100000, 999999), qtd)
+    alunos = []
+    for index in range(qtd):
+        alunos.append(Aluno(
+            nome=f.text()[:25],
+            codigo_eol=''.join(random.choice(string.digits) for x in range(6)),
+            data_nascimento=datetime.date(2015, 10, 19),
+            escola=_get_random_escola()
+        ))
+    Aluno.objects.bulk_create(alunos)
+
+    solicitacoes = []
+    anexos = []
     for index in range(qtd):
         if index % 10 == 0:
             print(f'{index / 10}% COMPLETO')
@@ -88,17 +114,6 @@ def cria_solicitacoes_dieta_especial(qtd=50):
             tipo_dieta_2 = _get_random_tipo_de_dieta()
             alergia_1 = _get_random_alergia()
             alergia_2 = _get_random_alergia()
-            created = False
-            while not created:
-                try:
-                    aluno = Aluno.objects.create(
-                        nome=f.text()[:25],
-                        codigo_eol=''.join(random.choice(string.digits) for x in range(6)),
-                        data_nascimento=datetime.date(2015, 10, 19)
-                    )
-                    created = True
-                except IntegrityError:
-                    pass
             for i in range(random.randint(1, 7)):
                 solicitacao_dieta_especial = SolicitacaoDietaEspecial.objects.create(
                     criado_por=user,
@@ -107,17 +122,24 @@ def cria_solicitacoes_dieta_especial(qtd=50):
                     registro_funcional_nutricionista=''.join(random.choice(string.digits) for x in range(6)),
                     observacoes=f.text()[:25],
                     classificacao=_get_random_classificacao_de_dieta(),
-                    aluno=aluno,
+                    aluno=alunos[index],
                     ativo=True if random.randint(0, 1) == 1 else False
                 )
                 solicitacao_dieta_especial.alergias_intolerancias.add(alergia_1, alergia_2)
                 solicitacao_dieta_especial.tipos.add(tipo_dieta_1, tipo_dieta_2)
 
-                Anexo.objects.create(
+                # Anexo.objects.create(
+                #     solicitacao_dieta_especial=solicitacao_dieta_especial,
+                #     arquivo=base64_encode(f.text()[:20])
+                # )
+                anexos.append(Anexo(
                     solicitacao_dieta_especial=solicitacao_dieta_especial,
                     arquivo=base64_encode(f.text()[:20])
-                )
+                ))
                 fluxo_escola_felix_dieta_especial(solicitacao_dieta_especial, user, index)
+                solicitacoes.append(solicitacao_dieta_especial)
+
+    Anexo.objects.bulk_create(anexos)
 
 
 QTD_PEDIDOS = 1000
