b00300b1e4133039de78a735d2d6e32b1d3bedfa
Marcelo Maia <mmaia.cc@gmail.com>
2020-02-05 11:00:47 -0300

    ajuste

--- sme_terceirizadas/relatorios/urls.py
+++ sme_terceirizadas/relatorios/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path(r'relatorio-alteracao-cardapio', relatorios.relatorio_alteracao_cardapio, name='generate_pdf'),
     path(r'relatorio-dieta-especial', relatorios.relatorio_dieta_especial, name='generate_pdf'),
     path(r'relatorio-inclusao', relatorios.relatorio_inclusao_alimentacao_normal, name='generate_pdf'),
-    path('xxx', relatorios.relatorio_resumo_anual_e_mensal)
 ]
