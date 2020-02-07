f1040c01e0d5062da1a369ad9c110b16101db99f
Pedro <pedrohardware@gmail.com>
2020-02-07 13:47:55 -0300

    Script para reset do banco

--- utility/reset_db.sh
+++ utility/reset_db.sh
@@ -1,6 +1,7 @@
-PGPASSWORD=adminadmin
-for dbname in $(psql -h localhost -U admin -d postgres -c "copy (select datname from pg_database where datname like 'terceirizadas%') to stdout") ; do
-    echo "$dbname"
-    #dropdb -i "$dbname"
+export PGPASSWORD=adminadmin
+export PGOPTS="-h localhost -U admin "
+for dbname in $(psql $PGOPTS -d postgres -c "copy (select datname from pg_database where datname like 'terceirizadas%' or datname like 'test_terceirizadas%') to stdout") ; do
+    echo "Apagando $dbname"
+    dropdb $PGOPTS -i "$dbname"
 done
-
+createdb $PGOPTS terceirizadas
