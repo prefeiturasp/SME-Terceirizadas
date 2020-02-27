a95e3aa0e492a7c401b24c40edaa5dde50f5092c
Pedro <pedrohardware@gmail.com>
2020-02-27 16:54:50 -0300

    Cria modelo para alteração de cardápio CEI

--- sme_terceirizadas/cardapio/behaviors.py
+++ sme_terceirizadas/cardapio/behaviors.py
@@ -1,5 +1,11 @@
 from django.db import models
 
+from .managers import (
+    AlteracoesCardapioDestaSemanaManager,
+    AlteracoesCardapioDesteMesManager,
+    AlteracoesCardapioVencidaManager
+)
+
 
 class TemLabelDeTiposDeAlimentacao(models.Model):
     @property
@@ -11,6 +17,26 @@ class TemLabelDeTiposDeAlimentacao(models.Model):
             else:
                 label += f' e {tipo_alimentacao.nome}'
         return label
+
+    class Meta:
+        abstract = True
+
+
+class EhAlteracaoCardapio(models.Model):
+    objects = models.Manager()  # Manager Padrão
+    desta_semana = AlteracoesCardapioDestaSemanaManager()
+    deste_mes = AlteracoesCardapioDesteMesManager()
+    vencidos = AlteracoesCardapioVencidaManager()
+
+    escola = models.ForeignKey('escola.Escola', on_delete=models.DO_NOTHING, blank=True, null=True)
+    motivo = models.ForeignKey('MotivoAlteracaoCardapio', on_delete=models.PROTECT, blank=True, null=True)
+
+    @classmethod
+    def get_rascunhos_do_usuario(cls, usuario):
+        return cls.objects.filter(
+            criado_por=usuario,
+            status=cls.workflow_class.RASCUNHO
+        )
 
     class Meta:
         abstract = True
