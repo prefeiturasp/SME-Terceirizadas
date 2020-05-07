c9a164b5ca7d8e26d14c5d5740512c430cc9653a
Pedro <pedrohardware@gmail.com>
2020-05-07 15:32:21 -0300

    Inclui outro_motivo a inclusao alimentacao cei

--- sme_terceirizadas/inclusao_alimentacao/models.py
+++ sme_terceirizadas/inclusao_alimentacao/models.py
@@ -283,6 +283,7 @@ class InclusaoAlimentacaoDaCEI(Descritivel, TemData, TemChaveExterna, FluxoAprov
     escola = models.ForeignKey('escola.Escola', on_delete=models.DO_NOTHING,
                                related_name='grupos_inclusoes_por_cei')
     motivo = models.ForeignKey(MotivoInclusaoNormal, on_delete=models.DO_NOTHING)
+    outro_motivo = models.CharField('Outro motivo', blank=True, max_length=50)
     periodo_escolar = models.ForeignKey('escola.PeriodoEscolar', on_delete=models.DO_NOTHING)
     tipos_alimentacao = models.ManyToManyField('cardapio.ComboDoVinculoTipoAlimentacaoPeriodoTipoUE')
 
