aba8ac096fd6c54ea5ec040e031d2146bb6ad832
BrunoTSBuzzo <brunshzl@gmail.com>
2020-05-26 16:27:14 -0300

    inserindo logica para alteração de produto

--- sme_terceirizadas/produto/api/viewsets.py
+++ sme_terceirizadas/produto/api/viewsets.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from xworkflows import InvalidTransitionError
 
 from ...dados_comuns import constants
-from ...dados_comuns.permissions import UsuarioCODAEGestaoProduto
+from ...dados_comuns.permissions import UsuarioCODAEGestaoProduto, UsuarioTerceirizada
 from ..models import (
     Fabricante,
     HomologacaoDoProduto,
