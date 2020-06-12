3963c7ea24d14c7d2ff995f7bd94dbfac3b7eb0c
Pedro Walter <pedrohardware@gmail.com>
2020-06-12 11:51:12 -0300

    Mais filtros e endpoints necessarios

--- sme_terceirizadas/produto/forms.py
+++ sme_terceirizadas/produto/forms.py
@@ -6,3 +6,5 @@ class ProdutoPorParametrosForm(forms.Form):
     nome_marca = forms.CharField(required=False)
     nome_produto = forms.CharField(required=False)
     nome_terceirizada = forms.CharField(required=False)
+    data_inicial = forms.DateField(required=False)
+    data_final = forms.DateField(required=False)
