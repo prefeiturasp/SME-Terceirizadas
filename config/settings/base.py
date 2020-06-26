35d6bf110b6f3b8bc650dce22832f0152396f279
Pedro Walter <pedrohardware@gmail.com>
2020-06-26 13:38:13 -0300

    Tentando corrigir erro de CSS nos PDFs, parte 2

--- config/settings/base.py
+++ config/settings/base.py
@@ -81,6 +81,10 @@ DJANGO_APPS = [
     'django.contrib.admin',
 ]
 THIRD_PARTY_APPS = [
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'crispy_forms',
     'django_prometheus',
     'rest_framework',
     'rest_framework_swagger',
