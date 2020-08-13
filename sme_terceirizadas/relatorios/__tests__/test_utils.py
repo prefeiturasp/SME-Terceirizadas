92b9e33db290b72d48969aa11e9357f5f36a1507
rodolfo <rodolfo@lemeconsultoria.com.br>
2020-08-13 12:05:30 -0300

    Cria teste de config de cabecario

--- sme_terceirizadas/relatorios/__tests__/test_utils.py
+++ sme_terceirizadas/relatorios/__tests__/test_utils.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from ..utils import get_config_cabecario_relatorio_analise
 
 
@@ -5,3 +7,9 @@ def test_config_cabecario_obter_cabecario_reduzido():
     filtros = {}
     config = get_config_cabecario_relatorio_analise(filtros, None, None)
     assert config['cabecario_tipo'] == 'CABECARIO_REDUZIDO'
+
+
+def test_config_cabecario_obter_cabecario_por_data():
+    filtros = {'data_analise_inicial': date.today().strftime('%d/%m/%Y')}
+    config = get_config_cabecario_relatorio_analise(filtros, None, None)
+    assert config['cabecario_tipo'] == 'CABECARIO_POR_DATA'
