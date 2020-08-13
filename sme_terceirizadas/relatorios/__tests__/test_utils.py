aaa9d9a09a3e87ce652265e796923464d9df9d6a
rodolfo <rodolfo@lemeconsultoria.com.br>
2020-08-13 02:45:51 -0300

    Ajusta teste get cabecario analise sensorial

--- sme_terceirizadas/relatorios/__tests__/test_utils.py
+++ sme_terceirizadas/relatorios/__tests__/test_utils.py
@@ -3,5 +3,5 @@ from ..utils import get_config_cabecario_relatorio_analise
 
 def test_config_cabecario_obter_cabecario_reduzido():
     filtros = {}
-    config = get_config_cabecario_relatorio_analise(filtros)
+    config = get_config_cabecario_relatorio_analise(filtros, None, None)
     assert config['cabecario_tipo'] == 'CABECARIO_REDUZIDO'
