a6260e8d9f450078b5b4ecc346e12b0c92eefe03
rg3915 <rg3915@yahoo.com.br>
2020-09-26 00:06:55 -0300

    Carga de dados: Adiciona usuários. feature 24572

--- sme_terceirizadas/perfil/management/commands/carga_dados.py
+++ sme_terceirizadas/perfil/management/commands/carga_dados.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from utility.carga_dados.usuarios import cria_usuarios
 
 
 class Command(BaseCommand):
@@ -6,3 +7,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write('Importando dados...')
+        cria_usuarios()
---
+++ utility/carga_dados/usuarios.py
@@ -0,0 +1,140 @@
+from sme_terceirizadas.perfil.models import Usuario
+from utility.carga_dados.escola.helper import bcolors
+
+
+USUARIOS = [
+    {
+        'mensagem': 'admin do sistema',
+        'email': 'admin@admin.com',
+        'password': 'adminadmin',
+        'cpf': '11111111110',
+        'registro_funcional': '1111110',
+    },
+    {
+        'mensagem': 'Escola admin',
+        'email': 'escola@admin.com',
+        'password': 'adminadmin',
+        'cpf': '11111111111',
+        'registro_funcional': '1111111',
+    },
+    {
+        'mensagem': 'DRE admin',
+        'email': 'dre@admin.com',
+        'password': 'adminadmin',
+        'cpf': '11111111112',
+        'registro_funcional': '1111112',
+    },
+    {
+        'mensagem': 'TERC admin',
+        'email': 'terceirizada@admin.com',
+        'password': 'adminadmin',
+        'cpf': '11111111113',
+        'registro_funcional': '1111113',
+    },
+    {
+        'mensagem': 'CODAE admin',
+        'email': 'codae@admin.com',
+        'password': 'adminadmin',
+        'cpf': '11111111114',
+        'registro_funcional': '1111114',
+    },
+    {
+        'mensagem': 'CODAE Nutricionista admin',
+        'email': 'nutricodae@admin.com',
+        'password': 'adminadmin',
+        'cpf': '11111111115',
+        'registro_funcional': '1111115',
+    },
+    {
+        'mensagem': 'Nutricionista Supervisao admin',
+        'email': 'nutrisupervisao@admin.com',
+        'password': 'adminadmin',
+        'cpf': '11111111125',
+        'registro_funcional': '1111125',
+    },
+    {
+        'mensagem': 'CODAE - Gestao de Produtos - admin',
+        'email': 'gpcodae@admin.com',
+        'password': 'adminadmin',
+        'cpf': '21111111114',
+        'registro_funcional': '2111114',
+    },
+    {
+        'mensagem': 'Escola CEI admin',
+        'email': 'escolacei@admin.com',
+        'password': 'adminadmin',
+        'cpf': '11111111116',
+        'registro_funcional': '1111116',
+    },
+    {
+        'mensagem': 'Escola CEI CEU admin',
+        'email': 'escolaceiceu@admin.com',
+        'password': 'adminadmin',
+        'cpf': '11111111117',
+        'registro_funcional': '1111117',
+    },
+    {
+        'mensagem': 'Escola CCI admin',
+        'email': 'escolacci@admin.com',
+        'password': 'adminadmin',
+        'cpf': '11111111118',
+        'registro_funcional': '1111118',
+    },
+    {
+        'mensagem': 'Escola EMEF admin',
+        'email': 'escolaemef@admin.com',
+        'password': 'adminadmin',
+        'cpf': '11111111119',
+        'registro_funcional': '1111119',
+    },
+    {
+        'mensagem': 'Escola EMEBS admin',
+        'email': 'escolaemebs@admin.com',
+        'password': 'adminadmin',
+        'cpf': '11111111120',
+        'registro_funcional': '1111120',
+    },
+    {
+        'mensagem': 'Escola CIEJA admin',
+        'email': 'escolacieja@admin.com',
+        'password': 'adminadmin',
+        'cpf': '11111111121',
+        'registro_funcional': '1111121',
+    },
+    {
+        'mensagem': 'Escola EMEI admin',
+        'email': 'escolaemei@admin.com',
+        'password': 'adminadmin',
+        'cpf': '11111111122',
+        'registro_funcional': '1111122',
+    },
+    {
+        'mensagem': 'Escola CEU EMEI admin',
+        'email': 'escolaceuemei@admin.com',
+        'password': 'adminadmin',
+        'cpf': '11111111123',
+        'registro_funcional': '1111123',
+    },
+    {
+        'mensagem': 'Escola CEU EMEF admin',
+        'email': 'escolaceuemef@admin.com',
+        'password': 'adminadmin',
+        'cpf': '11111111124',
+        'registro_funcional': '1111124',
+    },
+]
+
+
+def cria_usuarios():
+    print(f'{bcolors.OKGREEN}Criando os usuários...{bcolors.ENDC}')
+    for usuario in USUARIOS:
+        if Usuario.objects.filter(email=usuario['email']).first():
+            print(f"{bcolors.FAIL}Usuário {usuario['email']} já existe!{bcolors.ENDC}")
+        else:
+            print(f"{usuario['mensagem']}")
+            Usuario.objects.create_superuser(
+                email=usuario['email'],
+                password=usuario['password'],
+                cpf=usuario['cpf'],
+                registro_funcional=usuario['registro_funcional'],
+            )
