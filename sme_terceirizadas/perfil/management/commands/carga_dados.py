3699813ad0248fd1ca90bd86c77f5b4dc8e468b2
rg3915 <rg3915@yahoo.com.br>
2020-09-26 03:01:50 -0300

    Importa dados_comuns.contatos.py. feature 24572

--- sme_terceirizadas/perfil/management/commands/carga_dados.py
+++ sme_terceirizadas/perfil/management/commands/carga_dados.py
@@ -3,6 +3,7 @@ from utility.carga_dados.usuarios import cria_usuarios
 from utility.carga_dados.cardapio.importa_dados import cria_motivoalteracaocardapio
 from utility.carga_dados.cardapio.importa_dados import cria_motivosuspensao
 from utility.carga_dados.cardapio.importa_dados import cria_tipo_alimentacao
+from utility.carga_dados.dados_comuns.importa_dados import cria_contatos
 
 
 class Command(BaseCommand):
@@ -14,3 +15,4 @@ class Command(BaseCommand):
         cria_motivoalteracaocardapio()
         cria_motivosuspensao()
         cria_tipo_alimentacao()
+        cria_contatos()
--- utility/carga_dados/dados_comuns/importa_dados.py
+++ utility/carga_dados/dados_comuns/importa_dados.py
@@ -2,4 +2,15 @@ from sme_terceirizadas.dados_comuns.data.contatos import data_contatos
 from sme_terceirizadas.dados_comuns.data.templatemensagem import data_templatemensagem
 from sme_terceirizadas.dados_comuns.models import Contato
 from sme_terceirizadas.dados_comuns.models import TemplateMensagem
-from utility.carga_dados.helper import ja_existe
+
+
+def cria_contatos():
+    for item in data_contatos:
+        obj = Contato.objects.filter(email=item['email']).first()
+        if not obj:
+            Contato.objects.create(
+                telefone=item['telefone'],
+                telefone2=item['telefone2'],
+                celular=item['celular'],
+                email=item['email'],
+            )
