9849792997172ea3c1b2a0388772f1c50680a2ab
rg3915 <rg3915@yahoo.com.br>
2020-10-03 03:01:26 -0300

    Importa kit_lanche.kit_lanche.py. feature 24572

--- utility/carga_dados/kit_lanche/importa_dados.py
+++ utility/carga_dados/kit_lanche/importa_dados.py
@@ -1,6 +1,6 @@
+from random import sample
 from utility.carga_dados.helper import ja_existe, progressbar
 
-from sme_terceirizadas.kit_lanche.data.kit_lanche import data_kit_lanche
 from sme_terceirizadas.kit_lanche.data.kit_lanche_item import data_kit_lanche_item
 from sme_terceirizadas.kit_lanche.models import ItemKitLanche, KitLanche
 
@@ -13,4 +13,12 @@ def cria_kit_lanche_item():
 
 
 def cria_kit_lanche():
-    pass
+    # Valores randomicos
+    items = list(ItemKitLanche.objects.all())
+    # Deleta kits existentes
+    KitLanche.objects.all().delete()
+    for i in progressbar(range(1, 11), 'Kit Lanche'):
+        kit_amostra = sample(items, 3)
+        kit_lanche = KitLanche.objects.create(nome=f'Kit {i}')
+        for kit in kit_amostra:
+            kit_lanche.itens.add(kit)
