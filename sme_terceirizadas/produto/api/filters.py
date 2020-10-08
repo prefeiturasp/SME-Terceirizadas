b89e441e2fd5bd0a73ac54b01a4563efff974ea9
rodolfo <rodolfo@lemeconsultoria.com.br>
2020-10-08 20:37:41 -0300

    Ajusta filtro de status.

--- sme_terceirizadas/produto/api/filters.py
+++ sme_terceirizadas/produto/api/filters.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 
 from django_filters import rest_framework as filters
 
+from ...dados_comuns.fluxo_status import HomologacaoProdutoWorkflow
 from ..models import Produto
 from ..utils import converte_para_datetime, cria_filtro_aditivos
 
@@ -9,7 +10,6 @@ from ..utils import converte_para_datetime, cria_filtro_aditivos
 class ProdutoFilter(filters.FilterSet):
     uuid = filters.CharFilter(field_name='homologacoes__uuid', lookup_expr='iexact')
     nome_produto = filters.CharFilter(field_name='nome', lookup_expr='icontains')
-    status = filters.CharFilter(field_name='homologacoes__status__in', method='filtra_homologacoes')
     data_inicial = filters.DateFilter(field_name='homologacoes__criado_em', lookup_expr='date__gte')
     data_final = filters.DateFilter(field_name='homologacoes__criado_em', lookup_expr='date__lte')
     nome_marca = filters.CharFilter(field_name='marca__nome', lookup_expr='icontains')
@@ -18,19 +18,25 @@ class ProdutoFilter(filters.FilterSet):
                                            lookup_expr='icontains')
     aditivos = filters.CharFilter(field_name='aditivos', method='filtra_aditivos')
 
+    status = filters.MultipleChoiceFilter(
+        field_name='homologacoes__status',
+        choices=[(str(state), state) for state in HomologacaoProdutoWorkflow.states]
+    )
+
     class Meta:
         model = Produto
-        fields = ['nome_produto', 'nome_marca', 'nome_fabricante', 'nome_terceirizada', 'data_inicial',
-                  'data_final', 'tem_aditivos_alergenicos', 'eh_para_alunos_com_dieta']
+        fields = ['nome_produto',
+                  'nome_marca',
+                  'nome_fabricante',
+                  'nome_terceirizada',
+                  'data_inicial',
+                  'data_final',
+                  'tem_aditivos_alergenicos',
+                  'eh_para_alunos_com_dieta']
 
     def filtra_aditivos(self, qs, name, value):
         filtro = cria_filtro_aditivos(value)
         return qs.filter(filtro)
-
-    def filtra_homologacoes(self, qs, name, value):
-        value = self.request.query_params.getlist('status')
-        filtro = {name: value}
-        return qs.filter(**filtro)
 
 
 def filtros_produto_reclamacoes(request):
