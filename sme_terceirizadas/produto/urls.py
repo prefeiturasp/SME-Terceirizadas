5e878f95139bf31ec2ca52dad37a9e5e157f46b2
Regis Santos <rg3915@yahoo.com.br>
2020-12-30 15:17:52 -0300

    Adiciona viewsets e rota.feature 30533-30839

--- sme_terceirizadas/produto/urls.py
+++ sme_terceirizadas/produto/urls.py
@@ -6,19 +6,29 @@ from .api import viewsets
 router = routers.DefaultRouter()
 
 router.register('produtos', viewsets.ProdutoViewSet, 'Produtos')
+router.register('nome-de-produtos-edital', viewsets.NomeDeProdutoEditalViewSet, 'Nome de Produto Edital')
 router.register('homologacoes-produtos', viewsets.HomologacaoProdutoViewSet, 'Homologação de Produtos')
 router.register('reclamacoes-produtos', viewsets.ReclamacaoProdutoViewSet, 'Reclamação de Produtos')
-router.register('painel-gerencial-homologacoes-produtos', viewsets.HomologacaoProdutoPainelGerencialViewSet,
-                'Painel Gerencial de Homologação de Produtos')
+router.register(
+    'painel-gerencial-homologacoes-produtos',
+    viewsets.HomologacaoProdutoPainelGerencialViewSet,
+    'Painel Gerencial de Homologação de Produtos'
+)
 router.register('informacoes-nutricionais', viewsets.InformacaoNutricionalViewSet, 'Informações Nutricionais')
 router.register('protocolo-dieta-especial', viewsets.ProtocoloDeDietaEspecialViewSet, 'Protocolo Dieta Especial')
 router.register('fabricantes', viewsets.FabricanteViewSet, 'Fabricantes')
 router.register('marcas', viewsets.MarcaViewSet, 'Marcas')
 router.register('produto-imagens', viewsets.ImagensViewset, 'Imagens')
-router.register('analise-sensorial', viewsets.RespostaAnaliseSensorialViewSet,
-                basename='analise-sensorial')
-router.register('solicitacao-cadastro-produto-dieta', viewsets.SolicitacaoCadastroProdutoDietaViewSet,
-                'Solicitação cadastro de Produtos')
+router.register(
+    'analise-sensorial',
+    viewsets.RespostaAnaliseSensorialViewSet,
+    basename='analise-sensorial'
+)
+router.register(
+    'solicitacao-cadastro-produto-dieta',
+    viewsets.SolicitacaoCadastroProdutoDietaViewSet,
+    'Solicitação cadastro de Produtos'
+)
 
 urlpatterns = [
     path('', include(router.urls)),
