c957b71a7fb93cc3db0dcb4855f815818ba553f6
Pedro Walter <pedrohardware@gmail.com>
2021-01-06 22:13:49 -0300

    Corrige calculo de matriculados convencional e cria testes

--- sme_terceirizadas/lancamento_inicial/__tests__/test_utils.py
+++ sme_terceirizadas/lancamento_inicial/__tests__/test_utils.py
@@ -1,6 +1,8 @@
 from datetime import date
 
-from ..utils import eh_feriado_ou_fds, mes_para_faixa
+import pytest
+
+from ..utils import eh_feriado_ou_fds, matriculados_convencional_em_uma_data, mes_para_faixa
 
 
 def test_mes_para_faixa(faixas_de_data):
@@ -21,3 +23,32 @@ def test_eh_feriado_ou_fds():
     assert eh_feriado_ou_fds(date(2020, 10, 12)) is True
     assert eh_feriado_ou_fds(date(2020, 10, 13)) is False
     assert eh_feriado_ou_fds(date(2020, 10, 14)) is False
+
+
+@pytest.mark.django_db
+def test_matriculados_convencional_em_uma_data_1(escola_periodo_escolar_com_quantidade_de_alunos,
+                                                 log_mudanca_qtde_1):
+    hoje = date.today()
+
+    assert matriculados_convencional_em_uma_data(
+        escola_periodo_escolar_com_quantidade_de_alunos, hoje) == 123
+
+
+@pytest.mark.django_db
+def test_matriculados_convencional_em_uma_data_2(escola_periodo_escolar_com_quantidade_de_alunos,
+                                                 log_mudanca_qtde_1,
+                                                 log_mudanca_qtde_2):
+    data = date(2020, 12, 16)
+
+    assert matriculados_convencional_em_uma_data(
+        escola_periodo_escolar_com_quantidade_de_alunos, data) == 121
+
+
+@pytest.mark.django_db
+def test_matriculados_convencional_em_uma_data_3(escola_periodo_escolar_com_quantidade_de_alunos,
+                                                 log_mudanca_qtde_1,
+                                                 log_mudanca_qtde_2):
+    data = date(2020, 12, 6)
+
+    assert matriculados_convencional_em_uma_data(
+        escola_periodo_escolar_com_quantidade_de_alunos, data) == 119
