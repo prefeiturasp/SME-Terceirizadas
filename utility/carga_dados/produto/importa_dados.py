60cef6f12d10563c36a9964a63a4931e4aa40900
Regis Santos <rg3915@yahoo.com.br>
2021-01-12 16:16:03 -0300

    Carga de dados de produtos. feature 30256-30849

--- utility/carga_dados/produto/importa_dados.py
+++ utility/carga_dados/produto/importa_dados.py
@@ -9,6 +9,8 @@ from sme_terceirizadas.produto.data.informacao_nutricional import data_informaca
 from sme_terceirizadas.produto.data.protocolo_de_dieta_especial import data_protocolo_de_dieta_especial  # noqa
 from sme_terceirizadas.produto.data.tipo_informacao_nutricional import data_tipo_informacao_nutricional  # noqa
 from sme_terceirizadas.produto.data.produtos import data_produtos
+from sme_terceirizadas.produto.data.produtos_marcas import data_produtos_marcas
+from sme_terceirizadas.produto.data.marcas import data_marcas
 from sme_terceirizadas.produto.models import (
     Fabricante,
     HomologacaoDoProduto,
@@ -72,15 +74,16 @@ def cria_marca():
     Produto.objects.all().delete()
     Marca.objects.all().delete()
     # Cria marcas novas.
-    for _ in progressbar(range(20), 'Marca'):
-        nome = faker.company().split()[0].replace(',', '').split('-')[0]
+    for item in progressbar(data_marcas, 'Marca'):
+        nome = item
         Marca.objects.create(nome=nome)
 
 
 def cria_fabricante():
     Fabricante.objects.all().delete()
-    for _ in progressbar(range(20), 'Fabricante'):
-        nome = fake.company()
+    for item in progressbar(data_marcas, 'Fabricante'):
+        sufixo = choice(['Ltda', 'S.A.', 'M.E.'])
+        nome = f'{item} {sufixo}'
         Fabricante.objects.create(nome=nome)
 
 
@@ -134,6 +137,40 @@ def cria_produto():
 
         produto = Produto.objects.create(
             nome=item,
+            marca=marca,
+            fabricante=fabricante,
+            componentes=componentes,
+            aditivos=aditivos,
+            tipo=tipo,
+            embalagem=embalagem,
+            prazo_validade=prazo_validade,
+            info_armazenamento=info_armazenamento,
+            outras_informacoes=outras_informacoes,
+            numero_registro=numero_registro,
+            porcao=porcao,
+            unidade_caseira=unidade_caseira,
+        )
+        cria_homologacao_do_produto_passo_01(produto)
+
+
+def cria_produto_marca():
+    for item in progressbar(data_produtos_marcas, 'Produto/Marca'):
+        marca = Marca.objects.filter(nome=item[1]).first()
+        fabricante = Fabricante.objects.filter(nome__startswith=item[1]).first()
+
+        componentes = fake.sentence(nb_words=5)
+        aditivos = fake.sentence(nb_words=10)
+        tipo = fake.word()
+        embalagem = fake.word()
+        prazo_validade = faker.date_this_year()
+        info_armazenamento = fake.sentence(nb_words=5)
+        outras_informacoes = fake.sentence(nb_words=10)
+        numero_registro = faker.bothify(letters='ABCDEFGHIJ')
+        porcao = str(randint(50, 300)) + str(choice([' g', ' ml']))
+        unidade_caseira = str(round(randint(1, 5) * random(), 2)).replace('.', ',') + ' unidade'  # noqa
+
+        produto = Produto.objects.create(
+            nome=item[0],
             marca=marca,
             fabricante=fabricante,
             componentes=componentes,
