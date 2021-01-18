e2a2713bdc82509087209b040ec1e8447a0bc6e9
Kelwy <kelwyduarte@gmail.com>
2021-01-18 12:22:13 -0300

    adiciona null true codigo codae

--- sme_terceirizadas/escola/models.py
+++ sme_terceirizadas/escola/models.py
@@ -306,11 +306,12 @@ class Escola(ExportModelOperationsMixin('escola'), Ativavel, TemChaveExterna, Te
         unique=True,
         validators=[MinLengthValidator(6)]
     )
-    codigo_codae = models.CharField(
+    codigo_codae = models.CharField( # noqa DJ01
         'Código CODAE',
         max_length=10,
         blank=True,
-        default=''
+        null=True,
+        default=None
     )
     diretoria_regional = models.ForeignKey(
         DiretoriaRegional,
