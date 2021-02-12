e32889ad7f6004c7c639f1e3ee9589908f61b044
rg3915 <regis42santos@gmail.com>
2021-02-12 06:03:45 -0300

    Lê o arquivo principal. feature 33347-33423
    Instala openpyxl

--- sme_terceirizadas/escola/management/commands/dieta01_analise_dietas_ativas.py
+++ sme_terceirizadas/escola/management/commands/dieta01_analise_dietas_ativas.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from utility.carga_dados.helper import excel_to_list_with_openpyxl
 
 
 class Command(BaseCommand):
@@ -7,5 +8,14 @@ class Command(BaseCommand):
     Valida se o código EOL da unidade educacional da planilha Excel existe na base do SIGPAE.
     """
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--arquivo', '-a',
+            dest='arquivo',
+            help='Informar caminho absoluto do arquivo xlsx.'
+        )
+
     def handle(self, *args, **options):
-        self.stdout.write('OK')
+        arquivo = options['arquivo']
+        items = excel_to_list_with_openpyxl(arquivo, in_memory=False)
+        self.stdout.write(len(items))
