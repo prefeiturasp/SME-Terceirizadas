aef8b163c91698bebcc16add3174b933b4ff6647
rg3915 <regis42santos@gmail.com>
2021-02-12 14:48:35 -0300

    Separando em funções. feature 33347-33423

--- sme_terceirizadas/escola/management/commands/dieta01_analise_dietas_ativas.py
+++ sme_terceirizadas/escola/management/commands/dieta01_analise_dietas_ativas.py
@@ -1,7 +1,31 @@
 import openpyxl
+from datetime import date
 from django.core.management.base import BaseCommand
 from utility.carga_dados.helper import excel_to_list_with_openpyxl, progressbar
 from sme_terceirizadas.escola.models import Escola
+
+DATA = date.today().isoformat().replace('-', '_')
+
+
+def get_codigo_eol(valor):
+    return valor.strip().zfill(6)
+
+
+def escreve_xlsx(codigo_eol_escola_nao_existentes, arquivo_saida):
+    wb = openpyxl.Workbook()
+    ws = wb.create_sheet('Código EOL das Escolas não identificadas no SIGPAE')
+    ws['A1'] = 'codigo_eol_escola'
+    for i, item in enumerate(progressbar(list(codigo_eol_escola_nao_existentes), 'Escrevendo...')):
+        ws[f'A{i+2}'] = str(item)
+    nome = arquivo_saida.split('.')
+    wb.save(f'{nome[0]}_{DATA}.{nome[1]}')
+
+
+def retorna_codigo_eol_escolas_nao_identificadas(items, arquivo_saida):
+    cod_escola_unicos = set([get_codigo_eol(item['CodEscola']) for item in items])
+    codigo_eol_sigpae_lista = Escola.objects.values_list('codigo_eol', flat=True)
+    codigo_eol_escola_nao_existentes = cod_escola_unicos - set(codigo_eol_sigpae_lista)
+    escreve_xlsx(codigo_eol_escola_nao_existentes, arquivo_saida)
 
 
 class Command(BaseCommand):
@@ -19,14 +43,6 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         arquivo = options['arquivo']
+        arquivo_saida = '/home/regis/Downloads/resultado_analise_dietas_ativas.xlsx'
         items = excel_to_list_with_openpyxl(arquivo, in_memory=False)
-        cod_escola_unicos = set([item['CodEscola'].strip().zfill(6) for item in items])
-        codigo_eol_sigpae_lista = Escola.objects.values_list('codigo_eol', flat=True)
-        codigo_eol_escola_nao_existentes = cod_escola_unicos - set(codigo_eol_sigpae_lista)
-        arquivo_saida = '/home/regis/Downloads/resultado_analise_dietas_ativas_2021_02_12.xlsx'
-        wb = openpyxl.Workbook()
-        ws = wb.create_sheet('Código EOL das Escolas não identificadas no SIGPAE')
-        ws['A1'] = 'codigo_eol_escola'
-        for i, item in enumerate(progressbar(list(codigo_eol_escola_nao_existentes), 'Escrevendo...')):
-            ws[f'A{i+2}'] = str(item)
-        wb.save(arquivo_saida)
+        retorna_codigo_eol_escolas_nao_identificadas(items, arquivo_saida)
