671c2d936ed415514a383d8875a7346e77d871e8
rg3915 <regis42santos@gmail.com>
2021-02-12 14:19:54 -0300

    Salva lista com o nome "Código EOL das Escolas não identificadas no SIGPAE no arquivo resultado_analise_dietas_ativas_DATA.xlsx". feature 33347-33423

--- sme_terceirizadas/escola/management/commands/dieta01_analise_dietas_ativas.py
+++ sme_terceirizadas/escola/management/commands/dieta01_analise_dietas_ativas.py
@@ -1,5 +1,6 @@
+import openpyxl
 from django.core.management.base import BaseCommand
-from utility.carga_dados.helper import excel_to_list_with_openpyxl
+from utility.carga_dados.helper import excel_to_list_with_openpyxl, progressbar
 from sme_terceirizadas.escola.models import Escola
 
 
@@ -19,7 +20,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         arquivo = options['arquivo']
         items = excel_to_list_with_openpyxl(arquivo, in_memory=False)
-        cod_escola_unicos = set([item['CodEscola'] for item in items])
+        cod_escola_unicos = set([item['CodEscola'].strip().zfill(6) for item in items])
         codigo_eol_sigpae_lista = Escola.objects.values_list('codigo_eol', flat=True)
         codigo_eol_escola_nao_existentes = cod_escola_unicos - set(codigo_eol_sigpae_lista)
-        print(len(codigo_eol_escola_nao_existentes))
+        arquivo_saida = '/home/regis/Downloads/resultado_analise_dietas_ativas_2021_02_12.xlsx'
+        wb = openpyxl.Workbook()
+        ws = wb.create_sheet('Código EOL das Escolas não identificadas no SIGPAE')
+        ws['A1'] = 'codigo_eol_escola'
+        for i, item in enumerate(progressbar(list(codigo_eol_escola_nao_existentes), 'Escrevendo...')):
+            ws[f'A{i+2}'] = str(item)
+        wb.save(arquivo_saida)
