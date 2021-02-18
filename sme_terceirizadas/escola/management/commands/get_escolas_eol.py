9ab9b55437d0cf473ceef7f75ebe588aabcab85d
rg3915 <regis42santos@gmail.com>
2021-02-18 16:49:39 -0300

    Renomeando os comandos.

--- sme_terceirizadas/escola/management/commands/get_escolas_eol.py
+++ sme_terceirizadas/escola/management/commands/get_escolas_eol.py
@@ -20,8 +20,8 @@ DATA = date.today().isoformat().replace('-', '_')
 home = str(Path.home())
 dict_codigos_escolas = {}
 dict_codigo_aluno_por_codigo_escola = {}
-arquivo = '/home/regis/Downloads/banco_de_dietas_ativas_05.02.xlsx'
-arquivo_codigos_escolas = '/home/regis/Downloads/unidades_da_rede_28.01_.xlsx'
+arquivo = f'{home}/Downloads/banco_de_dietas_ativas_05.02.xlsx'
+arquivo_codigos_escolas = f'{home}/Downloads/unidades_da_rede_28.01_.xlsx'
 
 
 def get_codigo_eol_escola(valor):
@@ -37,14 +37,18 @@ def gera_dict_codigos_escolas(items_codigos_escolas):
         dict_codigos_escolas[str(item['CÓDIGO UNIDADE'])] = str(item['CODIGO EOL'])
 
 
+def grava_codescola_nao_existentes(valor):
+    with open(f'{home}/codescola_nao_existentes.txt', 'a') as f:
+        f.write(f"{valor}\n")
+
+
 def gera_dict_codigo_aluno_por_codigo_escola(items):
     for item in items:
         try:
             codigo_eol_escola = dict_codigos_escolas[item['CodEscola']]
         except Exception as e:
             # Grava os CodEscola não existentes em unidades_da_rede_28.01_.xlsx
-            with open(f'{home}/codescola_nao_existentes.txt', 'a') as f:
-                f.write(f"{item['CodEscola']}\n")
+            grava_codescola_nao_existentes(item['CodEscola'])
 
         cod_eol_aluno = get_codigo_eol_aluno(item['CodEOLAluno'])
         dict_codigo_aluno_por_codigo_escola[cod_eol_aluno] = get_codigo_eol_escola(codigo_eol_escola)
@@ -64,7 +68,7 @@ class EOLException(Exception):
 
 async def get_informacoes_escola_turma_aluno(codigo_eol: str):
     headers = DEFAULT_HEADERS
-    async with httpx.AsyncClient(headers=headers, timeout=120) as client:
+    async with httpx.AsyncClient(headers=headers, timeout=None) as client:
         url = f'{DJANGO_EOL_API_URL}/escola_turma_aluno/{codigo_eol}'
         response = await client.get(url)
         if response.status_code == status.HTTP_200_OK:
@@ -90,33 +94,15 @@ async def main(escolas_da_planilha):
             task_list.append(get_informacoes_escola_turma_aluno(codigo_eol_escola))
         except Exception as e:
             # Grava os CodEscola não existentes em unidades_da_rede_28.01_.xlsx
-            with open(f'{home}/codescola_nao_existentes.txt', 'a') as f:
-                f.write(f"{escola}\n")
+            grava_codescola_nao_existentes(escola)
 
     await asyncio.gather(*task_list)
 
 
 class Command(BaseCommand):
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            '--start_interval', '-st',
-            dest='start_interval',
-            default=None,
-            help='start_interval.'
-        )
-        parser.add_argument(
-            '--end_interval', '-end',
-            dest='end_interval',
-            default=None,
-            help='end_interval.'
-        )
-
     def handle(self, *args, **options):
         start_time = time.monotonic()
-
-        start_interval = options['start_interval']
-        end_interval = options['end_interval']
 
         items = excel_to_list_with_openpyxl(arquivo, in_memory=False)
         items_codigos_escolas = excel_to_list_with_openpyxl(arquivo_codigos_escolas, in_memory=False)
@@ -125,12 +111,18 @@ class Command(BaseCommand):
         gera_dict_codigo_aluno_por_codigo_escola(items)
 
         # A partir da planilha, pegar todas as escolas únicas "escolas_da_planilha"
-        escolas_da_planilha = list(get_escolas_unicas(items))[int(start_interval):int(end_interval)]
+        escolas_da_planilha = list(get_escolas_unicas(items))
         print('Escolas:', len(escolas_da_planilha))
 
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(main(escolas_da_planilha))
+        # Particiona os intervalos da lista para fazer apenas 100 requisições por vez.
+        for i in range(0, len(escolas_da_planilha) + 1, 100):
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(main(escolas_da_planilha[i:i+100]))
+            print('Waiting...')
+            time.sleep(10)
+
         time_taken = round(time.monotonic() - start_time, 2)
         print(f"Time Taken:{time_taken}")
+        # Remover
         with open('/tmp/tempo.txt', 'a') as f:
             f.write(f'{len(escolas_da_planilha)} escolas em {time_taken} s\n')
