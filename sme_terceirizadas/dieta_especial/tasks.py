f401d27f381bf012d2d65582a32019659c1e0677
rg3915 <regis42santos@gmail.com>
2021-02-24 12:08:47 -0300

    Fix to path

--- sme_terceirizadas/dieta_especial/tasks.py
+++ sme_terceirizadas/dieta_especial/tasks.py
@@ -22,6 +22,6 @@ def processa_dietas_especiais_task():
 @shared_task()
 def get_escolas_task():
     obj = PlanilhaDietasAtivas.objects.first()  # Tem um problema aqui, e se selecionar outro arquivo?
-    arquivo = obj.arquivo
-    arquivo_unidades_da_rede = obj.arquivo_unidades_da_rede
+    arquivo = obj.arquivo.path
+    arquivo_unidades_da_rede = obj.arquivo_unidades_da_rede.path
     get_escolas(arquivo, arquivo_unidades_da_rede, obj.tempfile, in_memory=True)
