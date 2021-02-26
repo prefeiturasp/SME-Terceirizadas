a3ac0275b43d5f138d23279e219072a5901baacc
Kelwy <kelwyduarte@gmail.com>
2021-02-26 18:46:17 -0300

    Fix isort

--- sme_terceirizadas/logistica/models/__init__.py
+++ sme_terceirizadas/logistica/models/__init__.py
@@ -2,5 +2,5 @@ from .alimento import Alimento  # noqa
 from .alimento import Embalagem  # noqa
 from .alimento import TipoEmbalagem  # noqa
 from .guia import Guia  # noqa
-from .solicitacao import SolicitacaoRemessa  # noqa
 from .solicitacao import SolicitacaoDeAlteracaoRequisicao  # noqa
+from .solicitacao import SolicitacaoRemessa  # noqa
