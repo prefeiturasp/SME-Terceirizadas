2c9a67ae61181f68a7f3b9ab62e86bff12609dbd
Kelwy <kelwyduarte@gmail.com>
2021-03-03 18:18:33 -0300

    Abstrai metodo geral de exportação e cria metodo da visao dilog

--- sme_terceirizadas/logistica/api/services/exporta_para_excel.py
+++ sme_terceirizadas/logistica/api/services/exporta_para_excel.py
@@ -1,14 +1,35 @@
 from io import BytesIO
 
 from openpyxl import Workbook
-from openpyxl.styles import Font
+from openpyxl.styles import Font, Border, Side, PatternFill
 from openpyxl.writer.excel import save_virtual_workbook
 
 
 class RequisicoesExcelService(object):
+    DEFAULT_BORDER = Border(right=Side(border_style='thin', color='24292E'),
+                            left=Side(border_style='thin', color='24292E'),
+                            top=Side(border_style='thin', color='24292E'),
+                            bottom=Side(border_style='thin', color='24292E'))
 
     @classmethod
-    def exportar(cls, requisioes):
+    def aplicar_estilo_padrao(cls, ws, count_data, count_fields):
+
+        for linha in range(1, (count_data + 2)):
+            for coluna in range(1, (count_fields + 1)):
+                celula = ws.cell(row=linha, column=coluna)
+                celula.border = cls.DEFAULT_BORDER
+                if linha == 1:
+                    celula.fill = PatternFill(fill_type='solid', fgColor='198459')
+
+        # Ajuste automatico do tamanho das colunas
+        for column_cells in ws.columns:
+            unmerged_cells = list(
+                filter(lambda cell_to_check: cell_to_check.coordinate not in ws.merged_cells, column_cells))
+            length = max(len(str(cell.value)) for cell in unmerged_cells)
+            ws.column_dimensions[unmerged_cells[0].column_letter].width = length * 1.2
+
+    @classmethod
+    def exportar_visao_distribuidor(cls, requisioes):
 
         cabecalho = ['Número da Requisição', 'Status da Requisição', 'Quantidade Total de Guias', 'Número da Guia',
                      'Data de Entrega', 'Código CODAE da UE', 'Nome UE', 'Endereço UE', 'Número UE', 'Bairro UE',
@@ -18,6 +39,9 @@ class RequisicoesExcelService(object):
                      'Descrição Embalagem Fracionada', 'Capacidade da Embalagem Fracionada',
                      'Unidade de Medida da Embalagem Fracionada', 'Quantidade de Volumes da Embalagem Fracionada']
 
+        count_fields = len(cabecalho)
+        count_data = requisioes.count()
+
         wb = Workbook()
         ws = wb.active
         ws.title = 'Visão Analítica Abastecimento'
@@ -25,7 +49,7 @@ class RequisicoesExcelService(object):
         for ind, title in enumerate(cabecalho, 1):
             celula = ws.cell(row=1, column=ind)
             celula.value = title
-            celula.font = Font(size="13", bold=True)
+            celula.font = Font(size="13", bold=True, color='00FFFFFF')
 
         for ind, requisicao in enumerate(requisioes, 2):
             ws.cell(row=ind, column=1, value=requisicao['numero_solicitacao'])
@@ -57,13 +81,67 @@ class RequisicoesExcelService(object):
                 ws.cell(row=ind, column=25, value=requisicao['guias__alimentos__embalagens__unidade_medida'])
                 ws.cell(row=ind, column=26, value=requisicao['guias__alimentos__embalagens__qtd_volume'])
 
-        # auto adjusting column width of cells
-        for column_cells in ws.columns:
-            unmerged_cells = list(
-                filter(lambda cell_to_check: cell_to_check.coordinate not in ws.merged_cells, column_cells))
-            length = max(len(str(cell.value)) for cell in unmerged_cells)
-            ws.column_dimensions[unmerged_cells[0].column_letter].width = length * 1.2
+        cls.aplicar_estilo_padrao(ws, count_data, count_fields)
+        arquivo = BytesIO(save_virtual_workbook(wb))
+        filename = 'visao-consolidada.xlsx'
 
+        return {'arquivo': arquivo, 'filename': filename}
+
+    @classmethod
+    def exportar_visao_dilog(cls, requisioes):
+
+        cabecalho = ['Nome do Distribuidor', 'Número da Requisição', 'Status da Requisição',
+                     'Quantidade Total de Guias', 'Número da Guia', 'Data de Entrega', 'Código EOL da UE',
+                     'Telefone UE', 'Nome do Alimento', 'Código SUPRI', 'Código PAPA', 'Descrição Embalagem Fechada',
+                     'Capacidade da Embalagem Fechada', 'Unidade de Medida da Embalagem Fechada',
+                     'Quantidade de Volumes da Embalagem Fechada', 'Descrição Embalagem Fracionada',
+                     'Capacidade da Embalagem Fracionada', 'Unidade de Medida da Embalagem Fracionada',
+                     'Quantidade de Volumes da Embalagem Fracionada', 'Status da Guia']
+
+        count_fields = len(cabecalho)
+        count_data = requisioes.count()
+
+        wb = Workbook()
+        ws = wb.active
+        ws.title = 'Visão Analítica Abastecimento'
+
+        for ind, title in enumerate(cabecalho, 1):
+            celula = ws.cell(row=1, column=ind)
+            celula.value = title
+            celula.font = Font(size="13", bold=True, color='00FFFFFF')
+
+        for ind, requisicao in enumerate(requisioes, 2):
+            ws.cell(row=ind, column=1, value=requisicao['nome_distribuidor'])
+            ws.cell(row=ind, column=1, value=requisicao['numero_solicitacao'])
+            ws.cell(row=ind, column=2, value=requisicao['status_requisicao'])
+            ws.cell(row=ind, column=3, value=requisicao['quantidade_total_guias'])
+            ws.cell(row=ind, column=4, value=requisicao['guias__numero_guia'])
+            ws.cell(row=ind, column=5, value=requisicao['guias__data_entrega'])
+            ws.cell(row=ind, column=6, value=requisicao['guias__codigo_unidade'])
+            ws.cell(row=ind, column=7, value=requisicao['guias__nome_unidade'])
+            ws.cell(row=ind, column=8, value=requisicao['guias__endereco_unidade'])
+            ws.cell(row=ind, column=9, value=int(requisicao['guias__numero_unidade']))
+            ws.cell(row=ind, column=10, value=requisicao['guias__bairro_unidade'])
+            ws.cell(row=ind, column=11, value=requisicao['guias__cep_unidade'])
+            ws.cell(row=ind, column=12, value=requisicao['guias__cidade_unidade'])
+            ws.cell(row=ind, column=13, value=requisicao['guias__estado_unidade'])
+            ws.cell(row=ind, column=14, value=requisicao['guias__contato_unidade'])
+            ws.cell(row=ind, column=15, value=requisicao['guias__telefone_unidade'])
+            ws.cell(row=ind, column=16, value=requisicao['guias__alimentos__nome_alimento'])
+            ws.cell(row=ind, column=17, value=requisicao['guias__alimentos__codigo_suprimento'])
+            ws.cell(row=ind, column=18, value=requisicao['guias__alimentos__codigo_papa'])
+            if requisicao['guias__alimentos__embalagens__tipo_embalagem'] == 'FECHADA':
+                ws.cell(row=ind, column=19, value=requisicao['guias__alimentos__embalagens__descricao_embalagem'])
+                ws.cell(row=ind, column=20, value=requisicao['guias__alimentos__embalagens__capacidade_embalagem'])
+                ws.cell(row=ind, column=21, value=requisicao['guias__alimentos__embalagens__unidade_medida'])
+                ws.cell(row=ind, column=22, value=requisicao['guias__alimentos__embalagens__qtd_volume'])
+            else:
+                ws.cell(row=ind, column=23, value=requisicao['guias__alimentos__embalagens__descricao_embalagem'])
+                ws.cell(row=ind, column=24, value=requisicao['guias__alimentos__embalagens__capacidade_embalagem'])
+                ws.cell(row=ind, column=25, value=requisicao['guias__alimentos__embalagens__unidade_medida'])
+                ws.cell(row=ind, column=26, value=requisicao['guias__alimentos__embalagens__qtd_volume'])
+
+        cls.aplicar_estilo_padrao(ws, count_data, count_fields)
         arquivo = BytesIO(save_virtual_workbook(wb))
         filename = 'visao-consolidada.xlsx'
 
