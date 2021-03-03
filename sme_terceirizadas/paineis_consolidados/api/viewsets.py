1501d07f7f3b52530b78b1198d685e89b7c67661
rg3915 <regis42santos@gmail.com>
2021-03-03 14:18:09 -0300

    fix: Corrige ao clicar em ver mais para Dietas Inativas Temporariamente. Perfil Terceirazada. feature 34563-34830

--- sme_terceirizadas/paineis_consolidados/api/viewsets.py
+++ sme_terceirizadas/paineis_consolidados/api/viewsets.py
@@ -742,6 +742,16 @@ class TerceirizadaSolicitacoesViewSet(SolicitacoesViewSet):
             terceirizada_uuid=terceirizada_uuid)
         return self._retorno_base(query_set)
 
+    @action(
+        detail=False,
+        methods=['GET'],
+        url_path=f'{INATIVAS_TEMPORARIAMENTE_DIETA_ESPECIAL}/{FILTRO_TERCEIRIZADA_UUID}'
+    )
+    def inativas_temporariamente_dieta_especial(self, request, terceirizada_uuid=None):
+        query_set = SolicitacoesTerceirizada.get_inativas_temporariamente_dieta_especial(
+            terceirizada_uuid=terceirizada_uuid)
+        return self._retorno_base(query_set)
+
     @action(detail=False, methods=['GET'], url_path=f'{QUESTIONAMENTOS}/{FILTRO_TERCEIRIZADA_UUID}')
     def questionamentos(self, request, terceirizada_uuid=None):
         query_set = SolicitacoesTerceirizada.get_questionamentos(
