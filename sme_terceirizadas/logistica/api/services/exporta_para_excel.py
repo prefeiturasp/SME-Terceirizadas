df77d7757d4c0ce7ed92bc6eeb443909c66bf3d8
Kelwy <kelwyduarte@gmail.com>
2021-03-04 18:54:32 -0300

    Ajusta celulas com novos valores da queryset

--- sme_terceirizadas/logistica/api/services/exporta_para_excel.py
+++ sme_terceirizadas/logistica/api/services/exporta_para_excel.py
@@ -22,9 +22,9 @@ class RequisicoesExcelService(object):
                     celula.fill = PatternFill(fill_type='solid', fgColor='198459')
 
         # Ajuste automatico do tamanho das colunas
-        for column_cells in ws.columns:
+        for colunas in ws.columns:
             unmerged_cells = list(
-                filter(lambda cell_to_check: cell_to_check.coordinate not in ws.merged_cells, column_cells))
+                filter(lambda cell_to_check: cell_to_check.coordinate not in ws.merged_cells, colunas))
             length = max(len(str(cell.value)) for cell in unmerged_cells)
             ws.column_dimensions[unmerged_cells[0].column_letter].width = length * 1.2
 
@@ -92,11 +92,13 @@ class RequisicoesExcelService(object):
 
         cabecalho = ['Nome do Distribuidor', 'Número da Requisição', 'Status da Requisição',
                      'Quantidade Total de Guias', 'Número da Guia', 'Data de Entrega', 'Código EOL da UE',
-                     'Telefone UE', 'Nome do Alimento', 'Código SUPRI', 'Código PAPA', 'Descrição Embalagem Fechada',
-                     'Capacidade da Embalagem Fechada', 'Unidade de Medida da Embalagem Fechada',
-                     'Quantidade de Volumes da Embalagem Fechada', 'Descrição Embalagem Fracionada',
-                     'Capacidade da Embalagem Fracionada', 'Unidade de Medida da Embalagem Fracionada',
-                     'Quantidade de Volumes da Embalagem Fracionada', 'Status da Guia']
+                     'Código CODAE da UE', 'Nome UE', 'Endereço UE', 'Número UE', 'Bairro UE', 'CEP UE', 'Cidade UE',
+                     'Estado UE', 'Contato de Entrega', 'Telefone UE', 'Nome do Alimento', 'Código SUPRI',
+                     'Código PAPA', 'Descrição Embalagem Fechada', 'Capacidade da Embalagem Fechada',
+                     'Unidade de Medida da Embalagem Fechada', 'Quantidade de Volumes da Embalagem Fechada',
+                     'Descrição Embalagem Fracionada', 'Capacidade da Embalagem Fracionada',
+                     'Unidade de Medida da Embalagem Fracionada', 'Quantidade de Volumes da Embalagem Fracionada',
+                     'Status da Guia']
 
         count_fields = len(cabecalho)
         count_data = requisioes.count()
@@ -111,35 +113,37 @@ class RequisicoesExcelService(object):
             celula.font = Font(size="13", bold=True, color='00FFFFFF')
 
         for ind, requisicao in enumerate(requisioes, 2):
-            ws.cell(row=ind, column=1, value=requisicao['nome_distribuidor'])
-            ws.cell(row=ind, column=1, value=requisicao['numero_solicitacao'])
-            ws.cell(row=ind, column=2, value=requisicao['status_requisicao'])
-            ws.cell(row=ind, column=3, value=requisicao['quantidade_total_guias'])
-            ws.cell(row=ind, column=4, value=requisicao['guias__numero_guia'])
-            ws.cell(row=ind, column=5, value=requisicao['guias__data_entrega'])
-            ws.cell(row=ind, column=6, value=requisicao['guias__codigo_unidade'])
-            ws.cell(row=ind, column=7, value=requisicao['guias__nome_unidade'])
-            ws.cell(row=ind, column=8, value=requisicao['guias__endereco_unidade'])
-            ws.cell(row=ind, column=9, value=int(requisicao['guias__numero_unidade']))
-            ws.cell(row=ind, column=10, value=requisicao['guias__bairro_unidade'])
-            ws.cell(row=ind, column=11, value=requisicao['guias__cep_unidade'])
-            ws.cell(row=ind, column=12, value=requisicao['guias__cidade_unidade'])
-            ws.cell(row=ind, column=13, value=requisicao['guias__estado_unidade'])
-            ws.cell(row=ind, column=14, value=requisicao['guias__contato_unidade'])
-            ws.cell(row=ind, column=15, value=requisicao['guias__telefone_unidade'])
-            ws.cell(row=ind, column=16, value=requisicao['guias__alimentos__nome_alimento'])
-            ws.cell(row=ind, column=17, value=requisicao['guias__alimentos__codigo_suprimento'])
-            ws.cell(row=ind, column=18, value=requisicao['guias__alimentos__codigo_papa'])
+            ws.cell(row=ind, column=1, value=requisicao['distribuidor__nome_fantasia'])
+            ws.cell(row=ind, column=2, value=requisicao['numero_solicitacao'])
+            ws.cell(row=ind, column=3, value=requisicao['status_requisicao'])
+            ws.cell(row=ind, column=4, value=requisicao['quantidade_total_guias'])
+            ws.cell(row=ind, column=5, value=requisicao['guias__numero_guia'])
+            ws.cell(row=ind, column=6, value=requisicao['guias__data_entrega'])
+            ws.cell(row=ind, column=7, value=requisicao['codigo_eol_unidade'])
+            ws.cell(row=ind, column=8, value=requisicao['guias__codigo_unidade'])
+            ws.cell(row=ind, column=9, value=requisicao['guias__nome_unidade'])
+            ws.cell(row=ind, column=10, value=requisicao['guias__endereco_unidade'])
+            ws.cell(row=ind, column=11, value=int(requisicao['guias__numero_unidade']))
+            ws.cell(row=ind, column=12, value=requisicao['guias__bairro_unidade'])
+            ws.cell(row=ind, column=13, value=requisicao['guias__cep_unidade'])
+            ws.cell(row=ind, column=14, value=requisicao['guias__cidade_unidade'])
+            ws.cell(row=ind, column=15, value=requisicao['guias__estado_unidade'])
+            ws.cell(row=ind, column=16, value=requisicao['guias__contato_unidade'])
+            ws.cell(row=ind, column=17, value=requisicao['guias__telefone_unidade'])
+            ws.cell(row=ind, column=18, value=requisicao['guias__alimentos__nome_alimento'])
+            ws.cell(row=ind, column=19, value=requisicao['guias__alimentos__codigo_suprimento'])
+            ws.cell(row=ind, column=20, value=requisicao['guias__alimentos__codigo_papa'])
             if requisicao['guias__alimentos__embalagens__tipo_embalagem'] == 'FECHADA':
-                ws.cell(row=ind, column=19, value=requisicao['guias__alimentos__embalagens__descricao_embalagem'])
-                ws.cell(row=ind, column=20, value=requisicao['guias__alimentos__embalagens__capacidade_embalagem'])
-                ws.cell(row=ind, column=21, value=requisicao['guias__alimentos__embalagens__unidade_medida'])
-                ws.cell(row=ind, column=22, value=requisicao['guias__alimentos__embalagens__qtd_volume'])
+                ws.cell(row=ind, column=21, value=requisicao['guias__alimentos__embalagens__descricao_embalagem'])
+                ws.cell(row=ind, column=22, value=requisicao['guias__alimentos__embalagens__capacidade_embalagem'])
+                ws.cell(row=ind, column=23, value=requisicao['guias__alimentos__embalagens__unidade_medida'])
+                ws.cell(row=ind, column=24, value=requisicao['guias__alimentos__embalagens__qtd_volume'])
             else:
-                ws.cell(row=ind, column=23, value=requisicao['guias__alimentos__embalagens__descricao_embalagem'])
-                ws.cell(row=ind, column=24, value=requisicao['guias__alimentos__embalagens__capacidade_embalagem'])
-                ws.cell(row=ind, column=25, value=requisicao['guias__alimentos__embalagens__unidade_medida'])
-                ws.cell(row=ind, column=26, value=requisicao['guias__alimentos__embalagens__qtd_volume'])
+                ws.cell(row=ind, column=25, value=requisicao['guias__alimentos__embalagens__descricao_embalagem'])
+                ws.cell(row=ind, column=26, value=requisicao['guias__alimentos__embalagens__capacidade_embalagem'])
+                ws.cell(row=ind, column=27, value=requisicao['guias__alimentos__embalagens__unidade_medida'])
+                ws.cell(row=ind, column=28, value=requisicao['guias__alimentos__embalagens__qtd_volume'])
+            ws.cell(row=ind, column=29, value=requisicao['guias__status'])
 
         cls.aplicar_estilo_padrao(ws, count_data, count_fields)
         arquivo = BytesIO(save_virtual_workbook(wb))
