5708e7dcfdbf14d2c68dbba95311ace11e945927
rg3915 <regis42santos@gmail.com>
2021-03-22 18:44:09 -0300

    Fix flake8

--- sme_terceirizadas/dieta_especial/utils.py
+++ sme_terceirizadas/dieta_especial/utils.py
@@ -1,5 +1,6 @@
 from datetime import date
 from time import sleep
+
 from django.contrib.contenttypes.models import ContentType
 from django.template.loader import render_to_string
 from rest_framework.pagination import PageNumberPagination
