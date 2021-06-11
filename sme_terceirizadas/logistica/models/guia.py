f30e2711923b659bd3b7f6548eee029fe6cc62e9
Kelwy Oliveira <kelwyduarte@gmail.com>
2021-06-11 19:10:50 -0300

    Adiciona referencia a reposicao no modelo de conferencia

--- sme_terceirizadas/logistica/models/guia.py
+++ sme_terceirizadas/logistica/models/guia.py
@@ -83,6 +83,7 @@ class ConferenciaGuia(ModeloBase, CriadoPor):
     hora_recebimento = models.TimeField('Hora do recebimento')
     nome_motorista = models.CharField('Nome do motorista', max_length=100)
     placa_veiculo = models.CharField('Placa do veículo', max_length=7)
+    eh_reposicao = models.BooleanField(default=False)
 
     def __str__(self):
         return f'Conferência da guia {self.guia.numero_guia}'
