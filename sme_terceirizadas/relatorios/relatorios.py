c00a222eb2b2f0756cd9e605545d24b9d89f0645
Kelwy Oliveira <kelwyduarte@gmail.com>
2021-07-26 10:40:18 -0300

    Finaliza endpoint do relatório base

--- sme_terceirizadas/relatorios/relatorios.py
+++ sme_terceirizadas/relatorios/relatorios.py
@@ -158,7 +158,7 @@ def relatorio_guia_de_remessa(guias):
             else:
                 break
         inicio = 0
-    html_string = render_to_string('logistica/guia_distribuidor/index.html', {'pages': pages})
+    html_string = render_to_string('logistica/guia_remessa/relatorio_guia.html', {'pages': pages})
     data_arquivo = datetime.date.today().strftime('%d/%m/%Y')
 
     return html_to_pdf_response(html_string.replace('dt_file', data_arquivo), 'guia_de_remessa.pdf')
