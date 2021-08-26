06e2df80c9861aa03fa5571ba2b60ea0f316018f
Kelwy Oliveira <kelwyduarte@gmail.com>
2021-08-26 16:37:54 -0300

    Fix teste unitário

--- sme_terceirizadas/logistica/__tests__/test_guia/test_model.py
+++ sme_terceirizadas/logistica/__tests__/test_guia/test_model.py
@@ -21,17 +21,17 @@ def test_meta_modelo(guia):
     assert guia._meta.verbose_name_plural == 'Guias de Remessas'
 
 
-def test_instance_conferencia_guia_model(conferencia_guia):
-    assert isinstance(conferencia_guia, ConferenciaGuia)
+def test_instance_conferencia_guia_model(conferencia_guia_normal):
+    assert isinstance(conferencia_guia_normal, ConferenciaGuia)
 
 
-def test_srt_conferencia_guiamodel(conferencia_guia):
-    assert conferencia_guia.__str__() == 'Conferência da guia 987654'
+def test_srt_conferencia_guiamodel(conferencia_guia_normal):
+    assert conferencia_guia_normal.__str__() == 'Conferência da guia 987654'
 
 
-def test_meta_conferencia_guiamodelo(conferencia_guia):
-    assert conferencia_guia._meta.verbose_name == 'Conferência da Guia de Remessa'
-    assert conferencia_guia._meta.verbose_name_plural == 'Conferência das Guias de Remessas'
+def test_meta_conferencia_guiamodelo(conferencia_guia_normal):
+    assert conferencia_guia_normal._meta.verbose_name == 'Conferência da Guia de Remessa'
+    assert conferencia_guia_normal._meta.verbose_name_plural == 'Conferência das Guias de Remessas'
 
 
 def test_instance_insucesso_entrega_guia_model(insucesso_entrega_guia):
