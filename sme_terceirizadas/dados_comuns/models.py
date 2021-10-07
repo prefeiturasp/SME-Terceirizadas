eba8daf883b285c7885c347b73102cdb7fa2325f
Kelwy Oliveira <kelwyduarte@gmail.com>
2021-10-07 17:33:56 -0300

    Cria novos campos

--- sme_terceirizadas/dados_comuns/models.py
+++ sme_terceirizadas/dados_comuns/models.py
@@ -228,6 +228,7 @@ class Endereco(ExportModelOperationsMixin('endereco'), models.Model):
 
 
 class Contato(ExportModelOperationsMixin('contato'), models.Model):
+    nome = models.CharField('Nome', max_length=160, blank=True)
     telefone = models.CharField(
         max_length=13,
         validators=[MinLengthValidator(8)],
