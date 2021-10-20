4229e868a5d89a0097261cf1640213ef6a11bc2a
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2021-10-20 13:06:33 -0300

    Feature: correção no script de carga de dados impactado com a modificação do modelo

--- utility/carga_dados/kit_lanche/importa_dados.py
+++ utility/carga_dados/kit_lanche/importa_dados.py
@@ -14,12 +14,7 @@ def cria_kit_lanche_item():
 
 
 def cria_kit_lanche():
-    # Valores randomicos
-    items = list(ItemKitLanche.objects.all())
     # Deleta kits existentes
     KitLanche.objects.all().delete()
     for i in progressbar(range(1, 11), 'Kit Lanche'):
-        kit_amostra = sample(items, 3)
         kit_lanche = KitLanche.objects.create(nome=f'Kit {i}')
-        for kit in kit_amostra:
-            kit_lanche.itens.add(kit)
