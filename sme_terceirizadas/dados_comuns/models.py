0955db3cc34cb8838c60e0081649381d67151854
anderson89marques <andersonoanjo18@gmail.com>
2021-10-26 18:03:02 -0300

    Ajustes nos camandos para atualizar escolas e alunos

--- sme_terceirizadas/dados_comuns/models.py
+++ sme_terceirizadas/dados_comuns/models.py
@@ -6,7 +6,9 @@ from django.db import models
 from django_prometheus.models import ExportModelOperationsMixin
 
 
-class LogSolicitacoesUsuario(ExportModelOperationsMixin('log_solicitacoes'), models.Model):
+class LogSolicitacoesUsuario(
+    ExportModelOperationsMixin('log_solicitacoes'), models.Model
+):
     """Eventos de dados importantes para acompanhamento.
 
     Ex.: Fulano X  executou a atividade Y no objeto W no dia DDDDMMAA
@@ -21,7 +23,6 @@ class LogSolicitacoesUsuario(ExportModelOperationsMixin('log_solicitacoes'), mod
         # ESPECIFICA DO PARTINDO DA DRE
         CODAE_PEDIU_REVISAO,
         DRE_REVISOU,
-
         # ESPECIFICA DO PARTINDO DA ESCOLA
         DRE_VALIDOU,
         DRE_PEDIU_REVISAO,
@@ -29,18 +30,15 @@ class LogSolicitacoesUsuario(ExportModelOperationsMixin('log_solicitacoes'), mod
         ESCOLA_REVISOU,
         CODAE_QUESTIONOU,
         TERCEIRIZADA_RESPONDEU_QUESTIONAMENTO,
-
         # "BURLADO DO FLUXO", PODE SER CHAMADO A QUALQUER MOMENTO COM AS DEVIDAS RESTRIÇÕES
         ESCOLA_CANCELOU,
         DRE_CANCELOU,
-
         # ESPECIFICA DIETA ESPECIAL
         INICIO_FLUXO_INATIVACAO,
         CODAE_AUTORIZOU_INATIVACAO,
         CODAE_NEGOU_INATIVACAO,
         TERCEIRIZADA_TOMOU_CIENCIA_INATIVACAO,
         TERMINADA_AUTOMATICAMENTE_SISTEMA,
-
         # ESPECIFICA HOMOLOGACAO DE PRODUTO
         CODAE_PENDENTE_HOMOLOGACAO,
         CODAE_HOMOLOGADO,
@@ -53,7 +51,6 @@ class LogSolicitacoesUsuario(ExportModelOperationsMixin('log_solicitacoes'), mod
         CODAE_AUTORIZOU_RECLAMACAO,
         INATIVA,
         TERCEIRIZADA_CANCELOU_SOLICITACAO_HOMOLOGACAO,
-
         # ESPECIFICA RECLAMAÇÃO DE PRODUTO
         TERCEIRIZADA_RESPONDEU_RECLAMACAO,
         TERCEIRIZADA_RESPONDEU_ANALISE_SENSORIAL,
@@ -64,21 +61,17 @@ class LogSolicitacoesUsuario(ExportModelOperationsMixin('log_solicitacoes'), mod
         CODAE_RECUSOU_RECLAMACAO,
         CODAE_QUESTIONOU_TERCEIRIZADA,
         CODAE_RESPONDEU_RECLAMACAO,
-
         # ESPECIFICA SOLICITAÇÂO CADASTRO PRODUTO
         TERCEIRIZADA_ATENDE_SOLICITACAO_CADASTRO,
-
         # ESPECIFICA SOLICITAÇÃO DE REMESSA
         INICIO_FLUXO_SOLICITACAO,
         DILOG_ENVIA_SOLICITACAO,
         DISTRIBUIDOR_CONFIRMA_SOLICITACAO,
         DISTRIBUIDOR_SOLICITA_ALTERACAO_SOLICITACAO,
         PAPA_CANCELA_SOLICITACAO,
-
         # ESPECIFICA SOLICITAÇÃO DE ALTERACAO
         DILOG_ACEITA_ALTERACAO,
         DILOG_NEGA_ALTERACAO,
-
         CANCELADO_ALUNO_MUDOU_ESCOLA,
         CANCELADO_ALUNO_NAO_PERTENCE_REDE,
     ) = range(50)
@@ -98,41 +91,80 @@ class LogSolicitacoesUsuario(ExportModelOperationsMixin('log_solicitacoes'), mod
         (ESCOLA_CANCELOU, 'Escola cancelou'),
         (DRE_CANCELOU, 'DRE cancelou'),
         (CODAE_QUESTIONOU, 'Questionamento pela CODAE'),
-        (TERCEIRIZADA_RESPONDEU_QUESTIONAMENTO, 'Terceirizada respondeu questionamento'),  # noqa
+        (
+            TERCEIRIZADA_RESPONDEU_QUESTIONAMENTO,
+            'Terceirizada respondeu questionamento',
+        ),  # noqa
         (INICIO_FLUXO_INATIVACAO, 'Escola solicitou inativação'),
         (CODAE_AUTORIZOU_INATIVACAO, 'CODAE autorizou inativação'),
         (CODAE_NEGOU_INATIVACAO, 'CODAE negou inativação'),
-        (TERCEIRIZADA_TOMOU_CIENCIA_INATIVACAO, 'Terceirizada tomou ciência da inativação'),  # noqa
-        (TERMINADA_AUTOMATICAMENTE_SISTEMA, 'Cancelada por atingir data de término'),  # noqa
+        (
+            TERCEIRIZADA_TOMOU_CIENCIA_INATIVACAO,
+            'Terceirizada tomou ciência da inativação',
+        ),  # noqa
+        (
+            TERMINADA_AUTOMATICAMENTE_SISTEMA,
+            'Cancelada por atingir data de término',
+        ),  # noqa
         (CODAE_PENDENTE_HOMOLOGACAO, 'Pendente homologação da CODAE'),
         (CODAE_HOMOLOGADO, 'CODAE homologou'),
         (CODAE_NAO_HOMOLOGADO, 'CODAE não homologou'),
         (CODAE_PEDIU_ANALISE_SENSORIAL, 'CODAE pediu análise sensorial'),
         (TERCEIRIZADA_CANCELOU, 'Terceirizada cancelou homologação'),
         (INATIVA, 'Homologação inativa'),
-        (TERCEIRIZADA_CANCELOU_SOLICITACAO_HOMOLOGACAO, 'Terceirizada cancelou solicitação de homologação de produto'),
+        (
+            TERCEIRIZADA_CANCELOU_SOLICITACAO_HOMOLOGACAO,
+            'Terceirizada cancelou solicitação de homologação de produto',
+        ),
         (CODAE_SUSPENDEU, 'CODAE suspendeu o produto'),
-        (ESCOLA_OU_NUTRICIONISTA_RECLAMOU, 'Escola/Nutricionista reclamou do produto'),  # noqa
+        (
+            ESCOLA_OU_NUTRICIONISTA_RECLAMOU,
+            'Escola/Nutricionista reclamou do produto',
+        ),  # noqa
         (CODAE_PEDIU_ANALISE_RECLAMACAO, 'CODAE pediu análise da reclamação'),
         (CODAE_AUTORIZOU_RECLAMACAO, 'CODAE autorizou reclamação'),
         (CODAE_RECUSOU_RECLAMACAO, 'CODAE recusou reclamação'),
-        (CODAE_QUESTIONOU_TERCEIRIZADA, 'CODAE questionou terceirizada sobre reclamação'),  # noqa
+        (
+            CODAE_QUESTIONOU_TERCEIRIZADA,
+            'CODAE questionou terceirizada sobre reclamação',
+        ),  # noqa
         (CODAE_QUESTIONOU_UE, 'CODAE questionou U.E. sobre reclamação'),  # noqa
         (CODAE_RESPONDEU_RECLAMACAO, 'CODAE respondeu ao reclamante da reclamação'),
-        (CODAE_QUESTIONOU_NUTRISUPERVISOR, 'CODAE questionou nutrisupervisor sobre reclamação'),
+        (
+            CODAE_QUESTIONOU_NUTRISUPERVISOR,
+            'CODAE questionou nutrisupervisor sobre reclamação',
+        ),
         (TERCEIRIZADA_RESPONDEU_RECLAMACAO, 'Terceirizada respondeu a reclamação'),
         (UE_RESPONDEU_RECLAMACAO, 'U.E. respondeu a reclamação'),
-        (NUTRISUPERVISOR_RESPONDEU_RECLAMACAO, 'Nutrisupervisor respondeu a reclamação'),
-        (TERCEIRIZADA_RESPONDEU_ANALISE_SENSORIAL, 'Terceirizada respondeu a análise'),  # noqa
+        (
+            NUTRISUPERVISOR_RESPONDEU_RECLAMACAO,
+            'Nutrisupervisor respondeu a reclamação',
+        ),
+        (
+            TERCEIRIZADA_RESPONDEU_ANALISE_SENSORIAL,
+            'Terceirizada respondeu a análise',
+        ),  # noqa
         (INICIO_FLUXO_SOLICITACAO, 'Papa enviou a requisição'),
         (DILOG_ENVIA_SOLICITACAO, 'Dilog Enviou a requisição'),
-        (DISTRIBUIDOR_CONFIRMA_SOLICITACAO, 'Distribuidor confirmou requisição'),  # noqa
-        (DISTRIBUIDOR_SOLICITA_ALTERACAO_SOLICITACAO, 'Distribuidor pede alteração da requisição'),  # noqa
+        (
+            DISTRIBUIDOR_CONFIRMA_SOLICITACAO,
+            'Distribuidor confirmou requisição',
+        ),  # noqa
+        (
+            DISTRIBUIDOR_SOLICITA_ALTERACAO_SOLICITACAO,
+            'Distribuidor pede alteração da requisição',
+        ),  # noqa
         (PAPA_CANCELA_SOLICITACAO, 'Papa cancelou a requisição'),
         (DILOG_ACEITA_ALTERACAO, 'Dilog Aceita Alteração'),
         (DILOG_NEGA_ALTERACAO, 'Dilog Nega Alteração'),
-        (CANCELADO_ALUNO_MUDOU_ESCOLA, 'Cancelamento por alteração de unidade educacional'),
-        (CANCELADO_ALUNO_NAO_PERTENCE_REDE, 'Cancelamento para aluno não matriculado na rede municipal'),
+        (
+            CANCELADO_ALUNO_MUDOU_ESCOLA,
+            'Cancelamento por alteração de unidade educacional',
+        ),
+        (
+            CANCELADO_ALUNO_NAO_PERTENCE_REDE,
+            'Cancelamento para aluno não matriculado na rede municipal',
+        ),
     )
     (  # DA ESCOLA
         SOLICITACAO_KIT_LANCHE_AVULSA,
@@ -153,7 +185,7 @@ class LogSolicitacoesUsuario(ExportModelOperationsMixin('log_solicitacoes'), mod
         # DA LOGISTICA ABASTECIMENTO
         SOLICITACAO_REMESSA_PAPA,
         SOLICITACAO_DE_ALTERACAO_REQUISICAO,
-        ABASTECIMENTO_GUIA_DE_REMESSA
+        ABASTECIMENTO_GUIA_DE_REMESSA,
     ) = range(15)
 
     TIPOS_SOLICITACOES = (
@@ -172,19 +204,16 @@ class LogSolicitacoesUsuario(ExportModelOperationsMixin('log_solicitacoes'), mod
         (TERCEIRIZADA_RESPONDEU_ANALISE_SENSORIAL, 'Responde Análise Sensorial'),
         (SOLICITACAO_REMESSA_PAPA, 'Solicitação de remessa'),
         (SOLICITACAO_DE_ALTERACAO_REQUISICAO, 'Solicitação de Ateração de requisição'),
-        (ABASTECIMENTO_GUIA_DE_REMESSA, 'Abastecimento de guia de remessa')
+        (ABASTECIMENTO_GUIA_DE_REMESSA, 'Abastecimento de guia de remessa'),
     )
 
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    criado_em = models.DateTimeField(
-        'Criado em', editable=False, auto_now_add=True)
+    criado_em = models.DateTimeField('Criado em', editable=False, auto_now_add=True)
     descricao = models.TextField('Descricao', blank=True)
     justificativa = models.TextField('Justificativa', blank=True)
-    resposta_sim_nao = models.BooleanField(
-        'Resposta - Sim ou Não', default=False)
+    resposta_sim_nao = models.BooleanField('Resposta - Sim ou Não', default=False)
     status_evento = models.PositiveSmallIntegerField(choices=STATUS_POSSIVEIS)
-    solicitacao_tipo = models.PositiveSmallIntegerField(
-        choices=TIPOS_SOLICITACOES)
+    solicitacao_tipo = models.PositiveSmallIntegerField(choices=TIPOS_SOLICITACOES)
     uuid_original = models.UUIDField()
     usuario = models.ForeignKey('perfil.Usuario', on_delete=models.DO_NOTHING)
 
@@ -198,16 +227,18 @@ class LogSolicitacoesUsuario(ExportModelOperationsMixin('log_solicitacoes'), mod
 
     def __str__(self):
         data = datetime.strftime(self.criado_em, '%Y-%m-%d %H:%M:%S')
-        return (f'{self.usuario} executou {self.get_status_evento_display()} '
-                f'em {self.get_solicitacao_tipo_display()} no dia {data}')
+        return (
+            f'{self.usuario} executou {self.get_status_evento_display()} '
+            f'em {self.get_solicitacao_tipo_display()} no dia {data}'
+        )
 
 
-class AnexoLogSolicitacoesUsuario(ExportModelOperationsMixin('log_solicitacoes_anexo'), models.Model):
+class AnexoLogSolicitacoesUsuario(
+    ExportModelOperationsMixin('log_solicitacoes_anexo'), models.Model
+):
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     log = models.ForeignKey(
-        LogSolicitacoesUsuario,
-        related_name='anexos',
-        on_delete=models.DO_NOTHING
+        LogSolicitacoesUsuario, related_name='anexos', on_delete=models.DO_NOTHING
     )
     nome = models.CharField(max_length=255, blank=True)
     arquivo = models.FileField()
@@ -217,10 +248,7 @@ class AnexoLogSolicitacoesUsuario(ExportModelOperationsMixin('log_solicitacoes_a
 
 
 class Endereco(ExportModelOperationsMixin('endereco'), models.Model):
-    logradouro = models.CharField(
-        max_length=255,
-        validators=[MinLengthValidator(5)]
-    )
+    logradouro = models.CharField(max_length=255, validators=[MinLengthValidator(5)])
     numero = models.IntegerField(null=True)
     complemento = models.CharField(max_length=50, blank=True)
     bairro = models.CharField(max_length=50)
@@ -230,19 +258,13 @@ class Endereco(ExportModelOperationsMixin('endereco'), models.Model):
 class Contato(ExportModelOperationsMixin('contato'), models.Model):
     nome = models.CharField('Nome', max_length=160, blank=True)
     telefone = models.CharField(
-        max_length=13,
-        validators=[MinLengthValidator(8)],
-        blank=True
+        max_length=10, validators=[MinLengthValidator(8)], blank=True
     )
     telefone2 = models.CharField(
-        max_length=10,
-        validators=[MinLengthValidator(8)],
-        blank=True
+        max_length=10, validators=[MinLengthValidator(8)], blank=True
     )
     celular = models.CharField(
-        max_length=11,
-        validators=[MinLengthValidator(8)],
-        blank=True
+        max_length=11, validators=[MinLengthValidator(8)], blank=True
     )
     email = models.EmailField(blank=True)
 
@@ -283,7 +305,7 @@ class TemplateMensagem(ExportModelOperationsMixin('template_mensagem'), models.M
         (SOLICITACAO_KIT_LANCHE_UNIFICADA, 'Solicitação de kit lanche unificada'),
         (INVERSAO_CARDAPIO, 'Inversão de cardápio'),
         (DIETA_ESPECIAL, 'Dieta Especial'),
-        (HOMOLOGACAO_PRODUTO, 'Homologação de Produto')
+        (HOMOLOGACAO_PRODUTO, 'Homologação de Produto'),
     )
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     tipo = models.PositiveSmallIntegerField(choices=CHOICES, unique=True)
@@ -307,10 +329,14 @@ class CategoriaPerguntaFrequente(ExportModelOperationsMixin('cat_faq'), models.M
 
 
 class PerguntaFrequente(ExportModelOperationsMixin('faq'), models.Model):
-    categoria = models.ForeignKey('CategoriaPerguntaFrequente', on_delete=models.PROTECT)  # noqa
+    categoria = models.ForeignKey(
+        'CategoriaPerguntaFrequente', on_delete=models.PROTECT
+    )  # noqa
     pergunta = models.TextField('Pergunta')
     resposta = models.TextField('Resposta')
-    criado_em = models.DateTimeField('Criado em', editable=False, auto_now_add=True)  # noqa
+    criado_em = models.DateTimeField(
+        'Criado em', editable=False, auto_now_add=True
+    )  # noqa
     uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
     def __str__(self):
