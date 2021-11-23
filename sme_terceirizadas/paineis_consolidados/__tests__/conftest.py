01594a2d09a40646599abf6490a032293dc6c7dc
Kelwy Oliveira <kelwyduarte@gmail.com>
2021-11-23 10:06:41 -0300

    remove security hotspots de outros arquivos

--- sme_terceirizadas/paineis_consolidados/__tests__/conftest.py
+++ sme_terceirizadas/paineis_consolidados/__tests__/conftest.py
@@ -303,9 +303,9 @@ def solicitacoes_ano_dre(client, django_user_model, request, diretoria_regional2
                          solicitacoes_kit_lanche_dre, inclusoes_de_alimentacao_continua_dre):
     email, password, rf, cpf = request.param
     user_dre = django_user_model.objects.create_user(password=password, email=email, registro_funcional=rf, cpf=cpf)
-    user_codae = django_user_model.objects.create_user(password='xxx2', email='xxx@email.com',
+    user_codae = django_user_model.objects.create_user(password=constants.DJANGO_ADMIN_PASSWORD, email='xxx@email.com',
                                                        registro_funcional='9987634', cpf='12oiu3123')
-    user_escola = django_user_model.objects.create_user(password='xxx', email='user@escola.com',
+    user_escola = django_user_model.objects.create_user(password=constants.DJANGO_ADMIN_PASSWORD, email='user@escola.com',
                                                         registro_funcional='123123', cpf='12312312332')
     client.login(email=email, password=password)
 
@@ -420,7 +420,7 @@ def client_autenticado_dre_paineis_consolidados(client, django_user_model, diret
     email = 'test@test.com'
     password = constants.DJANGO_ADMIN_PASSWORD
     user = django_user_model.objects.create_user(password=password, email=email, registro_funcional='8888888')
-    user_escola = django_user_model.objects.create_user(password='xxx', email='user@escola.com',
+    user_escola = django_user_model.objects.create_user(password=password, email='user@escola.com',
                                                         registro_funcional='123123', cpf='12312312332')
     perfil_cogestor = mommy.make('Perfil',
                                  nome=constants.COGESTOR,
