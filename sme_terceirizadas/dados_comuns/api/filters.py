b286291e849177764316d359f8823006c82cb391
Kelwy Oliveira <kelwyduarte@gmail.com>
2021-12-08 19:50:27 -0300

    Adiciona filtros nas notificações

---
+++ sme_terceirizadas/dados_comuns/api/filters.py
@@ -0,0 +1,28 @@
+from django_filters import rest_framework as filters
+
+from ..models import Notificacao
+
+class NotificacaoFilter(filters.FilterSet):
+    uuid = filters.CharFilter(
+        field_name='uuid',
+        lookup_expr='exact',
+    )
+    tipo = filters.MultipleChoiceFilter(
+        field_name='tipo',
+        choices=[(str(state), state) for state in Notificacao.TIPO_NOTIFICACAO_NOMES],
+    )
+    categoria = filters.CharFilter(
+        field_name='categoria',
+        lookup_expr='icontains',
+    )
+    data_inicial = filters.DateFilter(
+        field_name='criado_em__date',
+        lookup_expr='gte',
+    )
+    data_final = filters.DateFilter(
+        field_name='criado_em__date',
+        lookup_expr='lte',
+    )
+    lido = filters.BooleanFilter(
+        field_name='lido'
+    )
