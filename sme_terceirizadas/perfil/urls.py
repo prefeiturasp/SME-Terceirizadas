45142d107c32d9662ac0f77049118f2484f7827c
jvictor-am <joaovictorengp@gmail.com>
2022-01-26 17:29:49 -0300

    Feature: criar urls para exportar arquivos .xlsx modelos

--- sme_terceirizadas/perfil/urls.py
+++ sme_terceirizadas/perfil/urls.py
@@ -12,5 +12,20 @@ router.register('confirmar_email/(?P<uuid>[^/]+)/(?P<confirmation_key>[^/]+)',
                 viewsets.UsuarioConfirmaEmailViewSet, 'Confirmar E-mail')
 
 urlpatterns = [
+    path(
+        'exportar_planilha_importacao_usuarios_perfil_codae/',
+        viewsets.exportar_planilha_importacao_usuarios_perfil_codae,
+        name='exportar_planilha_importacao_usuarios_perfil_codae'
+    ),
+    path(
+        'exportar_planilha_importacao_usuarios_perfil_escola/',
+        viewsets.exportar_planilha_importacao_usuarios_perfil_escola,
+        name='exportar_planilha_importacao_usuarios_perfil_escola'
+    ),
+    path(
+        'exportar_planilha_importacao_usuarios_perfil_dre/',
+        viewsets.exportar_planilha_importacao_usuarios_perfil_dre,
+        name='exportar_planilha_importacao_usuarios_perfil_dre'
+    ),
     path('', include(router.urls))
 ]
