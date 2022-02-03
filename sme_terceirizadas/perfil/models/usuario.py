2b30bc730c2478d5a6ad82b67c6fddee56a17a9a
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2022-02-03 14:05:42 -0300

    Feature: adicionar novo perfil para tipo_usuario property e nova constante para o usuário criado

--- sme_terceirizadas/perfil/models/usuario.py
+++ sme_terceirizadas/perfil/models/usuario.py
@@ -26,7 +26,8 @@ from ...dados_comuns.constants import (
     COORDENADOR_GESTAO_ALIMENTACAO_TERCEIRIZADA,
     COORDENADOR_GESTAO_PRODUTO,
     COORDENADOR_LOGISTICA,
-    COORDENADOR_SUPERVISAO_NUTRICAO
+    COORDENADOR_SUPERVISAO_NUTRICAO,
+    COORDENADOR_SUPERVISAO_NUTRICAO_MANIFESTACAO
 )
 from ...dados_comuns.tasks import envia_email_unico_task
 from ...dados_comuns.utils import url_configs
@@ -195,6 +196,8 @@ class Usuario(ExportModelOperationsMixin('usuario'), SimpleEmailConfirmationUser
                 elif self.vinculo_atual.perfil.nome in [COORDENADOR_SUPERVISAO_NUTRICAO,
                                                         ADMINISTRADOR_SUPERVISAO_NUTRICAO]:
                     tipo_usuario = 'supervisao_nutricao'
+                elif self.vinculo_atual.perfil.nome in [COORDENADOR_SUPERVISAO_NUTRICAO_MANIFESTACAO]:
+                    tipo_usuario = 'nutricao_manifestacao'
                 else:
                     tipo_usuario = 'dieta_especial'
         return tipo_usuario
