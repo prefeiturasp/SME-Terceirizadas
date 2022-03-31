825f408e81eb7905f430cdf427ec1a694bb30f5e
Kelwy Oliveira <kelwyduarte@gmail.com>
2022-03-31 15:25:14 -0300

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -189,7 +189,7 @@ MIDDLEWARE = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-root
 STATIC_ROOT = str(ROOT_DIR('staticfiles'))
 # https://docs.djangoproject.com/en/dev/ref/settings/#static-url
-STATIC_URL = '/code/staticfiles/'
+STATIC_URL = '/django_static/'
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
     str(APPS_DIR.path('static')),
