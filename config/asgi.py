448d0fae9b805e564eb013802fcf60f2c3a7e5bc
jvictor-am <joaovictorengp@gmail.com>
2022-07-14 01:50:51 -0300

    POC_Feature: criar arquivo de configuração asgi/py

---
+++ config/asgi.py
@@ -0,0 +1,31 @@
+"""
+ASGI config for SME-Terceirizadas project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+import os
+
+import django
+from channels.auth import AuthMiddlewareStack
+from channels.http import AsgiHandler
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from sme_terceirizadas.dieta_especial.urls import ws_urlpatterns
+
+# Fetch Django ASGI application early to ensure AppRegistry is populated
+# before importing consumers and AuthMiddlewareStack that may import ORM
+# models.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+django.setup()
+
+application = ProtocolTypeRouter({
+    'http': AsgiHandler(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter([
+            *ws_urlpatterns
+        ])
+    ),
+})
