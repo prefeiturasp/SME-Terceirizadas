ee3c44d3d4d408513c9a2e297ac86e37399c715d
Kelwy Oliveira <kelwyduarte@gmail.com>
2022-08-12 16:02:47 -0300

    Implementa funcionalidade para geração de planilha com usuários de empresas

--- sme_terceirizadas/dados_comuns/utils.py
+++ sme_terceirizadas/dados_comuns/utils.py
@@ -235,3 +235,26 @@ def atualiza_central_download_com_erro(obj_central_download, msg_erro):
     obj_central_download.status = CentralDeDownload.STATUS_ERRO
     obj_central_download.msg_erro = msg_erro
     obj_central_download.save()
+
+
+class ExportExcelAction:
+    @classmethod # noqa
+    def generate_header(cls, admin, model, list_display):
+        def default_format(value):
+            return value.replace('_', ' ').upper()
+
+        header = []
+        for field_display in list_display:
+            is_model_field = field_display in [f.name for f in model._meta.fields]
+            is_admin_field = hasattr(admin, field_display)
+            if is_model_field:
+                field = model._meta.get_field(field_display)
+                field_name = getattr(field, 'verbose_name', field_display)
+                header.append(default_format(field_name))
+            elif is_admin_field:
+                field = getattr(admin, field_display)
+                field_name = getattr(field, 'short_description', default_format(field_display))
+                header.append(default_format(field_name))
+            else:
+                header.append(default_format(field_display))
+        return header
