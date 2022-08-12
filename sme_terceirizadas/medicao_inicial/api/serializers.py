52d6b1d3e3608321606c60e8bb8830b9646ea511
jvictor-am <joaovictorengp@gmail.com>
2022-08-12 17:14:39 -0300

    Feature: criar viewsets, serializers, endpoints necessários

--- sme_terceirizadas/medicao_inicial/api/serializers.py
+++ sme_terceirizadas/medicao_inicial/api/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
 
+from sme_terceirizadas.dados_comuns.api.serializers import LogSolicitacoesUsuarioSerializer
 from sme_terceirizadas.escola.api.serializers import TipoUnidadeEscolarSerializer
-from sme_terceirizadas.medicao_inicial.models import DiaSobremesaDoce
+from sme_terceirizadas.medicao_inicial.models import (
+    DiaSobremesaDoce,
+    Responsavel,
+    SolicitacaoMedicaoInicial,
+    TipoContagemAlimentacao
+)
 from sme_terceirizadas.perfil.api.serializers import UsuarioSerializer
 
 
@@ -12,3 +18,42 @@ class DiaSobremesaDoceSerializer(serializers.ModelSerializer):
     class Meta:
         model = DiaSobremesaDoce
         exclude = ('id',)
+
+
+class AnexoOcorrenciaMedicaoInicialSerializer(serializers.ModelSerializer):
+    arquivo = serializers.SerializerMethodField()
+
+    def get_arquivo(self, obj):
+        request = self.context.get('request')
+        arquivo = obj.arquivo.url
+        return request.build_absolute_uri(arquivo)
+
+    class Meta:
+        model = TipoContagemAlimentacao
+        exclude = ('id',)
+
+
+class TipoContagemAlimentacaoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = TipoContagemAlimentacao
+        exclude = ('id',)
+
+
+class ResponsavelSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Responsavel
+        exclude = ('id', 'solicitacao_medicao_inicial',)
+
+
+class SolicitacaoMedicaoInicialSerializer(serializers.ModelSerializer):
+    escola = serializers.CharField(source='escola.nome')
+    tipo_contagem_alimentacoes = TipoContagemAlimentacaoSerializer()
+    responsaveis = ResponsavelSerializer(many=True)
+    anexo = AnexoOcorrenciaMedicaoInicialSerializer(required=False)
+    logs = LogSolicitacoesUsuarioSerializer(many=True)
+
+    class Meta:
+        model = SolicitacaoMedicaoInicial
+        exclude = ('id', 'criado_por',)
--- sme_terceirizadas/medicao_inicial/urls.py
+++ sme_terceirizadas/medicao_inicial/urls.py
@@ -6,6 +6,8 @@ from .api import viewsets
 router = routers.DefaultRouter()
 
 router.register('dias-sobremesa-doce', viewsets.DiaSobremesaDoceViewSet)
+router.register('solicitacao-medicao-inicial', viewsets.SolicitacaoMedicaoInicialViewSet)
+router.register('tipo-contagem-alimentacao', viewsets.TipoContagemAlimentacaoViewSet)
 
 urlpatterns = [
     path('medicao-inicial/', include(router.urls))
