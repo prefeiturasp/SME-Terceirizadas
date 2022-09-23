2e7d792f4b9789e3f4d4c234b1c55e90fa0490bf
Kelwy Oliveira <kelwyduarte@gmail.com>
2022-09-23 12:41:18 -0300

    Adiciona visão no endpoint de perfis

--- sme_terceirizadas/perfil/api/serializers.py
+++ sme_terceirizadas/perfil/api/serializers.py
@@ -40,7 +40,7 @@ logger = logging.getLogger(__name__)
 class PerfilSimplesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Perfil
-        fields = ('nome', 'uuid')
+        fields = ('nome', 'visao', 'uuid')
 
 
 class PerfilSerializer(serializers.ModelSerializer):
