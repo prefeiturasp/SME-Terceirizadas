7f3773b4786c6b90c9a71bd2d0d43386ab545156
Kelwy Oliveira <kelwyduarte@gmail.com>
2022-10-03 10:42:32 -0300

    Adiciona modelos no init para facil acesso

--- sme_terceirizadas/perfil/models/__init__.py
+++ sme_terceirizadas/perfil/models/__init__.py
@@ -2,9 +2,11 @@ from .perfil import ContentType, Perfil, Vinculo  # noqa F401
 from .usuario import ( # noqa F401
     Cargo,
     CustomUserManager,
+    ImportacaoPlanilhaUsuarioExternoCoreSSO,
     ImportacaoPlanilhaUsuarioPerfilCodae,
     ImportacaoPlanilhaUsuarioPerfilDre,
     ImportacaoPlanilhaUsuarioPerfilEscola,
+    ImportacaoPlanilhaUsuarioServidorCoreSSO,
     PlanilhaDiretorCogestor,
     Usuario
 )
