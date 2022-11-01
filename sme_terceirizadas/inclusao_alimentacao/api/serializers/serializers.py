b036ea3d16dd1eedf362cc029765875d0717598f
Calvin Rossinhole <calvin.masters@gmail.com>
2022-11-01 18:04:27 -0300

    complementa serializer

--- sme_terceirizadas/inclusao_alimentacao/api/serializers/serializers.py
+++ sme_terceirizadas/inclusao_alimentacao/api/serializers/serializers.py
@@ -215,6 +215,8 @@ class InclusaoDeAlimentacaoCEMEIRetrieveSerializer(serializers.ModelSerializer):
     quantidade_alunos_emei_da_inclusao_cemei = QuantidadeDeAlunosEMEIInclusaoDeAlimentacaoCEMEISerializer(many=True)
     id_externo = serializers.CharField()
     rastro_terceirizada = TerceirizadaSimplesSerializer()
+    prioridade = serializers.CharField()
+    logs = LogSolicitacoesUsuarioSerializer(many=True)
 
     class Meta:
         model = InclusaoDeAlimentacaoCEMEI
