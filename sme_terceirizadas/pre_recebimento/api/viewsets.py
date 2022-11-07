b84c6f49722042b8993e1b69cbad0be5256686fb
Kelwy Oliveira <kelwyduarte@gmail.com>
2022-11-07 19:00:43 -0300

    Atualiza permissionamento relacionado as rotas de cronogramas

--- sme_terceirizadas/pre_recebimento/api/viewsets.py
+++ sme_terceirizadas/pre_recebimento/api/viewsets.py
@@ -1,19 +1,27 @@
 
 from rest_framework import viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from sme_terceirizadas.dados_comuns.permissions import (
+    PermissaoParaCriarCronograma,
+    PermissaoParaVisualizarCronograma,
+    ViewSetActionPermissionMixin
+)
 from sme_terceirizadas.pre_recebimento.api.serializers.serializer_create import CronogramaCreateSerializer
 from sme_terceirizadas.pre_recebimento.api.serializers.serializers import CronogramaSerializer
 from sme_terceirizadas.pre_recebimento.models import Cronograma, EtapasDoCronograma
 
 
-class CronogramaModelViewSet(viewsets.ModelViewSet):
+class CronogramaModelViewSet(ViewSetActionPermissionMixin, viewsets.ModelViewSet):
     lookup_field = 'uuid'
     queryset = Cronograma.objects.all()
     serializer_class = CronogramaSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (PermissaoParaVisualizarCronograma,)
+    permission_action_classes = {
+        'create': [PermissaoParaCriarCronograma],
+        'delete': [PermissaoParaCriarCronograma]
+    }
 
     def get_serializer_class(self):
         if self.action in ['retrieve', 'list']:
