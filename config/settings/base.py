f5ab8e7a86a5608811e0e2a1237858e4371e6cdb
jvictor-am <joaovictorengp@gmail.com>
2022-12-27 12:36:22 -0300

    Feature: utilizar variável REDIS_URL

--- config/settings/base.py
+++ config/settings/base.py
@@ -71,7 +71,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [(env('REDIS_HOST'), env('REDIS_PORT'))]
+            'hosts': [env('REDIS_URL')]
         }
     }
 }
