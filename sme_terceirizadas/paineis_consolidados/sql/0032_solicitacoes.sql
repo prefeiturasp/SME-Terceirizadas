aa00a965c7409e89eb6b7b608d682b6019a723e0
Calvin Rossinhole <calvin.masters@gmail.com>
2023-01-05 15:05:18 -0300

    corrige sql

--- sme_terceirizadas/paineis_consolidados/sql/0032_solicitacoes.sql
+++ sme_terceirizadas/paineis_consolidados/sql/0032_solicitacoes.sql
@@ -470,6 +470,7 @@ from inclusao_alimentacao_inclusaoalimentacaodacei as inc_alimentacao_cei
          left join inclusao_alimentacao_diasmotivosinclusaodealimentacaocei as inc_alimentacao_item
                    on inc_alimentacao_item.inclusao_cei_id = inc_alimentacao_cei.id
 group by inc_alimentacao_cei.id,
+         inc_alimentacao_item.data,
          lote.nome,
          dre.nome,
          dre.iniciais,
