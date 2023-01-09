510177af58d075a020aefb9e9be796c0d52e830a
jvictor-am <joaovictorengp@gmail.com>
2023-01-09 14:57:08 -0300

    ajusta asgi.py para tentar evitar "django.core.exceptions.AppRegistryNotReady: Apps aren't loaded yet."

--- config/asgi.py
+++ config/asgi.py
@@ -7,20 +7,18 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 import os
 
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production') # noqa I001
+django.setup() # noqa I001
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+
 from django.core.asgi import get_asgi_application
 
 from sme_terceirizadas.dados_comuns.urls import ws_urlpatterns
 
-# Fetch Django ASGI application early to ensure AppRegistry is populated
-# before importing consumers and AuthMiddlewareStack that may import ORM
-# models.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
-django_asgi_app = get_asgi_application()
-
 application = ProtocolTypeRouter({
-    'http': django_asgi_app,
+    'http': get_asgi_application(),
     'websocket': AuthMiddlewareStack(
         URLRouter([
             *ws_urlpatterns
