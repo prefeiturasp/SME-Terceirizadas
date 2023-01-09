6a2cf8715e26f612e9c971d8e38bba6350177524
jvictor-am <joaovictorengp@gmail.com>
2023-01-09 13:09:43 -0300

    corrige ordem imports

--- config/asgi.py
+++ config/asgi.py
@@ -6,11 +6,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 import os
-from django.core.asgi import get_asgi_application
 
 import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 
 from sme_terceirizadas.dados_comuns.urls import ws_urlpatterns
 
