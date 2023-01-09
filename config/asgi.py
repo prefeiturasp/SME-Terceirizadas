b6d138f16c1fea7b053dccae377d650477c82401
jvictor-am <joaovictorengp@gmail.com>
2023-01-09 13:13:31 -0300

    remove importação não utilizada

--- config/asgi.py
+++ config/asgi.py
@@ -7,7 +7,6 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 import os
 
-import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
