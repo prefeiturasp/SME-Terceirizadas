455710de628d1bf3d47a4dd12f12ad87bdbcf354
Diogo Santos <euamofisica2006@hotmail.com>
2023-02-09 18:32:04 -0300

    corrige migration

--- sme_terceirizadas/dados_comuns/models.py
+++ sme_terceirizadas/dados_comuns/models.py
@@ -20,7 +20,6 @@ class LogSolicitacoesUsuario(
         TERCEIRIZADA_TOMOU_CIENCIA,
         TERCEIRIZADA_RECUSOU,
         CODAE_NEGOU,
-        CODAE_ATUALIZAZOU_PROTOCOLO,
         # ESPECIFICA DO PARTINDO DA DRE
         CODAE_PEDIU_REVISAO,
         DRE_REVISOU,
@@ -88,7 +87,8 @@ class LogSolicitacoesUsuario(
         FORNECEDOR_SOLICITA_ALTERACAO_CRONOGRAMA,
         SUSPENSO_EM_ALGUNS_EDITAIS,
         ATIVO_EM_ALGUNS_EDITAIS,
-        CRONOGRAMA_ASSINADO_PELO_USUARIO_CRONOGRAMA
+        CRONOGRAMA_ASSINADO_PELO_USUARIO_CRONOGRAMA,
+        CODAE_ATUALIZOU_PROTOCOLO
     ) = range(64)
 
     STATUS_POSSIVEIS = (
@@ -97,7 +97,6 @@ class LogSolicitacoesUsuario(
         (TERCEIRIZADA_TOMOU_CIENCIA, 'Terceirizada tomou ciência'),
         (TERCEIRIZADA_RECUSOU, 'Terceirizada recusou'),
         (CODAE_NEGOU, 'CODAE negou'),
-        (CODAE_ATUALIZAZOU_PROTOCOLO, 'CODAE Atualizou o protocolo'),
         (CODAE_PEDIU_REVISAO, 'CODAE pediu revisão'),
         (DRE_REVISOU, 'DRE revisou'),
         (DRE_VALIDOU, 'DRE validou'),
@@ -193,7 +192,8 @@ class LogSolicitacoesUsuario(
         (FORNECEDOR_SOLICITA_ALTERACAO_CRONOGRAMA, 'Alteração Fornecedor'),
         (SUSPENSO_EM_ALGUNS_EDITAIS, 'Suspenso em alguns editais'),
         (ATIVO_EM_ALGUNS_EDITAIS, 'Ativo em alguns editais'),
-        (CRONOGRAMA_ASSINADO_PELO_USUARIO_CRONOGRAMA, 'Assinado Cronograma')
+        (CRONOGRAMA_ASSINADO_PELO_USUARIO_CRONOGRAMA, 'Assinado Cronograma'),
+        (CODAE_ATUALIZOU_PROTOCOLO, 'CODAE Atualizou o protocolo')
     )
     (  # DA ESCOLA
         SOLICITACAO_KIT_LANCHE_AVULSA,
