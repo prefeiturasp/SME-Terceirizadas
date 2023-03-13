80191dd44355be38e89a8f91c68786b229e7884f
Calvin Rossinhole <calvin.masters@gmail.com>
2023-03-13 09:30:14 -0300

    export funcionando com grupos

--- sme_terceirizadas/medicao_inicial/api/viewsets.py
+++ sme_terceirizadas/medicao_inicial/api/viewsets.py
@@ -22,7 +22,7 @@ from ..models import (
     Medicao,
     SolicitacaoMedicaoInicial,
     TipoContagemAlimentacao,
-    ValorMedicao
+    ValorMedicao, GrupoMedicao
 )
 from .permissions import EhAdministradorMedicaoInicialOuGestaoAlimentacao
 from .serializers import (
@@ -226,7 +226,8 @@ class SolicitacaoMedicaoInicialViewSet(
     @action(detail=True, methods=['GET'], url_path='relatorio-pdf')
     def relatorio_pdf(self, request, uuid):
         solicitacao = self.get_object()
-        tabelas = [{'periodos': [], 'categorias': [], 'nomes_campos': [], 'len_categorias': [], 'valores_campos': []}]
+        tabelas = [{'periodos': [], 'categorias': [], 'nomes_campos': [], 'len_periodos': [], 'len_categorias': [],
+                    'valores_campos': []}]
         MAX_COLUNAS = 15
         CATEGORIA = 0
         CAMPO = 1
@@ -261,23 +262,31 @@ class SolicitacaoMedicaoInicialViewSet(
                 else:
                     dict_categorias_campos[categoria_campo[CATEGORIA]] += [categoria_campo[CAMPO]]
             for categoria in dict_categorias_campos.keys():
+                nome_periodo = medicao.periodo_escolar.nome
+                if medicao.grupo:
+                    nome_periodo = medicao.grupo.nome + ' - ' + medicao.periodo_escolar.nome
                 if len(tabelas[indice_atual]['nomes_campos']) + len(dict_categorias_campos[categoria]) <= MAX_COLUNAS:
-                    if medicao.periodo_escolar.nome not in tabelas[indice_atual]['periodos']:
-                        tabelas[indice_atual]['periodos'] += [medicao.periodo_escolar.nome]
+                    if nome_periodo not in tabelas[indice_atual]['periodos']:
+                        tabelas[indice_atual]['periodos'] += [nome_periodo]
                     tabelas[indice_atual]['categorias'] += [categoria]
                     tabelas[indice_atual]['nomes_campos'] += sorted(
                         dict_categorias_campos[categoria], key=lambda k: ORDEM_CAMPOS[k])
                     tabelas[indice_atual]['len_categorias'] += [len(dict_categorias_campos[categoria])]
                 else:
                     indice_atual += 1
-                    tabelas += [{'periodos': [], 'categorias': [], 'nomes_campos': [], 'len_categorias': [],
-                                 'valores_campos': []}]
-                    tabelas[indice_atual]['periodos'] += [medicao.periodo_escolar.nome]
+                    tabelas += [{'periodos': [], 'categorias': [], 'nomes_campos': [], 'len_periodos': [],
+                                 'len_categorias': [], 'valores_campos': []}]
+                    tabelas[indice_atual]['periodos'] += [nome_periodo]
                     tabelas[indice_atual]['categorias'] += [categoria]
                     tabelas[indice_atual]['nomes_campos'] += dict_categorias_campos[categoria]
                     tabelas[indice_atual]['len_categorias'] += [len(dict_categorias_campos[categoria])]
+        for tabela in tabelas:
+            if len(tabela['periodos']) == 1:
+                tabela['len_periodos'] = [len(tabela['nomes_campos'])]
+            else:
+                tabela['len_periodos'] = tabela['len_categorias']
         dias_no_mes = range(1, monthrange(int(solicitacao.ano), int(solicitacao.mes))[1] + 1)
-
+        indice_periodo = 0
         logs_alunos_matriculados = LogAlunosMatriculadosPeriodoEscola.objects.filter(
             escola=solicitacao.escola, criado_em__month=solicitacao.mes, criado_em__year=solicitacao.ano)
         logs_dietas = LogQuantidadeDietasAutorizadas.objects.filter(
@@ -295,10 +304,18 @@ class SolicitacaoMedicaoInicialViewSet(
                         indice_categoria += 1
                         categoria_corrente = tabela['categorias'][indice_categoria]
                     if campo == 'matriculados':
+                        if indice_campo > 1 and len(tabela['periodos']) > 1:
+                            indice_periodo += 1
                         try:
-                            valores_dia += [logs_alunos_matriculados.get(criado_em__day=dia).quantidade_alunos]
+                            periodo = tabela['periodos'][indice_periodo]
+                            if '-' in periodo:
+                                periodo = periodo.split(' - ')[1]
+                            valores_dia += [logs_alunos_matriculados.get(
+                                periodo_escolar__nome=periodo,
+                                criado_em__day=dia
+                            ).quantidade_alunos]
                         except LogAlunosMatriculadosPeriodoEscola.DoesNotExist:
-                            valores_dia += ['-']
+                            valores_dia += ['0']
                     elif campo == 'aprovadas':
                         try:
                             if 'ENTERAL' in categoria_corrente:
@@ -319,9 +336,26 @@ class SolicitacaoMedicaoInicialViewSet(
                                     classificacao__nome=categoria_corrente.split(' - ')[1].title()
                                 ).quantidade]
                         except LogQuantidadeDietasAutorizadas.DoesNotExist:
-                            valores_dia += ['-']
+                            valores_dia += ['0']
+                    elif campo in ['solicitado', 'total_refeicoes_pagamento', 'total_sobremesas_pagamento']:
+                        valores_dia += ['0']
                     else:
-                        valores_dia += ['-']
+                        try:
+                            periodo = tabela['periodos'][indice_periodo]
+                            grupo = None
+                            if '-' in periodo:
+                                grupo = GrupoMedicao.objects.get(nome=periodo.split(' - ')[0])
+                                periodo = periodo.split(' - ')[1]
+                            valores_dia += [solicitacao.medicoes.get(
+                                periodo_escolar__nome=periodo,
+                                grupo=grupo
+                            ).valores_medicao.get(
+                                dia=f'{dia:02d}',
+                                categoria_medicao__nome=categoria_corrente,
+                                nome_campo=campo
+                            ).valor]
+                        except ValorMedicao.DoesNotExist:
+                            valores_dia += ['0']
                     indice_campo += 1
                 tabela['valores_campos'] += [valores_dia]
         html_string = render_to_string(
