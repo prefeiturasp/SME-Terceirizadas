938fc0ccb758a0aacb62501e077e7461a4ddf02f
Kelwy Oliveira <kelwyduarte@gmail.com>
2023-06-07 08:50:08 -0300

    Cria modelagem de Notificaões de guias com ocorrencias

--- sme_terceirizadas/logistica/models/__init__.py
+++ sme_terceirizadas/logistica/models/__init__.py
@@ -3,6 +3,8 @@ from .alimento import Embalagem  # noqa
 from .alimento import TipoEmbalagem  # noqa
 from .guia import ConferenciaGuia  # noqa
 from .guia import Guia  # noqa
+from .guia import NotificacaoOcorrenciasGuia  # noqa
+from .guia import PrevisaoContratualNotificacao  # noqa
 from .solicitacao import LogSolicitacaoDeCancelamentoPeloPapa  # noqa
 from .solicitacao import SolicitacaoDeAlteracaoRequisicao  # noqa
 from .solicitacao import SolicitacaoRemessa  # noqa
