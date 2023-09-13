c3ae84fb462f279763b3096f5a7bb899f3ee0e10
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2023-09-13 12:33:14 -0300

    Feature: adicionar integração com perídoso parciais

--- sme_terceirizadas/paineis_consolidados/api/viewsets.py
+++ sme_terceirizadas/paineis_consolidados/api/viewsets.py
@@ -802,13 +802,23 @@ class EscolaSolicitacoesViewSet(SolicitacoesViewSet):
         for inclusao in query_set:
             inc = inclusao.get_raw_model.objects.get(uuid=inclusao.uuid)
             if inclusao.tipo_doc == 'INC_ALIMENTA_CEI':
+                if 'PARCIAL' in periodos_escolares:
+                    periodos_externos = ['INTEGRAL']
+                    periodos_internos = ['MANHA', 'TARDE']
+                if 'INTEGRAL' in periodos_escolares:
+                    periodos_externos = ['INTEGRAL']
+                    periodos_internos = ['INTEGRAL']
                 dias_motivos = inc.dias_motivos_da_inclusao_cei.filter(data__month=mes, data__year=ano, cancelado=False)
-                quantidade_por_faixa = inc.quantidade_alunos_da_inclusao.filter(periodo__nome__in=periodos_escolares)
-                for dia_motivo in dias_motivos:
-                    return_dict.append({
-                        'dia': dia_motivo.data.day,
-                        'faixas_etarias': quantidade_por_faixa.values_list('faixa_etaria__uuid', flat=True),
-                    })
+                quantidade_por_faixa = inc.quantidade_alunos_da_inclusao.filter(
+                    periodo__nome__in=periodos_internos,
+                    periodo_externo__nome__in=periodos_externos
+                )
+                if quantidade_por_faixa:
+                    for dia_motivo in dias_motivos:
+                        return_dict.append({
+                            'dia': dia_motivo.data.day,
+                            'faixas_etarias': quantidade_por_faixa.values_list('faixa_etaria__uuid', flat=True),
+                        })
             else:
                 for periodo in inc.quantidades_periodo.all():
                     if periodo.periodo_escolar.nome in periodos_escolares:
