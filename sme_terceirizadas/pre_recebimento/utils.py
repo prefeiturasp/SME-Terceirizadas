f858e9de0953479860299136b643372ae1a0ad14
Kelwy Oliveira <kelwyduarte@gmail.com>
2023-09-25 09:06:33 -0300

    Cria metodo de paginação para layout

--- sme_terceirizadas/pre_recebimento/utils.py
+++ sme_terceirizadas/pre_recebimento/utils.py
@@ -53,3 +53,9 @@ class UnidadeMedidaPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+
+class LayoutDeEmbalagemPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
