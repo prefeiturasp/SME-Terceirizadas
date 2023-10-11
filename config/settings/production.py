35c7624a36f43a180e205297653da544b07ebc09
Calvin Rossinhole <calvin.masters@gmail.com>
2023-10-11 15:24:00 -0300

    feat: altera parametro CSRF_TRUSTED_ORIGIN

--- config/settings/production.py
+++ config/settings/production.py
@@ -123,4 +123,9 @@ LOGGING = {
 # Your stuff...
 # ------------------------------------------------------------------------------
 DATA_UPLOAD_MAX_MEMORY_SIZE = 26214400  # 25 MB
-CSRF_TRUSTED_ORIGINS = ['https://sigpae.sme.prefeitura.sp.gov.br', 'https://www.sigpae.sme.prefeitura.sp.gov.br']
+CSRF_TRUSTED_ORIGINS = [
+    'https://dev-sigpae.sme.prefeitura.sp.gov.br', 'https://www.dev-sigpae.sme.prefeitura.sp.gov.br',
+    'https://hom-sigpae.sme.prefeitura.sp.gov.br', 'https://www.hom-sigpae.sme.prefeitura.sp.gov.br',
+    'https://treinamento-sigpae.sme.prefeitura.sp.gov.br', 'https://www.treinamento-sigpae.sme.prefeitura.sp.gov.br',
+    'https://sigpae.sme.prefeitura.sp.gov.br', 'https://www.sigpae.sme.prefeitura.sp.gov.br'
+]
