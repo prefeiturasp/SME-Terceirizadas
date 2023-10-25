4ff1bdcf3defed58e80bc90404140e808a7e188f
Joao Mesquita <joaovictorengp@gmail.com>
2023-10-25 14:53:55 -0300

    Feature: ajustar filtro DiaParaCorrecaoFilter

--- sme_terceirizadas/medicao_inicial/api/filters.py
+++ sme_terceirizadas/medicao_inicial/api/filters.py
@@ -2,6 +2,7 @@ from django_filters import rest_framework as filters
 
 
 class DiaParaCorrecaoFilter(filters.FilterSet):
-    uuid_solicitacao_medicao = filters.CharFilter(field_name='uuid', lookup_expr='iexact')
+    uuid_solicitacao_medicao = filters.CharFilter(field_name='medicao__solicitacao_medicao_inicial__uuid',
+                                                  lookup_expr='iexact')
     nome_grupo = filters.CharFilter(field_name='medicao__grupo__nome', lookup_expr='iexact')
     nome_periodo_escolar = filters.CharFilter(field_name='medicao__periodo_escolar__nome', lookup_expr='iexact')
