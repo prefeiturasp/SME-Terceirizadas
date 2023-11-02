8f27e526d821951dece340950dae64f8ce5bae9f
Elder Santos <elder_rn@hotmail.com>
2023-11-02 11:43:02 -0300

    adiciona EmpresaFactory como subfactory em CronogramaFactory

--- sme_terceirizadas/pre_recebimento/fixtures/factories/cronograma_factory.py
+++ sme_terceirizadas/pre_recebimento/fixtures/factories/cronograma_factory.py
@@ -1,7 +1,8 @@
-from factory import DjangoModelFactory, Sequence
+from factory import DjangoModelFactory, Sequence, SubFactory
 from faker import Faker
 
 from sme_terceirizadas.pre_recebimento.models import Cronograma
+from sme_terceirizadas.terceirizada.fixtures.factories.terceirizada_factory import EmpresaFactory
 
 fake = Faker('pt_BR')
 
@@ -11,3 +12,4 @@ class CronogramaFactory(DjangoModelFactory):
         model = Cronograma
 
     numero = Sequence(lambda n: f'{str(fake.unique.random_int(min=0, max=1000))}/{str(fake.date(pattern="%Y"))}')
+    empresa = SubFactory(EmpresaFactory)
