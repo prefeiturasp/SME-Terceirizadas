6683d194d54165eee981087664ec66581156a594
Elder Santos <elder_rn@hotmail.com>
2023-11-03 11:29:56 -0300

    adiciona subfactory NomeDeProdutoEditalFactory em CronogramaFactory

--- sme_terceirizadas/pre_recebimento/fixtures/factories/cronograma_factory.py
+++ sme_terceirizadas/pre_recebimento/fixtures/factories/cronograma_factory.py
@@ -2,6 +2,8 @@ from factory import DjangoModelFactory, Sequence, SubFactory
 from faker import Faker
 
 from sme_terceirizadas.pre_recebimento.models import Cronograma
+from sme_terceirizadas.produto.fixtures.factories.nome_produto_edital_factory import NomeDeProdutoEditalFactory
+from sme_terceirizadas.produto.models import NomeDeProdutoEdital
 from sme_terceirizadas.terceirizada.fixtures.factories.terceirizada_factory import EmpresaFactory
 
 fake = Faker('pt_BR')
@@ -13,3 +15,4 @@ class CronogramaFactory(DjangoModelFactory):
 
     numero = Sequence(lambda n: f'{str(fake.unique.random_int(min=0, max=1000))}/{str(fake.date(pattern="%Y"))}')
     empresa = SubFactory(EmpresaFactory)
+    produto = SubFactory(NomeDeProdutoEditalFactory, tipo_produto=NomeDeProdutoEdital.LOGISTICA)
