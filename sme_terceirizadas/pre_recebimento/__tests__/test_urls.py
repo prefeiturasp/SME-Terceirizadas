8f97ab80122e69e43295291471bd83c7ca186c32
Elder Santos <elder_rn@hotmail.com>
2023-12-18 11:09:48 -0300

    test(108489): corrige testes problemáticos

--- sme_terceirizadas/pre_recebimento/__tests__/test_urls.py
+++ sme_terceirizadas/pre_recebimento/__tests__/test_urls.py
@@ -1253,7 +1253,8 @@ def test_url_dashboard_layout_embalagens_status_retornados(
 
     assert response.status_code == status.HTTP_200_OK
 
-    user = get_user_model().objects.get()
+    user_id = client_autenticado_codae_dilog.session["_auth_user_id"]
+    user = get_user_model().objects.get(id=user_id)
     status_esperados = ServiceDashboardLayoutEmbalagem.get_dashboard_status(user)
     status_recebidos = [result["status"] for result in response.json()["results"]]
 
@@ -2071,7 +2072,8 @@ def test_url_documentos_de_recebimento_listagem_not_authorized(client_autenticad
 def test_url_dashboard_documentos_de_recebimento_status_retornados(
     client_autenticado_codae_dilog, documento_de_recebimento_factory
 ):
-    user = get_user_model().objects.get()
+    user_id = client_autenticado_codae_dilog.session["_auth_user_id"]
+    user = get_user_model().objects.get(id=user_id)
     status_esperados = ServiceDashboardDocumentosDeRecebimento.get_dashboard_status(
         user
     )
