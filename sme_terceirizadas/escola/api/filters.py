e5582d9651577204f9740c70ab34733973dcb173
Luis Guerreiro <luiscvlh11@gmail.com>
2024-02-07 12:33:58 -0300

    feat(114004): :sparkles: ajusta filtro de escola para incluir alunos que ja tiveram matriculados na escola

--- sme_terceirizadas/escola/api/filters.py
+++ sme_terceirizadas/escola/api/filters.py
@@ -1,5 +1,7 @@
 from django_filters import rest_framework as filters
 
+from sme_terceirizadas.escola.models import Aluno, HistoricoMatriculaAluno
+
 
 class DiretoriaRegionalFilter(filters.FilterSet):
     dre = filters.CharFilter(
@@ -12,13 +14,24 @@ class AlunoFilter(filters.FilterSet):
     dre = filters.CharFilter(
         field_name="escola__diretoria_regional__uuid", lookup_expr="iexact"
     )
-    escola = filters.CharFilter(field_name="escola__uuid", lookup_expr="iexact")
+    escola = filters.CharFilter(field_name="escola__uuid", method="filter_escola")
     nao_tem_dieta_especial = filters.BooleanFilter(
         field_name="dietas_especiais", lookup_expr="isnull"
     )
     periodo_escolar_nome = filters.CharFilter(
         field_name="periodo_escolar__nome", lookup_expr="iexact"
     )
+
+    def filter_escola(self, queryset, name, value):
+        _filter = {name: value}
+
+        historicos_da_escola = HistoricoMatriculaAluno.objects.filter(**_filter)
+        alunos_atualmente_na_escola = queryset.filter(**_filter)
+        alunos_com_historico_na_escola = Aluno.objects.filter(
+            id__in=historicos_da_escola.values_list("aluno_id", flat=True)
+        )
+
+        return alunos_atualmente_na_escola | alunos_com_historico_na_escola
 
 
 class LogAlunosMatriculadosFaixaEtariaDiaFilter(filters.FilterSet):
