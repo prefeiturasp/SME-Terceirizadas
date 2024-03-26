4bfec9f577e9d0885712782ffcb387d8ad5892e6
Kelwy Oliveira <kelwyduarte@gmail.com>
2024-03-26 09:56:15 -0300

    feat(117541): cria modelo de cadastro de questões

---
+++ sme_terceirizadas/recebimento/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from multiselectfield import MultiSelectField
+
+from ..dados_comuns.behaviors import ModeloBase
+
+
+class QuestaoConferencia(ModeloBase):
+    # Tipo Questão Choice
+    TIPO_QUESTAO_PRIMARIA = "PRIMARIA"
+    TIPO_QUESTAO_SECUNDARIA = "SECUNDARIA"
+
+    TIPO_QUESTAO_NOMES = {
+        TIPO_QUESTAO_PRIMARIA: "Primária",
+        TIPO_QUESTAO_SECUNDARIA: "Secundária",
+    }
+
+    TIPO_QUESTAO_CHOICES = (
+        (TIPO_QUESTAO_PRIMARIA, TIPO_QUESTAO_NOMES[TIPO_QUESTAO_PRIMARIA]),
+        (TIPO_QUESTAO_SECUNDARIA, TIPO_QUESTAO_NOMES[TIPO_QUESTAO_SECUNDARIA]),
+    )
+
+    # status choice
+    ATIVO = "ATIVO"
+    INATIVO = "INATIVO"
+
+    STATUS_CHOICES = (
+        (ATIVO, "Ativo"),
+        (INATIVO, "Inativo"),
+    )
+
+    questao = models.CharField("Questão")
+    tipo_questao = MultiSelectField("Tipo de Questão", choices=TIPO_QUESTAO_CHOICES)
+    pergunta_obrigatoria = models.BooleanField("Pergunta Obrigatória?", default=False)
+    posicao = models.PositiveSmallIntegerField("Posição", blank=True, null=True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=10, default=ATIVO)
+
+    def __str__(self):
+        return f"{self.questao}"
+
+    class Meta:
+        verbose_name = "Questão para Conferência"
+        verbose_name_plural = "Questões para Conferência"
