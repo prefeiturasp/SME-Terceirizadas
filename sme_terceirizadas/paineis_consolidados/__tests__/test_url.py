3012de956675b61740f563977464f0962f118fa1
Calvin Rossinhole <calvin.masters@gmail.com>
2024-03-26 12:08:51 -0300

    test(117420): :white_check_mark: implementa teste unitario grafico tipo unidade

--- sme_terceirizadas/paineis_consolidados/__tests__/test_url.py
+++ sme_terceirizadas/paineis_consolidados/__tests__/test_url.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from ...dados_comuns.constants import SEM_FILTRO
 from ...eol_servico.utils import EOLService
 from ...escola.__tests__.conftest import mocked_informacoes_escola_turma_aluno
+from ...escola.models import TipoUnidadeEscolar
 from ..api.constants import (
     AGUARDANDO_CODAE,
     AUTORIZADOS,
@@ -815,3 +816,38 @@ def test_filtrar_solicitacoes_ga_graficos(
     )
     assert response.status_code == status.HTTP_200_OK
     assert len(response.json()[0]["labels"]) == 1
+
+
+def test_filtrar_solicitacoes_ga_graficos_tipo_unidade(
+    client_autenticado_codae_gestao_alimentacao,
+    escola,
+    escola_outro_lote,
+    solicitacoes_kit_lanche_autorizadas,
+    inclusoes_normais,
+):
+    data = {"status": "AUTORIZADOS"}
+    response = client_autenticado_codae_gestao_alimentacao.post(
+        "/solicitacoes-genericas/filtrar-solicitacoes-ga-graficos/",
+        content_type="application/json",
+        data=data,
+    )
+    assert response.status_code == status.HTTP_200_OK
+    assert (
+        response.json()[3]["datasets"][0]["label"]
+        == "Total de Solicitações Autorizadas por Tipo de Unidade"
+    )
+    assert (
+        len(response.json()[3]["labels"])
+        == TipoUnidadeEscolar.objects.filter(
+            pertence_relatorio_solicitacoes_alimentacao=True
+        ).count()
+    )
+
+    data = {"status": "AUTORIZADOS", "tipos_unidade": [escola.tipo_unidade.uuid]}
+    response = client_autenticado_codae_gestao_alimentacao.post(
+        "/solicitacoes-genericas/filtrar-solicitacoes-ga-graficos/",
+        content_type="application/json",
+        data=data,
+    )
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response.json()[3]["labels"]) == 1
