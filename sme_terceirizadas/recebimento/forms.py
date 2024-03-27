d8c1b238462b95a7cdb0a54e6a059cbb88b10f7f
Kelwy Oliveira <kelwyduarte@gmail.com>
2024-03-27 11:25:41 -0300

    fix(117541): ajusta lógica de validação do campo posicao

--- sme_terceirizadas/recebimento/forms.py
+++ sme_terceirizadas/recebimento/forms.py
@@ -1,5 +1,9 @@
+import functools
+import operator
+
 from django import forms
 from django.core.exceptions import ValidationError
+from django.db.models import Q
 
 from sme_terceirizadas.recebimento.models import QuestaoConferencia
 
@@ -17,15 +21,30 @@ class QuestaoForm(forms.ModelForm):
             ),
         }
 
+    def full_clean(self, *args, **kwargs):
+        super().full_clean()
+        # Limpa os erros extras para o campo 'posicao'
+        if "posicao" in self.errors and len(self.errors["posicao"]) > 1:
+            self.errors["posicao"] = self.error_class([self.errors["posicao"][0]])
+
     def clean(self):
-        posicao = self.cleaned_data["posicao"]
-        tipo_questao = self.cleaned_data["tipo_questao"]
-        if posicao is not None and tipo_questao:
-            qs = QuestaoConferencia.objects.filter(
-                posicao=posicao, tipo_questao__contains=tipo_questao
+        cleaned_data = super().clean()
+        posicao = cleaned_data.get("posicao", None)
+        tipo_questao = cleaned_data.get("tipo_questao", None)
+
+        if posicao and tipo_questao:
+            tipos_questoes = list(tipo_questao)
+            filtro = functools.reduce(
+                operator.or_,
+                (
+                    Q(tipo_questao__icontains=tipo, posicao=posicao)
+                    for tipo in tipos_questoes
+                ),
             )
+            qs = QuestaoConferencia.objects.filter(filtro)
             if self.instance:
                 qs = qs.exclude(pk=self.instance.pk)
+
             if qs.exists():
                 raise ValidationError(
                     {
