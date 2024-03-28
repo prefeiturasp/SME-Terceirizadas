b5113a9a5149064b2fbe5d88011fa5e0051415af
Elder Santos <elder_rn@hotmail.com>
2024-03-28 18:20:12 -0300

    feat(117391): cria permissão PermissaoParaVisualizarQuestoesConferencia

--- sme_terceirizadas/dados_comuns/permissions.py
+++ sme_terceirizadas/dados_comuns/permissions.py
@@ -1263,3 +1263,15 @@ class PermissaoParaAnalisarFichaTecnica(BasePermission):
             and usuario.vinculo_atual
             and usuario.vinculo_atual.perfil.nome in self.PERFIS_PERMITIDOS
         )
+
+
+class PermissaoParaVisualizarQuestoesConferencia(BasePermission):
+    PERFIS_PERMITIDOS = [DILOG_QUALIDADE]
+
+    def has_permission(self, request, view):
+        usuario = request.user
+        return (
+            not usuario.is_anonymous
+            and usuario.vinculo_atual
+            and usuario.vinculo_atual.perfil.nome in self.PERFIS_PERMITIDOS
+        )
