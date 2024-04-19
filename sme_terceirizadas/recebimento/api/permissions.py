ecb1e16dd1357f56edab29ca1a5cb77888fbed32
Elder Santos <elder_rn@hotmail.com>
2024-04-19 11:42:24 -0300

    feat(118113): cria permissão PermissaoParaCadastrarFichaRecebimento

--- sme_terceirizadas/recebimento/api/permissions.py
+++ sme_terceirizadas/recebimento/api/permissions.py
@@ -13,3 +13,15 @@ class PermissaoParaVisualizarQuestoesConferencia(BasePermission):
             and usuario.vinculo_atual
             and usuario.vinculo_atual.perfil.nome in self.PERFIS_PERMITIDOS
         )
+
+
+class PermissaoParaCadastrarFichaRecebimento(BasePermission):
+    PERFIS_PERMITIDOS = [DILOG_QUALIDADE]
+
+    def has_permission(self, request, view):
+        usuario = request.user
+        return (
+            not usuario.is_anonymous
+            and usuario.vinculo_atual
+            and usuario.vinculo_atual.perfil.nome in self.PERFIS_PERMITIDOS
+        )
