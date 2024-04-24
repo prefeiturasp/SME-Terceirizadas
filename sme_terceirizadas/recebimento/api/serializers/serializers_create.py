b3d7025d9d18cfabd63b8c6a56e903c477b71958
Elder Santos <elder_rn@hotmail.com>
2024-04-24 16:42:42 -0300

    feat(118332): implementa VeiculoFichaDeRecebimentoRascunhoSerializer e atualiza FichaDeRecebimentoRascunhoSerializer

--- sme_terceirizadas/recebimento/api/serializers/serializers_create.py
+++ sme_terceirizadas/recebimento/api/serializers/serializers_create.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 
+from sme_terceirizadas.dados_comuns.utils import update_instance_from_dict
 from sme_terceirizadas.pre_recebimento.models import FichaTecnicaDoProduto
 from sme_terceirizadas.pre_recebimento.models.cronograma import EtapasDoCronograma
 from sme_terceirizadas.recebimento.models import (
     FichaDeRecebimento,
     QuestaoConferencia,
     QuestoesPorProduto,
+    VeiculoFichaDeRecebimento,
 )
 
 
@@ -51,12 +53,44 @@ class QuestoesPorProdutoCreateSerializer(serializers.ModelSerializer):
         fields = ("ficha_tecnica", "questoes_primarias", "questoes_secundarias")
 
 
+class VeiculoFichaDeRecebimentoRascunhoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = VeiculoFichaDeRecebimento
+        exclude = ("id", "ficha_recebimento")
+
+
 class FichaDeRecebimentoRascunhoSerializer(serializers.ModelSerializer):
     etapa = serializers.SlugRelatedField(
         slug_field="uuid",
         required=True,
         queryset=EtapasDoCronograma.objects.all(),
     )
+    veiculos = VeiculoFichaDeRecebimentoRascunhoSerializer(many=True)
+
+    def create(self, validated_data):
+        dados_veiculos = validated_data.pop("veiculos")
+        instance = FichaDeRecebimento.objects.create(**validated_data)
+
+        self._cria_veiculos(instance, dados_veiculos)
+
+        return instance
+
+    def update(self, instance, validated_data):
+        instance.veiculos.all().delete()
+        dados_veiculos = validated_data.pop("veiculos")
+
+        instance = update_instance_from_dict(instance, validated_data, save=True)
+
+        self._cria_veiculos(instance, dados_veiculos)
+
+        return instance
+
+    def _cria_veiculos(self, instance, dados_veiculos):
+        for dados_veiculo in dados_veiculos:
+            VeiculoFichaDeRecebimento.objects.create(
+                ficha_recebimento=instance,
+                **dados_veiculo,
+            )
 
     class Meta:
         model = FichaDeRecebimento
