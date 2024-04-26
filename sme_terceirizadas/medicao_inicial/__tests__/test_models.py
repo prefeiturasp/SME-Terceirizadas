3c28309cf4eee278b938188ad545bdf845ffaf0d
Joao Mesquita <joaovictorengp@gmail.com>
2024-04-25 21:15:25 -0300

    feat(119254): ajusta testes dias_sobremesa_doce

--- sme_terceirizadas/medicao_inicial/__tests__/test_models.py
+++ sme_terceirizadas/medicao_inicial/__tests__/test_models.py
@@ -4,7 +4,10 @@ pytestmark = pytest.mark.django_db
 
 
 def test_dia_sobremesa_doce_model(dia_sobremesa_doce):
-    assert dia_sobremesa_doce.__str__() == "08/08/2022 - EMEF"
+    assert (
+        dia_sobremesa_doce.__str__()
+        == "08/08/2022 - EMEF - Edital Edital de Pregão nº 13/SME/2020"
+    )
 
 
 def test_solicitacao_medicao_inicial_model(solicitacao_medicao_inicial):
