cfc8e5519fb06d2a2c1fefb2e3b5a19731ca4751
Calvin Rossinhole <calvin.masters@gmail.com>
2024-04-26 10:31:39 -0300

    test(119015): :white_check_mark: implementa teste unitario planilha modelo tipo penalidade

--- sme_terceirizadas/conftest.py
+++ sme_terceirizadas/conftest.py
@@ -109,6 +109,7 @@ def client_admin_django(client, django_user_model):
         email=email,
         registro_funcional="8888888",
         is_staff=True,
+        is_superuser=True,
     )
     client.login(username=email, password=password)
     return client
