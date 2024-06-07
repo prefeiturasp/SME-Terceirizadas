30fa54e2cd52d3c95ccb47b0d78c7361d6ea310f
Calvin Rossinhole <calvin.masters@gmail.com>
2024-06-07 16:27:59 -0300

    test(122442): :white_check_mark: teste unitario erro parametrizacao uuid incorreta

--- sme_terceirizadas/imr/__tests__/test_views/test_formulario_diretor_viewset.py
+++ sme_terceirizadas/imr/__tests__/test_views/test_formulario_diretor_viewset.py
@@ -1,4 +1,5 @@
 import json
+import uuid
 
 import pytest
 from rest_framework import status
@@ -111,3 +112,68 @@ def test_formulario_diretor(
     )
     assert instance.respostas_campo_numerico.count() == 1
     assert instance.respostas_campo_numerico.filter(resposta=10).exists() is True
+
+
+def test_formulario_diretor_erro_parametrizacao_uuid(
+    client_autenticado_diretor_escola,
+    escola,
+    solicitacao_medicao_inicial_factory,
+    tipo_resposta_modelo_factory,
+    tipo_pergunta_parametrizacao_ocorrencia_factory,
+    tipo_ocorrencia_factory,
+    parametrizacao_ocorrencia_factory,
+):
+    solicitacao_medicao_inicial = solicitacao_medicao_inicial_factory(escola=escola)
+
+    tipo_resposta_campo_simples = tipo_resposta_modelo_factory(
+        nome="RespostaCampoTextoSimples"
+    )
+    tipo_pergunta_parametrizacao_ocorrencia_texto_simples = (
+        tipo_pergunta_parametrizacao_ocorrencia_factory(
+            nome="Campo de Texto Simples", tipo_resposta=tipo_resposta_campo_simples
+        )
+    )
+
+    tipo_ocorrencia_1 = tipo_ocorrencia_factory.create()
+    parametrizacao_ocorrencia_1 = parametrizacao_ocorrencia_factory(
+        tipo_ocorrencia=tipo_ocorrencia_1,
+        tipo_pergunta=tipo_pergunta_parametrizacao_ocorrencia_texto_simples,
+        titulo="Qual uniforme faltou?",
+    )
+
+    uuid_incorreto = uuid.uuid4()
+
+    payload = {
+        "datas": ["07/06/2024", "08/06/2024"],
+        "solicitacao_medicao_inicial": str(solicitacao_medicao_inicial.uuid),
+        "ocorrencias": [
+            {
+                "grupo": 1,
+                "parametrizacao": str(parametrizacao_ocorrencia_1.uuid),
+                "tipo_ocorrencia": str(tipo_ocorrencia_1.uuid),
+                "resposta": "calça",
+            },
+            {
+                "grupo": 2,
+                "parametrizacao": str(parametrizacao_ocorrencia_1.uuid),
+                "tipo_ocorrencia": str(tipo_ocorrencia_1.uuid),
+                "resposta": "camisa",
+            },
+            {
+                "grupo": 2,
+                "parametrizacao": str(uuid_incorreto),
+                "tipo_ocorrencia": str(tipo_ocorrencia_1.uuid),
+                "resposta": 10,
+            },
+        ],
+    }
+
+    response = client_autenticado_diretor_escola["client"].post(
+        f"/imr/formulario-diretor/",
+        data=json.dumps(payload),
+        content_type="application/json",
+    )
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == {
+        "detail": f"ParametrizacaoOcorrencia com o UUID {str(uuid_incorreto)} não foi encontrada"
+    }
