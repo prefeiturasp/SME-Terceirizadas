a48e9d2eca22c416894baa388903f5ca5c2ab3cf
Calvin Rossinhole <calvin.masters@gmail.com>
2024-06-13 17:28:56 -0300

    test(121643): :white_check_mark: testes unitarios dashboard relatorios supervisao

--- sme_terceirizadas/perfil/fixtures/factories/perfil_base_factories.py
+++ sme_terceirizadas/perfil/fixtures/factories/perfil_base_factories.py
@@ -8,6 +8,8 @@ fake = Faker("pt_BR")
 
 class UsuarioFactory(DjangoModelFactory):
     nome = Sequence(lambda n: f"nome - {fake.unique.name()}")
+    username = Sequence(lambda n: fake.random.randint(1, 9999999))
+    email = Sequence(lambda n: fake.unique.email())
 
     class Meta:
         model = Usuario
