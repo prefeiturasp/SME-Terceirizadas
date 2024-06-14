972ec4fab40bd502f6e73a8bc982caa63958ebfa
Calvin Rossinhole <calvin.masters@gmail.com>
2024-06-14 17:15:25 -0300

    test(-): :white_check_mark: teste unitario tipo ocorrencia admin

--- sme_terceirizadas/imr/__tests__/test_admin.py
+++ sme_terceirizadas/imr/__tests__/test_admin.py
@@ -2,8 +2,8 @@ from unittest.mock import Mock
 
 from django.contrib.admin.sites import AdminSite
 
-from sme_terceirizadas.imr.admin import TipoPenalidadeAdmin
-from sme_terceirizadas.imr.models import TipoPenalidade
+from sme_terceirizadas.imr.admin import TipoOcorrenciaAdmin, TipoPenalidadeAdmin
+from sme_terceirizadas.imr.models import TipoOcorrencia, TipoPenalidade
 
 
 def test_tipo_penalidade_admin(
@@ -19,3 +19,18 @@ def test_tipo_penalidade_admin(
         obj=tipo_penalidade, request=Mock(user=usuario), form=None, change=None
     )
     assert tipo_penalidade.criado_por == usuario
+
+
+def test_tipo_ocorrencia_admin(
+    client_autenticado_vinculo_coordenador_supervisao_nutricao, tipo_ocorrencia_factory
+):
+    client, usuario = client_autenticado_vinculo_coordenador_supervisao_nutricao
+
+    tipo_ocorrencia = tipo_ocorrencia_factory()
+    tipo_ocorrencia_admin = TipoOcorrenciaAdmin(
+        model=TipoOcorrencia, admin_site=AdminSite()
+    )
+    tipo_ocorrencia_admin.save_model(
+        obj=tipo_ocorrencia, request=Mock(user=usuario), form=None, change=None
+    )
+    assert tipo_ocorrencia.criado_por == usuario
