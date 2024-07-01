b7481e124cf1c86668c8b49b7a9c8e86313d069f
Calvin Rossinhole <calvin.masters@gmail.com>
2024-07-01 09:06:05 -0300

    test(-): :white_check_mark: teste formulario diretor get ocorrencias viewset

--- sme_terceirizadas/imr/__tests__/test_views/test_formulario_diretor_viewset.py
+++ sme_terceirizadas/imr/__tests__/test_views/test_formulario_diretor_viewset.py
@@ -4,12 +4,12 @@ import uuid
 import pytest
 from rest_framework import status
 
-from sme_terceirizadas.imr.models import FormularioOcorrenciasBase
+from sme_terceirizadas.imr.models import FormularioOcorrenciasBase, TipoOcorrencia
 
 pytestmark = pytest.mark.django_db
 
 
-def test_formulario_diretor(
+def test_create_formulario_diretor(
     client_autenticado_diretor_escola,
     escola,
     solicitacao_medicao_inicial_factory,
@@ -114,7 +114,7 @@ def test_formulario_diretor(
     assert instance.respostas_campo_numerico.filter(resposta=10).exists() is True
 
 
-def test_formulario_diretor_erro_parametrizacao_uuid(
+def test_create_formulario_diretor_erro_parametrizacao_uuid(
     client_autenticado_diretor_escola,
     escola,
     solicitacao_medicao_inicial_factory,
@@ -177,3 +177,127 @@ def test_formulario_diretor_erro_parametrizacao_uuid(
     assert response.json() == {
         "detail": f"ParametrizacaoOcorrencia com o UUID {str(uuid_incorreto)} não foi encontrada"
     }
+
+
+def test_formulario_diretor_get_tipos_ocorrencias(
+    client_autenticado_diretor_escola,
+    edital_factory,
+    categoria_ocorrencia_factory,
+    tipo_resposta_modelo_factory,
+    tipo_pergunta_parametrizacao_ocorrencia_factory,
+    tipo_ocorrencia_factory,
+    parametrizacao_ocorrencia_factory,
+):
+    edital = edital_factory.create(numero="78/SME/2016")
+
+    tipo_resposta_campo_simples = tipo_resposta_modelo_factory(
+        nome="RespostaCampoTextoSimples"
+    )
+    tipo_pergunta_parametrizacao_ocorrencia_texto_simples = (
+        tipo_pergunta_parametrizacao_ocorrencia_factory(
+            nome="Campo de Texto Simples", tipo_resposta=tipo_resposta_campo_simples
+        )
+    )
+
+    tipo_resposta_campo_numerico = tipo_resposta_modelo_factory(
+        nome="RespostaCampoNumerico"
+    )
+    tipo_pergunta_parametrizacao_ocorrencia_campo_numerico = (
+        tipo_pergunta_parametrizacao_ocorrencia_factory(
+            nome="Campo Numérico", tipo_resposta=tipo_resposta_campo_numerico
+        )
+    )
+
+    categoria = categoria_ocorrencia_factory.create(
+        posicao=1,
+        nome="FUNCIONÁRIOS",
+        perfis=[TipoOcorrencia.DIRETOR],
+    )
+    categoria_2 = categoria_ocorrencia_factory.create(
+        posicao=2,
+        nome="RECEBIMENTO DE ALIMENTOS",
+        perfis=[TipoOcorrencia.SUPERVISAO],
+    )
+
+    tipo_ocorrencia_1 = tipo_ocorrencia_factory.create(
+        posicao=1,
+        titulo="UNIFORME DOS MANIPULADORES",
+        descricao="Funcionários utilizavam uniforme completo? Se NÃO, detalhar qual item do uniforme faltou, o que "
+        "estava utilizando em substituição aos itens previstos e nome completo do(s) funcionário(s).",
+        penalidade__edital=edital,
+        penalidade__numero_clausula="10.42",
+        perfis=[TipoOcorrencia.DIRETOR],
+        categoria=categoria,
+        edital=edital,
+    )
+    parametrizacao_ocorrencia_factory(
+        tipo_ocorrencia=tipo_ocorrencia_1,
+        tipo_pergunta=tipo_pergunta_parametrizacao_ocorrencia_texto_simples,
+        titulo="Qual uniforme faltou?",
+    )
+    parametrizacao_ocorrencia_factory(
+        tipo_ocorrencia=tipo_ocorrencia_1,
+        tipo_pergunta=tipo_pergunta_parametrizacao_ocorrencia_campo_numerico,
+        titulo="Quantos uniformes faltaram?",
+    )
+
+    response = client_autenticado_diretor_escola["client"].get(
+        f"/imr/formulario-diretor/tipos-ocorrencias/?edital_uuid={edital.uuid}",
+        content_type="application/json",
+    )
+
+    assert response.status_code == status.HTTP_200_OK
+
+    tipos_ocorrencia = response.json()
+
+    assert len(tipos_ocorrencia) == 1
+    assert (
+        any(
+            tipo_ocorrencia
+            for tipo_ocorrencia in tipos_ocorrencia
+            if tipo_ocorrencia["categoria"]["nome"] == categoria.nome
+        )
+        is True
+    )
+    tipo_ocorrencia = next(
+        tipo_ocorrencia
+        for tipo_ocorrencia in tipos_ocorrencia
+        if tipo_ocorrencia["categoria"]["nome"] == categoria.nome
+    )
+    assert len(tipo_ocorrencia["parametrizacoes"]) == 2
+
+    assert (
+        any(
+            tipo_ocorrencia
+            for tipo_ocorrencia in tipos_ocorrencia
+            if tipo_ocorrencia["categoria"]["nome"] == categoria_2.nome
+        )
+        is False
+    )
+
+
+def test_formulario_diretor_get_tipos_ocorrencias_edital_does_not_exist_error(
+    client_autenticado_diretor_escola,
+):
+    uuid_invalido = uuid.uuid4()
+    response = client_autenticado_diretor_escola["client"].get(
+        f"/imr/formulario-diretor/tipos-ocorrencias/?edital_uuid={uuid_invalido}",
+        content_type="application/json",
+    )
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == {
+        "detail": "Edital do tipo IMR com o UUID informado não foi encontrado."
+    }
+
+
+def test_formulario_diretor_get_tipos_ocorrencias_validation_error(
+    client_autenticado_diretor_escola,
+):
+    response = client_autenticado_diretor_escola["client"].get(
+        f"/imr/formulario-diretor/tipos-ocorrencias/?edital_uuid=",
+        content_type="application/json",
+    )
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == {"detail": ["O valor “” não é um UUID válido"]}
