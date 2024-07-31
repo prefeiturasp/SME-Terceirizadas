78d4249da0073f77e19ff2b65eebe78084b66a99
Marco Lima <marco.lima18@gmail.com>
2024-07-31 11:01:18 -0300

    ajuste validacao campo especificar

--- sme_terceirizadas/desperdicio/api/serializers/serializers.py
+++ sme_terceirizadas/desperdicio/api/serializers/serializers.py
@@ -72,7 +72,7 @@ class ControleSobrasCreateSerializer(serializers.Serializer):
         usuario = self.context['request'].user
         data_medicao = validated_data['data_medicao']
         periodo = validated_data['periodo']
-        especiar = validated_data['especificar']
+        especiar = validated_data.get('especificar', '')
 
         try:
             item = ControleSobras.objects.create(
